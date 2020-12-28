b60a14b1398e3d4618d1e682ee3ae29e2af8ce5d
Marvin M <39344769+M123-dev@users.noreply.github.com>
2020-12-28 19:42:39 +0100

    Create smooth_listTile.dart

---
+++ packages/smooth_ui_library/lib/widgets/smooth_listTile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SmoothListTile extends StatelessWidget {
+
+  const SmoothListTile({@required this.text, @required this.onPressed,});
+
+  final String text;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onPressed,
+      child: Container(
+        height: 60.0,
+        padding:
+        const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+        margin: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            color: Colors.black.withAlpha(10),
+            borderRadius:
+            const BorderRadius.all(Radius.circular(20.0))),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(text),
+            SvgPicture.asset(
+              'assets/misc/right_arrow.svg',
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
