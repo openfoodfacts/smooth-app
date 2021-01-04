ba62031583e9a5ef390d9ca56e5f91515779d189
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-01-04 20:31:37 +0100

    Update smooth_navigation_bar_classic.dart

--- packages/smooth_ui_library/lib/navigation/widgets/smooth_navigation_bar_classic.dart
+++ packages/smooth_ui_library/lib/navigation/widgets/smooth_navigation_bar_classic.dart
@@ -47,213 +47,197 @@ class _SmoothNavigationBarClassicState extends State<SmoothNavigationBarClassic>
     return Stack(
       children: <Widget>[
         ClipRRect(
-            borderRadius:
-                BorderRadius.all(Radius.circular(widget.borderRadius)),
-            child: BackdropFilter(
-                filter: ImageFilter.blur(
-                  sigmaX: 4.0,
-                  sigmaY: 4.0,
-                ),
-                child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 68.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                          Radius.circular(widget.borderRadius)),
-                      color: widget.color,
-                    ),
-                    child: Consumer<SmoothNavigationStateModel>(builder:
-                        (BuildContext context,
-                            SmoothNavigationStateModel
-                                smoothNavigationStateModel,
-                            Widget child) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              //left side of scanButton
-                              Row(
-                                children: List<Widget>.generate(
-                                  (widget.buttons.length / 2).floor(),
-                                  (int i) {
-                                    if (smoothNavigationStateModel
-                                            .currentIndex ==
-                                        i) {
-                                      return Column(
-                                        children: <Widget>[
-                                          widget.buttons[i],
-                                          SmoothRevealAnimation(
-                                            child: Text(
-                                              widget.buttons[i].title,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .bodyText1
-                                                  .copyWith(
-                                                      fontWeight:
-                                                          FontWeight.w500,
-                                                          fontSize: 15,
-                                              ),
-                                            ),
-                                            animationCurve:
-                                                widget.animationCurve,
-                                            animationDuration:
-                                                widget.animationDuration,
-                                            startOffset: const Offset(0.0, 1.0),
-                                          )
-                                        ],
-                                      );
-                                    } else {
-                                      return Column(
-                                        children: <Widget>[
-                                          widget.buttons[i],
-                                          Text(
-                                            widget.buttons[i].title,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyText1
-                                                .copyWith(
-                                                    fontWeight:
-                                                        FontWeight.normal,
-                                                    fontSize: 10.0),
-                                          )
-                                        ],
-                                      );
-                                    }
-                                  },
-                                ),
-                              ),
-                              //right side of scanButton
-                              Row(
-                                children: List<Widget>.generate(
-                                  widget.buttons.length -
-                                      (widget.buttons.length / 2).floor(),
-                                  (int i) {
-                                    if (smoothNavigationStateModel
-                                            .currentIndex ==
-                                        i +
-                                            (widget.buttons.length / 2)
-                                                .floor()) {
-                                      return Column(
-                                        children: <Widget>[
-                                          widget.buttons[i +
-                                              (widget.buttons.length / 2)
-                                                  .floor()],
-                                          SmoothRevealAnimation(
-                                            child: Text(
-                                              widget
-                                                  .buttons[i +
-                                                      (widget.buttons.length /
-                                                              2)
-                                                          .floor()]
-                                                  .title,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .bodyText1
-                                                  .copyWith(
-                                                      fontWeight:
-                                                          FontWeight.w500,
-                                                          fontSize: 15,
-                                              ),
-                                            ),
-                                            animationCurve:
-                                                widget.animationCurve,
-                                            animationDuration:
-                                                widget.animationDuration,
-                                            startOffset: const Offset(0.0, 1.0),
-                                          )
-                                        ],
-                                      );
-                                    } else {
-                                      return Column(
-                                        children: <Widget>[
-                                          widget.buttons[i +
-                                              (widget.buttons.length / 2)
-                                                  .floor()],
-                                          Text(
-                                            widget
-                                                .buttons[i +
-                                                    (widget.buttons.length / 2)
-                                                        .floor()]
-                                                .title,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyText1
-                                                .copyWith(
-                                                    fontWeight:
-                                                        FontWeight.normal,
-                                                        fontSize: 10.0,
-                                            ),
-                                          )
-                                        ],
-                                      );
-                                    }
-                                  },
-                                ),
-                              )
-                            ],
-                        ),
-                      );
-                    },
-                    ),
-                ),
+          borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius)),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 4.0,
+              sigmaY: 4.0,
             ),
-        ),
-        //Scan button
-        Consumer<SmoothNavigationStateModel>(builder: (BuildContext context,
-            SmoothNavigationStateModel smoothNavigationStateModel,
-            Widget child) {
-          return Container(
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Transform.translate(
-                  offset: const Offset(0.0, -20.0),
-                  child: GestureDetector(
-                    child: Material(
-                      elevation: 12.0,
-                      shadowColor: widget.scanShadowColor, //Colors.deepPurple,
-                      //shadowColor: Colors.deepPurple,
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(60.0)),
-                      child: Hero(
-                        tag: 'action_button',
-                        child: Container(
-                          width: 56.0,
-                          height: 56.0,
-                          decoration:  BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(60.0)),
-                              color: widget.scanButtonColor,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 68.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.circular(widget.borderRadius)),
+                color: widget.color,
+              ),
+              child: Consumer<SmoothNavigationStateModel>(
+                builder: (BuildContext context,
+                    SmoothNavigationStateModel smoothNavigationStateModel,
+                    Widget child) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: List<Widget>.generate(
+                            (widget.buttons.length / 2).floor(),
+                            (int i) {
+                              if (smoothNavigationStateModel.currentIndex ==
+                                  i) {
+                                return Column(
+                                  children: <Widget>[
+                                    widget.buttons[i],
+                                    SmoothRevealAnimation(
+                                      child: Text(
+                                        widget.buttons[i].title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1
+                                            .copyWith(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 15,
+                                            ),
+                                      ),
+                                      animationCurve: widget.animationCurve,
+                                      animationDuration:
+                                          widget.animationDuration,
+                                      startOffset: const Offset(0.0, 1.0),
+                                    )
+                                  ],
+                                );
+                              } else {
+                                return Column(
+                                  children: <Widget>[
+                                    widget.buttons[i],
+                                    Text(
+                                      widget.buttons[i].title,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1
+                                          .copyWith(
+                                              fontWeight: FontWeight.normal,
+                                              fontSize: 10.0),
+                                    )
+                                  ],
+                                );
+                              }
+                            },
                           ),
-                          child: Center(
-                            child: SvgPicture.asset(
-                              widget.actionSvgIcons[
-                                  smoothNavigationStateModel.currentIndex],
-                              width: 24.0,
-                              height: 24.0,
-                              color: widget.scanIconColor,
+                        ),
+                        Row(
+                          children: List<Widget>.generate(
+                            widget.buttons.length -
+                                (widget.buttons.length / 2).floor(),
+                            (int i) {
+                              if (smoothNavigationStateModel.currentIndex ==
+                                  i + (widget.buttons.length / 2).floor()) {
+                                return Column(
+                                  children: <Widget>[
+                                    widget.buttons[i +
+                                        (widget.buttons.length / 2).floor()],
+                                    SmoothRevealAnimation(
+                                      child: Text(
+                                        widget
+                                            .buttons[i +
+                                                (widget.buttons.length / 2)
+                                                    .floor()]
+                                            .title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1
+                                            .copyWith(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 15,
+                                            ),
+                                      ),
+                                      animationCurve: widget.animationCurve,
+                                      animationDuration:
+                                          widget.animationDuration,
+                                      startOffset: const Offset(0.0, 1.0),
+                                    )
+                                  ],
+                                );
+                              } else {
+                                return Column(
+                                  children: <Widget>[
+                                    widget.buttons[i +
+                                        (widget.buttons.length / 2).floor()],
+                                    Text(
+                                      widget
+                                          .buttons[i +
+                                              (widget.buttons.length / 2)
+                                                  .floor()]
+                                          .title,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1
+                                          .copyWith(
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 10.0,
+                                          ),
+                                    )
+                                  ],
+                                );
+                              }
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        Consumer<SmoothNavigationStateModel>(
+          builder: (BuildContext context,
+              SmoothNavigationStateModel smoothNavigationStateModel,
+              Widget child) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Transform.translate(
+                    offset: const Offset(0.0, -20.0),
+                    child: GestureDetector(
+                      child: Material(
+                        elevation: 12.0,
+                        shadowColor: widget.scanShadowColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(60.0)),
+                        child: Hero(
+                          tag: 'action_button',
+                          child: Container(
+                            width: 56.0,
+                            height: 56.0,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(60.0)),
+                              color: widget.scanButtonColor,
+                            ),
+                            child: Center(
+                              child: SvgPicture.asset(
+                                widget.actionSvgIcons[
+                                    smoothNavigationStateModel.currentIndex],
+                                width: 24.0,
+                                height: 24.0,
+                                color: widget.scanIconColor,
+                              ),
                             ),
                           ),
                         ),
                       ),
+                      onTap: () {
+                        if (widget.actions[
+                                smoothNavigationStateModel.currentIndex] !=
+                            null) {
+                          widget.actions[
+                              smoothNavigationStateModel.currentIndex]();
+                        }
+                        ;
+                      },
                     ),
-                    onTap: () {
-                      if (widget.actions[
-                              smoothNavigationStateModel.currentIndex] !=
-                          null) {
-                        widget
-                            .actions[smoothNavigationStateModel.currentIndex]();
-                      };
-                    },
                   ),
-                ),
-              ],
-            ),
-          );
-         },
+                ],
+              ),
+            );
+          },
         ),
       ],
     );
