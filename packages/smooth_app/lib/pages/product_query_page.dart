dd35d5c87c2ce64e11408689364dd5c0bb136c0b
monsieurtanuki <fabrice_fontaine@hotmail.com>
2021-01-08 10:21:06 +0100

    Minor merge fix.

--- packages/smooth_app/lib/pages/product_query_page.dart
+++ packages/smooth_app/lib/pages/product_query_page.dart
@@ -216,8 +216,7 @@ class _ProductQueryPageState extends State<ProductQueryPage> {
                             backgroundColor: Colors.transparent,
                             bounce: true,
                             barrierColor: Colors.black45,
-                            builder: (BuildContext context,
-                                    ScrollController scrollController) =>
+                            builder: (BuildContext context) =>
                                 GroupQueryFilterView(
                               categories: _model.categories,
                               categoriesList: _model.sortedCategories,
