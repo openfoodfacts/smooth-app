56b193b4d3227bcf1ccff08c0ee6608c4f5be95b
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-01-17 13:51:58 +0100

    Changed to decrypt individual files

---
+++ packages/smooth_app/android/fastlane/envfiles/decrypt_secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$API_JSON_FILE_DECRYPTKEY" \
+--output ./api-4712693179220384697-162836-33ea08672303.json api-4712693179220384697-162836-33ea08672303.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$STORE_JKS_DECRYPTKEY" \
+--output ./keystore.jks keystore.jks.gpg
