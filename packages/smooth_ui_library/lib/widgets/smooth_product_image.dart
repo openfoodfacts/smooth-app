66acfb9feb604c4b37bb85d87216f8479aaa5d3b
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-03-27 16:51:16 +0100

    Failed assertion width > 0.0': is not true

--- packages/smooth_ui_library/lib/widgets/smooth_product_image.dart
+++ packages/smooth_ui_library/lib/widgets/smooth_product_image.dart
@@ -16,37 +16,40 @@ class SmoothProductImage extends StatelessWidget {
       return ClipRRect(
         borderRadius: const BorderRadius.all(Radius.circular(15.0)),
         child: FittedBox(
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  product.imageFrontSmallUrl,
-                  scale: 1.0,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(minWidth: 1, minHeight: 1),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    product.imageFrontSmallUrl,
+                    scale: 1.0,
+                  ),
                 ),
               ),
-            ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
-              child: Image.network(
-                product.imageFrontSmallUrl,
-                fit: BoxFit.contain,
-                loadingBuilder: (BuildContext context, Widget child,
-                    ImageChunkEvent progress) {
-                  if (progress == null) {
-                    return child;
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(
-                      strokeWidth: 2.5,
-                      valueColor:
-                          const AlwaysStoppedAnimation<Color>(Colors.white),
-                      value: progress.cumulativeBytesLoaded /
-                          progress.expectedTotalBytes,
-                    ),
-                  );
-                },
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
+                child: Image.network(
+                  product.imageFrontSmallUrl,
+                  fit: BoxFit.contain,
+                  loadingBuilder: (BuildContext context, Widget child,
+                      ImageChunkEvent progress) {
+                    if (progress == null) {
+                      return child;
+                    }
+                    return Center(
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2.5,
+                        valueColor:
+                            const AlwaysStoppedAnimation<Color>(Colors.white),
+                        value: progress.cumulativeBytesLoaded /
+                            progress.expectedTotalBytes,
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ),
@@ -61,9 +64,12 @@ class SmoothProductImage extends StatelessWidget {
               borderRadius: BorderRadius.all(Radius.circular(15.0)),
             ),
             child: Center(
-              child: SvgPicture.asset(
-                'assets/product/product_not_found.svg',
-                fit: BoxFit.cover,
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(minWidth: 1, minHeight: 1),
+                child: SvgPicture.asset(
+                  'assets/product/product_not_found.svg',
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
