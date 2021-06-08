1c7d637fc9e48b93cdc06070036871891365ee3e
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-06-08 18:23:24 +0200

    Divided into subcategories

--- packages/smooth_app/lib/bottom_sheet_views/product_copy_view.dart
+++ packages/smooth_app/lib/bottom_sheet_views/product_copy_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_app/pages/pantry/common/pantry_button.dart';
+import 'package:smooth_app/pages/product/common/product_list_button.dart';
 
 ///The ModalBottomSheet to choose where to copy/add products to
 class ProductCopyView extends StatelessWidget {
@@ -10,39 +12,53 @@ class ProductCopyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height * 0.9;
     return Material(
       child: Container(
-        height: height,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
+        child: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
           children: <Widget>[
             Container(
-              height: height,
-              child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    margin: const EdgeInsets.only(top: 20.0, bottom: 24.0),
-                    child: Text(
-                      'Copy to:',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ),
-                  Wrap(
-                    direction: Axis.horizontal,
-                    children: children,
-                    spacing: 8.0,
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.15,
-                  )
-                ],
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              margin: const EdgeInsets.only(top: 20.0, bottom: 24.0),
+              child: Text(
+                'Add this product',
+                style: Theme.of(context).textTheme.headline1,
               ),
             ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Lists:',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              width: MediaQuery.of(context).size.width,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              child: Wrap(
+                direction: Axis.horizontal,
+                children: children.whereType<ProductListButton>().toList(),
+                spacing: 8.0,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Pantries:',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              width: MediaQuery.of(context).size.width,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              child: Wrap(
+                direction: Axis.horizontal,
+                children: children.whereType<PantryButton>().toList(),
+                spacing: 8.0,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
           ],
         ),
       ),
