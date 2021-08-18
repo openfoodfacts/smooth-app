e6c9a86947984d7c9b2b70862deb5b5c416c2c75
Slava Shklyaev <slavash@google.com>
2021-08-18 13:20:39 +0100

    Make attribute importance a binary choice

--- packages/smooth_app/lib/pages/user_preferences_page.dart
+++ packages/smooth_app/lib/pages/user_preferences_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/model/Attribute.dart';
 import 'package:openfoodfacts/model/AttributeGroup.dart';
+import 'package:openfoodfacts/personalized_search/preference_importance.dart';
 import 'package:provider/provider.dart';
+import 'package:smooth_app/cards/category_cards/svg_cache.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/pages/settings_page.dart';
@@ -32,8 +34,6 @@ class UserPreferencesPage extends StatelessWidget {
         context.watch<ProductPreferences>();
     final List<AttributeGroup> groups =
         _reorderGroups(productPreferences.attributeGroups!);
-    final List<String> orderedImportantAttributeIds =
-        productPreferences.getOrderedImportantAttributeIds();
     return Scaffold(
       appBar: AppBar(
         title: Text(appLocalizations.myPreferences),
@@ -60,10 +60,10 @@ class UserPreferencesPage extends StatelessWidget {
           groups.length,
           (int index) => _generateGroup(
             context,
+            index,
             groups[index],
             userPreferences,
             productPreferences,
-            _reorderAttributes(groups[index], orderedImportantAttributeIds),
           ),
         ),
       ),
@@ -72,91 +72,62 @@ class UserPreferencesPage extends StatelessWidget {
 
   Widget _generateGroup(
     final BuildContext context,
+    int index,
     final AttributeGroup group,
     final UserPreferences userPreferences,
     final ProductPreferences productPreferences,
-    final List<Attribute> orderedImportantAttributes,
-  ) =>
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
+  ) {
+    final AppLocalizations localizations = AppLocalizations.of(context)!;
+    final ThemeData theme = Theme.of(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
+          child: ListTile(
+            title: Text(
+              group.name ?? 'Unknown',
+              style: theme.textTheme.headline3,
+            ),
+            trailing: index != 0
+                ? null
+                : Text(
+                    localizations.important,
+                    style: theme.textTheme.headline3,
+                  ),
+          ),
+        ),
+        if (group.warning != null)
+          Container(
+            color: SmoothTheme.getColor(
+              theme.colorScheme,
+              AttributeButton.WARNING_COLOR,
+              ColorDestination.BUTTON_BACKGROUND,
+            ),
+            width: double.infinity,
             padding: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
-            child: ListTile(
-              title: Text(
-                group.name ?? 'Unknown',
-                style: Theme.of(context).textTheme.headline3,
+            margin: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
+            child: Text(
+              group.warning ?? 'Unknown',
+              style: TextStyle(
+                color: SmoothTheme.getColor(
+                  theme.colorScheme,
+                  AttributeButton.WARNING_COLOR,
+                  ColorDestination.BUTTON_FOREGROUND,
+                ),
               ),
             ),
           ),
-          if (group.warning != null)
-            Container(
-              color: SmoothTheme.getColor(
-                Theme.of(context).colorScheme,
-                AttributeButton.WARNING_COLOR,
-                ColorDestination.BUTTON_BACKGROUND,
-              ),
-              width: double.infinity,
-              padding: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
-              margin: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
-              child: Text(
-                group.warning ?? 'Unknown',
-                style: TextStyle(
-                  color: SmoothTheme.getColor(
-                    Theme.of(context).colorScheme,
-                    AttributeButton.WARNING_COLOR,
-                    ColorDestination.BUTTON_FOREGROUND,
-                  ),
-                ),
-              ),
-            ),
-          Wrap(
-            children: List<Widget>.generate(
-              orderedImportantAttributes.length,
-              (int index) => Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: AttributeButton(
-                  orderedImportantAttributes[index],
-                  productPreferences,
-                ),
-              ),
-            ),
-          )
-        ],
-      );
-
-  /// Returns a list of the attributes in the preferences order.
-  ///
-  /// First, the attributes ordered by id designated by [orderedAttributeIds],
-  /// if they belong to the [group].
-  /// Then, the remaining attributes of the group in the initial group order.
-  List<Attribute> _reorderAttributes(
-    final AttributeGroup group,
-    final List<String> orderedAttributeIds,
-  ) {
-    if (orderedAttributeIds.isEmpty) {
-      return group.attributes!;
-    }
-    final List<Attribute> importantAttributes = <Attribute>[];
-    final List<Attribute> otherAttributes = <Attribute>[];
-    for (final Attribute attribute in group.attributes!) {
-      if (orderedAttributeIds.contains(attribute.id)) {
-        importantAttributes.add(attribute);
-      } else {
-        otherAttributes.add(attribute);
-      }
-    }
-    if (importantAttributes.isEmpty) {
-      return otherAttributes;
-    }
-    importantAttributes.sort(
-      (Attribute a, Attribute b) => orderedAttributeIds
-          .indexOf(a.id!)
-          .compareTo(orderedAttributeIds.indexOf(b.id!)),
+        ...List<Widget>.generate(
+          group.attributes!.length,
+          (int index) => Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: _AttributeListTile(group.attributes![index]),
+          ),
+        ),
+      ],
     );
-    importantAttributes.addAll(otherAttributes);
-    return importantAttributes;
   }
 
   void _confirmReset(BuildContext context) {
@@ -194,5 +165,35 @@ class UserPreferencesPage extends StatelessWidget {
     result.addAll(groups.where(
         (AttributeGroup g) => !_ORDERED_ATTRIBUTE_GROUP_IDS.contains(g.id)));
     return result;
+  }
+}
+
+class _AttributeListTile extends StatelessWidget {
+  const _AttributeListTile(this.attribute);
+
+  final Attribute attribute;
+
+  @override
+  Widget build(BuildContext context) {
+    final ProductPreferences preferences = context.watch<ProductPreferences>();
+    final bool isImportant =
+        preferences.getImportanceIdForAttributeId(attribute.id!) !=
+            PreferenceImportance.ID_NOT_IMPORTANT;
+    return SwitchListTile(
+      title: Text(attribute.name!),
+      secondary: SvgCache(attribute.iconUrl, width: 40),
+      value: isImportant,
+      onChanged: (bool value) => _setImportance(context, value),
+    );
+  }
+
+  void _setImportance(BuildContext context, bool isImportant) {
+    final ProductPreferences preferences = context.read<ProductPreferences>();
+    preferences.setImportance(
+      attribute.id!,
+      isImportant
+          ? PreferenceImportance.ID_IMPORTANT
+          : PreferenceImportance.ID_NOT_IMPORTANT,
+    );
   }
 }
