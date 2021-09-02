b8b326c897cce2f5a2a4fb04448d6f42c3a191d6
Greg Spencer <gspencer@google.com>
2021-09-02 13:52:04 -0700

    Fix lint errors, add testabiility to static classes

--- packages/smooth_app/lib/themes/constant_icons.dart
+++ packages/smooth_app/lib/themes/constant_icons.dart
@@ -4,13 +4,22 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ConstantIcons {
+  @visibleForTesting
+  const ConstantIcons();
+
+  static ConstantIcons get instance => _instance ??= const ConstantIcons();
+  static ConstantIcons? _instance;
+
+  /// Setter that allows tests to override the singleton instance.
+  @visibleForTesting
+  static set instance(ConstantIcons testInstance) => _instance = testInstance;
+
   static bool _isApple() =>
       defaultTargetPlatform == TargetPlatform.iOS ||
       defaultTargetPlatform == TargetPlatform.macOS;
 
-  static IconData getShareIcon() =>
+  IconData getShareIcon() =>
       _isApple() ? CupertinoIcons.square_arrow_up : Icons.share;
 
-  static IconData getBackIcon() =>
-      _isApple() ? CupertinoIcons.back : Icons.arrow_back;
+  IconData getBackIcon() => _isApple() ? CupertinoIcons.back : Icons.arrow_back;
 }
