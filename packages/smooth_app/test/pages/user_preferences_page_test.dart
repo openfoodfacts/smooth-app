4e5a3618cf815523df39e85fee562d227f79c39a
Kate Lovett <katelovett@google.com>
2021-09-03 16:13:00 -0500

    ++

--- packages/smooth_app/test/pages/user_preferences_page_test.dart
+++ packages/smooth_app/test/pages/user_preferences_page_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:flutter/painting.dart';
+// import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
