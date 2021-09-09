e818732a65c5c947944ae54fcd7d24a8019b911e
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-09-09 17:52:17 +0200

    Update settings_page.dart

--- packages/smooth_app/lib/pages/settings_page.dart
+++ packages/smooth_app/lib/pages/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
