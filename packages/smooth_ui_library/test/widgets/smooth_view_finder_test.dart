c833a11bf9e7cdc9e126637e472976c0a2eee337
Slava Shklyaev <slavash@google.com>
2021-09-09 17:41:36 +0100

    Test animation

--- packages/smooth_ui_library/test/widgets/smooth_view_finder_test.dart
+++ packages/smooth_ui_library/test/widgets/smooth_view_finder_test.dart
@@ -3,24 +3,36 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:smooth_ui_library/widgets/smooth_view_finder.dart';
 
 void main() {
-  testWidgets('SmoothViewFinder looks as expected',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Container(
-          // Add background to make it easier to inspect the golden file.
-          color: Colors.lightGreen,
-          child: const SmoothViewFinder(
-            boxSize: Size(200, 150),
-            lineLength: 250,
-          ),
-        ),
+  group('SmoothViewFinder', () {
+    const Widget widget = Material(
+      // Add background to make it easier to inspect the golden file.
+      color: Colors.lightGreen,
+      child: SmoothViewFinder(
+        boxSize: Size(200, 150),
+        lineLength: 250,
       ),
     );
-    await tester.pump();
-    expect(
-      find.byType(SmoothViewFinder),
-      matchesGoldenFile('goldens/smooth_view_finder.png'),
-    );
+
+    testWidgets('looks as expected', (WidgetTester tester) async {
+      await tester.pumpWidget(widget);
+      expect(
+        find.byType(SmoothViewFinder),
+        matchesGoldenFile('goldens/smooth_view_finder.png'),
+      );
+    });
+
+    testWidgets('pulsates', (WidgetTester tester) async {
+      final AnimationSheetBuilder animationSheet =
+          AnimationSheetBuilder(frameSize: const Size(300, 300));
+      await tester.pumpFrames(
+        animationSheet.record(widget),
+        const Duration(seconds: 3),
+        const Duration(seconds: 1),
+      );
+      expect(
+        animationSheet.collate(1),
+        matchesGoldenFile('goldens/smooth_view_finder-animation.png'),
+      );
+    });
   });
 }
