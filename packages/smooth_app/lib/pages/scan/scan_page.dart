fbd6e274a42c3f3c5bbc1ec78758b110a8bc89cb
Slava Shklyaev <slavash@google.com>
2021-09-15 14:52:05 +0100

    Refactor ScanPage

--- packages/smooth_app/lib/pages/scan/scan_page.dart
+++ packages/smooth_app/lib/pages/scan/scan_page.dart
@@ -5,27 +5,43 @@ import 'package:smooth_app/database/local_database.dart';
 import 'package:smooth_app/database/product_query.dart';
 import 'package:smooth_app/pages/scan/continuous_scan_page.dart';
 
-class ScanPage extends StatelessWidget {
+class ScanPage extends StatefulWidget {
   const ScanPage();
 
   @override
-  Widget build(BuildContext context) {
-    final LocalDatabase localDatabase = context.watch<LocalDatabase>();
-    return FutureBuilder<ContinuousScanModel?>(
-        future: ContinuousScanModel(
-          languageCode: ProductQuery.getCurrentLanguageCode(context),
-          countryCode: ProductQuery.getCurrentCountryCode(),
-        ).load(localDatabase),
-        builder: (BuildContext context,
-            AsyncSnapshot<ContinuousScanModel?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            final ContinuousScanModel? continuousScanModel = snapshot.data;
-            if (continuousScanModel != null) {
-              return ContinuousScanPage(continuousScanModel);
-            }
-          }
-          return const Center(child: CircularProgressIndicator());
-        });
+  State<ScanPage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  ContinuousScanModel? _model;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _updateModel();
   }
 
+  Future<void> _updateModel() async {
+    final LocalDatabase localDatabase = context.watch<LocalDatabase>();
+    if (_model == null) {
+      _model = await ContinuousScanModel(
+        languageCode: ProductQuery.getCurrentLanguageCode(context),
+        countryCode: ProductQuery.getCurrentCountryCode(),
+      ).load(localDatabase);
+    } else {
+      await _model?.refresh();
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_model == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return ChangeNotifierProvider<ContinuousScanModel>(
+      create: (BuildContext context) => _model!,
+      child: ContinuousScanPage(),
+    );
+  }
 }
