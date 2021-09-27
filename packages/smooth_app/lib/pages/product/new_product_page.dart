2f4fa84650b497d0941341b7bf999580196bb77e
Jasmeet Singh <jasmeetsingh@google.com>
2021-09-27 18:44:28 +0200

    Explicitly specify list type

--- packages/smooth_app/lib/pages/product/new_product_page.dart
+++ packages/smooth_app/lib/pages/product/new_product_page.dart
@@ -261,7 +261,7 @@ class _ProductPageState extends State<NewProductPage> {
   }
 
   List<AttributeGroup> _getAttributeGroupsToBeRendered() {
-    final List<AttributeGroup> attributeGroupsToBeRendered = [];
+    final List<AttributeGroup> attributeGroupsToBeRendered = <AttributeGroup>[];
     for (final String groupId in _ATTRIBUTE_GROUP_ORDER) {
       final Iterable<AttributeGroup> groupIterable = _product.attributeGroups!
           .where((AttributeGroup group) => group.id == groupId);
