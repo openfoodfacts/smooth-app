c1026f54ab652dffd5b72b74d5d8da89b9e934a4
dannylo-gaivota <80546050+dannylo-gaivota@users.noreply.github.com>
2021-10-15 07:48:38 -0300

    fix: using String and ToString() on contr model

--- packages/smooth_app/lib/data_models/github_contributors_model.dart
+++ packages/smooth_app/lib/data_models/github_contributors_model.dart
@@ -1,9 +1,9 @@
 class ContributorsModel {
   ContributorsModel({required this.avatarUrl, required this.profilePath});
 
-  ContributorsModel.fromJson(Map<String, dynamic> json)
-      : profilePath = json['html_url'] as String,
-        avatarUrl = json['avatar_url'] as String;
+  ContributorsModel.fromJson(Map<String, String?> json)
+      : profilePath = json['html_url'].toString(),
+        avatarUrl = json['avatar_url'].toString();
 
   final String avatarUrl;
   final String profilePath;
