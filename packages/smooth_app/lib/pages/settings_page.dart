bd866f09bd8971399325619121f332ce19f3392f
M123-dev <marvinmoel05@gmail.com>
2021-11-12 14:50:39 +0100

    fix: deprecated share package + analyzer warnings

--- packages/smooth_app/lib/pages/settings_page.dart
+++ packages/smooth_app/lib/pages/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/themes/smooth_theme.dart';
