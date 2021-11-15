38116dfad0d97fd1fc840465d04fec7e21d78375
Jasmeet Singh <jasmeetsingh@google.com>
2021-11-15 18:39:42 +0100

    Remove unneeded padding from Welcome card which helps remove the overflow on tiny screens

--- packages/smooth_app/lib/widgets/smooth_product_carousel.dart
+++ packages/smooth_app/lib/widgets/smooth_product_carousel.dart
@@ -99,8 +99,9 @@ class SearchCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AppLocalizations localizations = AppLocalizations.of(context)!;
+    final ThemeData theme = Theme.of(context);
     return Card(
-      color: Theme.of(context).colorScheme.background.withOpacity(0.85),
+      color: theme.colorScheme.background.withOpacity(0.85),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
