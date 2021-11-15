7eb49a56f9ed15f3be5a08fb943ac52725591e38
Jasmeet Singh <jasmeetsingh@google.com>
2021-11-15 09:01:03 +0100

    Update buttons on scan page as per the spec

--- packages/smooth_app/lib/pages/scan/continuous_scan_page.dart
+++ packages/smooth_app/lib/pages/scan/continuous_scan_page.dart
@@ -7,16 +7,16 @@ import 'package:smooth_app/data_models/continuous_scan_model.dart';
 import 'package:smooth_app/pages/personalized_ranking_page.dart';
 import 'package:smooth_app/widgets/smooth_product_carousel.dart';
 import 'package:smooth_ui_library/smooth_ui_library.dart';
+import 'package:smooth_ui_library/util/ui_helpers.dart';
 
 class ContinuousScanPage extends StatelessWidget {
   final GlobalKey _scannerViewKey = GlobalKey(debugLabel: 'Barcode Scanner');
 
   @override
   Widget build(BuildContext context) {
-    final ContinuousScanModel model = context.watch<ContinuousScanModel>();
-    final AppLocalizations localizations = AppLocalizations.of(context)!;
     final Size screenSize = MediaQuery.of(context).size;
-    final double carouselHeight = screenSize.height / 2;
+    final ContinuousScanModel model = context.read<ContinuousScanModel>();
+    final double carouselHeight = screenSize.height / 2.2; // Roughly 45%
     final double viewFinderBottomOffset = carouselHeight / 2.0;
     return Scaffold(
       appBar: AppBar(toolbarHeight: 0.0),
@@ -26,8 +26,7 @@ class ContinuousScanPage extends StatelessWidget {
             alignment: Alignment.center,
             color: Colors.black,
             child: Padding(
-              // Double the offset to account for the vertical centering.
-              padding: EdgeInsets.only(bottom: 2 * viewFinderBottomOffset),
+              padding: const EdgeInsets.only(top: 48),
               child: SvgPicture.asset(
                 'assets/actions/scanner_alt_2.svg',
                 width: 60.0,
@@ -56,11 +55,10 @@ class ContinuousScanPage extends StatelessWidget {
             startOffset: const Offset(0.0, 0.1),
             animationCurve: Curves.easeInOutBack,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Padding(
-                  // Double the offset to account for the vertical centering.
-                  padding: EdgeInsets.only(bottom: 2 * viewFinderBottomOffset),
+                  padding: const EdgeInsets.only(top: 48),
                   child: SmoothViewFinder(
                     boxSize: Size(
                       screenSize.width * 0.6,
@@ -77,38 +75,55 @@ class ContinuousScanPage extends StatelessWidget {
             startOffset: const Offset(0.0, -0.1),
             animationCurve: Curves.easeInOutBack,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                AnimatedOpacity(
-                  opacity: model.isNotEmpty ? 1.0 : 0.0,
-                  duration: const Duration(milliseconds: 50),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      ElevatedButton.icon(
-                        icon: const Icon(Icons.cancel_outlined),
-                        onPressed: model.clearScanSession,
-                        label: const Text('Clear'),
-                      ),
-                      ElevatedButton.icon(
-                        icon: const Icon(Icons.emoji_events_outlined),
-                        onPressed: () => _openPersonalizedRankingPage(context),
-                        label: Text(localizations.myPersonalizedRanking),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: SmoothProductCarousel(
-                    showSearchCard: true,
-                    height: carouselHeight,
-                  ),
+                _buildButtonsRow(context, model),
+                const Spacer(),
+                SmoothProductCarousel(
+                  showSearchCard: true,
+                  height: carouselHeight,
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildButtonsRow(BuildContext context, ContinuousScanModel model) {
+    final AppLocalizations localizations = AppLocalizations.of(context)!;
+    final ButtonStyle buttonStyle = ButtonStyle(
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+      ),
+    );
+    return AnimatedOpacity(
+      opacity: model.hasMoreThanOneProduct ? 0.7 : 0.0,
+      duration: const Duration(milliseconds: 50),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: SMALL_SPACE),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: const Icon(Icons.cancel_outlined),
+              onPressed: model.clearScanSession,
+              // TODO(jasmeet): Internationalize
+              label: const Text('Clear'),
+            ),
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: const Icon(Icons.emoji_events_outlined),
+              onPressed: () => _openPersonalizedRankingPage(context),
+              // TODO(jasmeet): Internationalize
+              label: Text('Compare ${model.getBarcodes().length} Products'),
+            ),
+          ],
+        ),
       ),
     );
   }
