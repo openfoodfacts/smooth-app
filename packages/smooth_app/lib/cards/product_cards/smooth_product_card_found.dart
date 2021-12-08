335dace4e9369e9b3c335303c9c158edc25c4ef1
Martin Bruse <zondolfin@gmail.com>
2021-12-08 14:26:12 +0100

    Refactored the product card found widget to look like the Figma designs.

--- packages/smooth_app/lib/cards/product_cards/smooth_product_card_found.dart
+++ packages/smooth_app/lib/cards/product_cards/smooth_product_card_found.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,6 +8,7 @@ import 'package:provider/provider.dart';
 import 'package:smooth_app/cards/data_cards/svg_icon_chip.dart';
 import 'package:smooth_app/cards/expandables/attribute_list_expandable.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
+import 'package:smooth_app/helpers/product_compatibility_helper.dart';
 import 'package:smooth_app/pages/product/product_page.dart';
 import 'package:smooth_ui_library/util/ui_helpers.dart';
 import 'package:smooth_ui_library/widgets/smooth_product_image.dart';
@@ -38,31 +40,19 @@ class SmoothProductCardFound extends StatelessWidget {
       return _getOldStyle(context);
     }
 
-    final ProductPreferences productPreferences =
-        context.watch<ProductPreferences>();
     final Size screenSize = MediaQuery.of(context).size;
     final ThemeData themeData = Theme.of(context);
 
-    final List<String> attributeIds =
-        productPreferences.getOrderedImportantAttributeIds();
     final List<Widget> scores = <Widget>[];
     final double iconSize = IconWidgetSizer.getIconSizeFromContext(context);
     final List<Attribute> attributes =
-        AttributeListExpandable.getPopulatedAttributes(product, attributeIds);
+        AttributeListExpandable.getPopulatedAttributes(
+            product, <String>['nutriscore', 'ecoscore']);
     for (final Attribute attribute in attributes) {
       scores.add(SvgIconChip(attribute.iconUrl!, height: iconSize));
     }
-    String productTitle;
-    if (product.productName != null) {
-      productTitle = product.productName!;
-      if (product.brands != null) {
-        productTitle += ' - ${product.brands!}';
-      }
-    } else if (product.brands != null) {
-      productTitle = product.brands!;
-    } else {
-      productTitle = product.barcode!;
-    }
+    final ProductCompatibility compatibility =
+        getProductCompatibility(context.watch<ProductPreferences>(), product);
     return GestureDetector(
       onTap: () async {
         await Navigator.push<Widget>(
@@ -89,57 +79,53 @@ class SmoothProductCardFound extends StatelessWidget {
             ),
             padding: const EdgeInsets.all(5.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: SmoothProductImage(
-                      product: product,
-                      width: screenSize.width * 0.20,
-                      height: screenSize.width * 0.20),
+                SmoothProductImage(
+                  product: product,
+                  width: screenSize.width * 0.20,
+                  height: screenSize.width * 0.20,
                 ),
-                const SizedBox(
-                  width: 8.0,
-                ),
+                const SizedBox(width: 5),
                 Expanded(
-                  flex: 2,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
-                        width: screenSize.width * 0.65,
-                        child: Column(
+                  child: Container(
+                    height: screenSize.width * 0.2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          product.productName ?? '???',
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          product.brands ?? '???',
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Row(
                           children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Flexible(
-                                  child: Text(
-                                    productTitle,
-                                    maxLines: 2,
-                                    overflow: TextOverflow.fade,
-                                  ),
-                                ),
-                                if (handle != null) handle!,
-                              ],
+                            Icon(
+                              Icons.circle,
+                              size: 15,
+                              color:
+                                  getProductCompatibilityHeaderBackgroundColor(
+                                      compatibility),
                             ),
-                            SizedBox(
-                              width: screenSize.width * 0.65,
-                              child: Wrap(
-                                direction: Axis.horizontal,
-                                children: scores,
-                                spacing: 2.0,
-                                runSpacing: 2.0,
-                              ),
-                            ),
+                            const SizedBox(width: 5),
+                            Text(getProductCompatibilityHeaderTextWidget(
+                                compatibility)),
                           ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
+                ),
+                const SizedBox(width: 5),
+                Column(
+                  children: scores,
                 ),
               ],
             ),
