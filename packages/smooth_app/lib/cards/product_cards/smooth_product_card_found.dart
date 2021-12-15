706322bdcbb6d902867185f250d9483227c64893
jasmeet0817 <jasmeet0817@gmail.com>
2021-12-15 16:24:51 +0530

    Use the new product page everywhere (#727)

--- packages/smooth_app/lib/cards/product_cards/smooth_product_card_found.dart
+++ packages/smooth_app/lib/cards/product_cards/smooth_product_card_found.dart
@@ -8,7 +8,7 @@ import 'package:smooth_app/cards/data_cards/svg_icon_chip.dart';
 import 'package:smooth_app/cards/expandables/attribute_list_expandable.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
 import 'package:smooth_app/helpers/product_compatibility_helper.dart';
-import 'package:smooth_app/pages/product/product_page.dart';
+import 'package:smooth_app/pages/product/new_product_page.dart';
 import 'package:smooth_ui_library/util/ui_helpers.dart';
 import 'package:smooth_ui_library/widgets/smooth_product_image.dart';
 
@@ -58,7 +58,7 @@ class SmoothProductCardFound extends StatelessWidget {
         await Navigator.push<Widget>(
           context,
           MaterialPageRoute<Widget>(
-            builder: (BuildContext context) => ProductPage(product: product),
+            builder: (BuildContext context) => ProductPage(product),
           ),
         );
         refresh?.call();
@@ -153,9 +153,7 @@ class SmoothProductCardFound extends StatelessWidget {
           Navigator.push<Widget>(
             context,
             MaterialPageRoute<Widget>(
-              builder: (BuildContext context) => ProductPage(
-                product: product,
-              ),
+              builder: (BuildContext context) => ProductPage(product),
             ),
           );
         },
