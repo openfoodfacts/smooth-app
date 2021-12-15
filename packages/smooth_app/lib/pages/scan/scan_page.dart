572b44181360cb3c95c49e08215a6b3269c06923
Marvin M <39344769+M123-dev@users.noreply.github.com>
2021-12-15 19:36:45 +0100

    feat: Use visibility_detector to detect if camera is open (#730)
    * feat: Use visibility_detector to detect if camera is open
    
    * Update tab_navigator.dart
    
    * Update pubspec.lock
    
    * Update continuous_scan_page.dart

--- packages/smooth_app/lib/pages/scan/scan_page.dart
+++ packages/smooth_app/lib/pages/scan/scan_page.dart
@@ -6,10 +6,7 @@ import 'package:smooth_app/database/product_query.dart';
 import 'package:smooth_app/pages/scan/continuous_scan_page.dart';
 
 class ScanPage extends StatefulWidget {
-  const ScanPage({required this.offstage, required this.navigatorKey});
-
-  final bool offstage;
-  final GlobalKey<NavigatorState> navigatorKey;
+  const ScanPage();
 
   @override
   State<ScanPage> createState() => _ScanPageState();
@@ -45,28 +42,7 @@ class _ScanPageState extends State<ScanPage> {
 
     return ChangeNotifierProvider<ContinuousScanModel>(
       create: (BuildContext context) => _model!,
-      child: Navigator(
-        key: widget.navigatorKey,
-        onGenerateRoute: (RouteSettings routeSettings) {
-          return MaterialPageRoute<dynamic>(
-            builder: (BuildContext context) => _buildChild(),
-          );
-        },
-      ),
+      child: const ContinuousScanPage(),
     );
-  }
-
-  //This has to be build inside of the ChangeNotifierProvider to prevent the model to be disposed.
-  Widget _buildChild() {
-    //Don't build Scanner (+activate camera) when not on the Scan Tab
-    if (widget.offstage) {
-      _model?.stopQRView();
-      return const Center(
-          child: Text(
-        "This shouldn't be visible since only build when offstage, when you see this page send a email to contact@openfoodfacts.org",
-      ));
-    } else {
-      return const ContinuousScanPage();
-    }
   }
 }
