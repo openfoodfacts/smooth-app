73d87180c733afeab29ead4985e8cc762987ed21
M123-dev <marvinmoel05@gmail.com>
2021-12-15 19:19:54 +0100

    Update continuous_scan_page.dart

--- packages/smooth_app/lib/pages/scan/continuous_scan_page.dart
+++ packages/smooth_app/lib/pages/scan/continuous_scan_page.dart
@@ -18,108 +18,107 @@ class ContinuousScanPage extends StatefulWidget {
 
 class _ContinuousScanPageState extends State<ContinuousScanPage> {
   final GlobalKey _scannerViewKey = GlobalKey(debugLabel: 'Barcode Scanner');
-  ContinuousScanModel? model;
+  ContinuousScanModel? _model;
 
   @override
   Widget build(BuildContext context) {
     return VisibilityDetector(
       key: const Key('VisibilityDetector'),
       onVisibilityChanged: (VisibilityInfo visibilityInfo) {
-        final double visiblePercentage = visibilityInfo.visibleFraction * 100;
-
-        if (visiblePercentage == 0.0) {
-          model?.stopQRView();
+        if (visibilityInfo.visibleFraction == 0.0) {
+          _model?.stopQRView();
         } else {
-          model?.resumeQRView();
+          _model?.resumeQRView();
         }
       },
       child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-        model = context.watch<ContinuousScanModel>();
-        final Size screenSize = MediaQuery.of(context).size;
-        final Size scannerSize = Size(
-          screenSize.width * 0.6,
-          screenSize.width * 0.33,
-        );
-        final double carouselHeight =
-            constraints.maxHeight / 1.81; // roughly 55% of the available height
-        final double buttonRowHeight = areButtonsRendered(model!) ? 48 : 0;
-        final double availableScanHeight =
-            constraints.maxHeight - carouselHeight - buttonRowHeight;
-        // Padding for the qr code scanner. This ensures the scanner has equal spacing between buttons and carousel.
-        final EdgeInsets qrScannerPadding = EdgeInsets.only(
-            top: (availableScanHeight - scannerSize.height) / 2 +
-                buttonRowHeight);
-        final double viewFinderBottomOffset = carouselHeight / 2.0;
-        return Scaffold(
-          appBar: AppBar(toolbarHeight: 0.0),
-          body: Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                color: Colors.black,
-                child: Padding(
-                  padding: qrScannerPadding,
-                  child: SvgPicture.asset(
-                    'assets/actions/scanner_alt_2.svg',
-                    width: 60.0,
-                    height: 60.0,
-                    color: Colors.white,
+        builder: (BuildContext context, BoxConstraints constraints) {
+          _model = context.watch<ContinuousScanModel>();
+          final Size screenSize = MediaQuery.of(context).size;
+          final Size scannerSize = Size(
+            screenSize.width * 0.6,
+            screenSize.width * 0.33,
+          );
+          final double carouselHeight = constraints.maxHeight /
+              1.81; // roughly 55% of the available height
+          final double buttonRowHeight = areButtonsRendered(_model!) ? 48 : 0;
+          final double availableScanHeight =
+              constraints.maxHeight - carouselHeight - buttonRowHeight;
+          // Padding for the qr code scanner. This ensures the scanner has equal spacing between buttons and carousel.
+          final EdgeInsets qrScannerPadding = EdgeInsets.only(
+              top: (availableScanHeight - scannerSize.height) / 2 +
+                  buttonRowHeight);
+          final double viewFinderBottomOffset = carouselHeight / 2.0;
+          return Scaffold(
+            appBar: AppBar(toolbarHeight: 0.0),
+            body: Stack(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  color: Colors.black,
+                  child: Padding(
+                    padding: qrScannerPadding,
+                    child: SvgPicture.asset(
+                      'assets/actions/scanner_alt_2.svg',
+                      width: 60.0,
+                      height: 60.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              SmoothRevealAnimation(
-                delay: 400,
-                startOffset: Offset.zero,
-                animationCurve: Curves.easeInOutBack,
-                child: QRView(
-                  overlay: QrScannerOverlayShape(
-                    // We use [SmoothViewFinder] instead of the overlay.
-                    overlayColor: Colors.transparent,
-                    // This offset adjusts the scanning area on iOS.
-                    cutOutBottomOffset: viewFinderBottomOffset,
+                SmoothRevealAnimation(
+                  delay: 400,
+                  startOffset: Offset.zero,
+                  animationCurve: Curves.easeInOutBack,
+                  child: QRView(
+                    overlay: QrScannerOverlayShape(
+                      // We use [SmoothViewFinder] instead of the overlay.
+                      overlayColor: Colors.transparent,
+                      // This offset adjusts the scanning area on iOS.
+                      cutOutBottomOffset: viewFinderBottomOffset,
+                    ),
+                    key: _scannerViewKey,
+                    onQRViewCreated: _model!.setupScanner,
                   ),
-                  key: _scannerViewKey,
-                  onQRViewCreated: model!.setupScanner,
                 ),
-              ),
-              SmoothRevealAnimation(
-                delay: 400,
-                startOffset: const Offset(0.0, 0.1),
-                animationCurve: Curves.easeInOutBack,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: qrScannerPadding,
-                      child: SmoothViewFinder(
-                        boxSize: scannerSize,
-                        lineLength: screenSize.width * 0.8,
+                SmoothRevealAnimation(
+                  delay: 400,
+                  startOffset: const Offset(0.0, 0.1),
+                  animationCurve: Curves.easeInOutBack,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: qrScannerPadding,
+                        child: SmoothViewFinder(
+                          boxSize: scannerSize,
+                          lineLength: screenSize.width * 0.8,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SmoothRevealAnimation(
-                delay: 400,
-                startOffset: const Offset(0.0, -0.1),
-                animationCurve: Curves.easeInOutBack,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    _buildButtonsRow(context, model!),
-                    const Spacer(),
-                    SmoothProductCarousel(
-                      showSearchCard: true,
-                      height: carouselHeight,
-                    ),
-                  ],
+                SmoothRevealAnimation(
+                  delay: 400,
+                  startOffset: const Offset(0.0, -0.1),
+                  animationCurve: Curves.easeInOutBack,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      _buildButtonsRow(context, _model!),
+                      const Spacer(),
+                      SmoothProductCarousel(
+                        showSearchCard: true,
+                        height: carouselHeight,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      }),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 
