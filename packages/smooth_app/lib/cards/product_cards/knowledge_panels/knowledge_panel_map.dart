83073f731a762f5a123ee4adce0fd39c0325aa87
Jasmeet Singh <jasmeetsingh@google.com>
2021-12-15 12:51:08 +0100

    Render a mao

--- packages/smooth_app/lib/cards/product_cards/knowledge_panels/knowledge_panel_map.dart
+++ packages/smooth_app/lib/cards/product_cards/knowledge_panels/knowledge_panel_map.dart
@@ -3,43 +3,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:openfoodfacts/model/KnowledgePanelElement.dart';
+import 'package:smooth_ui_library/util/ui_helpers.dart';
 
 class KnowledgePanelMap extends StatelessWidget {
   const KnowledgePanelMap(this.mapElement);
 
-  final KnowledgePanelMapElement mapElement;
+  final KnowledgePanelWorldMapElement mapElement;
 
   @override
   Widget build(BuildContext context) {
+    if (mapElement.pointers.isEmpty || mapElement.pointers.first.geo == null) {
+      return EMPTY_WIDGET;
+    }
     return SizedBox(
       height: 300,
       child: FlutterMap(
         options: MapOptions(
-          bounds: LatLngBounds(LatLng(58.8, 6.1), LatLng(59, 6.2)),
-          boundsOptions: FitBoundsOptions(padding: EdgeInsets.all(8.0)),
-          zoom: 8.0,
+          center: LatLng(mapElement.pointers.first.geo!.lat,
+              mapElement.pointers.first.geo!.lng),
+          zoom: 13.0,
         ),
-        layers: [
+        layers: <LayerOptions>[
           TileLayerOptions(
             urlTemplate:
-            'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-            subdomains: ['a', 'b', 'c'],
+                'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+            subdomains: <String>['a', 'b', 'c'],
           ),
           MarkerLayerOptions(
-            markers: [
-              Marker(
-                point: LatLng(40.0, -120.0),
-                builder: (ctx) =>
-                    Icon(
-                      Icons.pin_drop,
-                      color: Colors.blue,
-                      size: 56,
-                    ),
-              ),
-            ],
+            markers: getMarkers(mapElement.pointers),
           ),
         ],
       ),
     );
+  }
+
+  List<Marker> getMarkers(List<KnowledgePanelGeoPointer> pointers) {
+    final List<Marker> markers = <Marker>[];
+    for (final KnowledgePanelGeoPointer pointer in pointers) {
+      if (pointer.geo == null) {
+        continue;
+      }
+      markers.add(Marker(
+        point: LatLng(pointer.geo!.lat, pointer.geo!.lng),
+        builder: (BuildContext ctx) => const Icon(
+          Icons.pin_drop,
+          color: Colors.blue,
+          size: 56,
+        ),
+      ));
+    }
+    return markers;
   }
 }
