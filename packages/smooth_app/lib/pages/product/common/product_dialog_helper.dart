6e20e057c4c6488017a6fc701ce863e533b8a646
monsieurtanuki <fabrice_fontaine@hotmail.com>
2021-12-28 21:19:23 +0100

    feature/#782 - dart analyze fix
    Impacted file:
    * `product_dialog_helper.dart`

--- packages/smooth_app/lib/pages/product/common/product_dialog_helper.dart
+++ packages/smooth_app/lib/pages/product/common/product_dialog_helper.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:openfoodfacts/model/Product.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:openfoodfacts/utils/CountryHelper.dart';
 import 'package:smooth_app/data_models/fetched_product.dart';
