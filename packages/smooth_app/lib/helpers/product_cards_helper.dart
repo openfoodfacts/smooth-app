fe5792148fb5328471050da0b630dbe0199b431f
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-01-03 19:26:26 +0100

    feat: #833 - removed useless cupertino.dart imports
    Impacted files:
    * `attributes_card_helper.dart`
    * `forgot_password_page.dart`
    * `product_cards_helper.dart`
    * `score_card_helper.dart`
    * `smooth_reveal_animation.dart`
    * `svg_icon_chip.dart`

--- packages/smooth_app/lib/helpers/product_cards_helper.dart
+++ packages/smooth_app/lib/helpers/product_cards_helper.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:openfoodfacts/model/Attribute.dart';
