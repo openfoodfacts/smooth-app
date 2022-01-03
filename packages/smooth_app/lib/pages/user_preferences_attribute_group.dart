3808e55b62feb1be8bc859df2db8276a1eb04165
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-01-03 15:24:18 +0100

    feat: #798 - localization and collapsable attribute groups
    New file:
    * `user_preferences_food.dart`: Collapsed/expanded display of attribute groups for the preferences page.
    
    Impacted files:
    * `abstract_user_preferences.dart`: refactored for localization
    * `app_en.arb`: added 6 labels for preferences titles and subtitles
    * `app_fr.arb`: added 6 labels for preferences titles and subtitles
    * `user_preferences_attribute_group.dart`: now we manage here only one group at a time, to make it collapsable
    * `user_preferences_page.dart`: refactored for localization
    * `user_preferences_profile.dart`: refactored for localization
    * `user_preferences_settings.dart`: refactored for localization

--- packages/smooth_app/lib/pages/user_preferences_attribute_group.dart
+++ packages/smooth_app/lib/pages/user_preferences_attribute_group.dart
@@ -1,127 +1,97 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/model/Attribute.dart';
 import 'package:openfoodfacts/model/AttributeGroup.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
+import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/pages/abstract_user_preferences.dart';
 import 'package:smooth_app/themes/smooth_theme.dart';
-import 'package:smooth_app/themes/theme_provider.dart';
 import 'package:smooth_app/widgets/attribute_button.dart';
 import 'package:smooth_app/widgets/attribute_helper.dart';
 
-/// Collapsed/expanded display of attributes for the preferences page.
+/// Collapsed/expanded display of an attribute group for the preferences page.
 class UserPreferencesAttributeGroup extends AbstractUserPreferences {
-  UserPreferencesAttributeGroup(
-    final Function(Function()) setState,
-    this.productPreferences,
-  ) : super(setState);
+  UserPreferencesAttributeGroup({
+    required this.productPreferences,
+    required this.group,
+    required final Function(Function()) setState,
+    required final BuildContext context,
+    required final UserPreferences userPreferences,
+    required final AppLocalizations appLocalizations,
+    required final ThemeData themeData,
+  }) : super(
+          setState: setState,
+          context: context,
+          userPreferences: userPreferences,
+          appLocalizations: appLocalizations,
+          themeData: themeData,
+        );
 
   final ProductPreferences productPreferences;
+  final AttributeGroup group;
+
+  static const double _TYPICAL_PADDING_OR_MARGIN = 12;
 
   @override
   bool isCollapsedByDefault() => false;
 
   @override
-  String getPreferenceFlagKey() => 'attributes';
+  String getPreferenceFlagKey() => 'attribute:${group.id}';
 
   @override
-  String getTitle() => 'Food Preferences';
+  Widget getTitle() => Text(
+        group.name ?? appLocalizations.unknown,
+        style: themeData.textTheme.headline3,
+      );
 
   @override
-  String getSubtitle() =>
-      'Choose what information about food matters most to you';
-
-  static const double _TYPICAL_PADDING_OR_MARGIN = 12;
-
-  static const List<String> _ORDERED_ATTRIBUTE_GROUP_IDS = <String>[
-    AttributeGroup.ATTRIBUTE_GROUP_NUTRITIONAL_QUALITY,
-    AttributeGroup.ATTRIBUTE_GROUP_INGREDIENT_ANALYSIS,
-    AttributeGroup.ATTRIBUTE_GROUP_ENVIRONMENT,
-    AttributeGroup.ATTRIBUTE_GROUP_PROCESSING,
-    AttributeGroup.ATTRIBUTE_GROUP_LABELS,
-    AttributeGroup.ATTRIBUTE_GROUP_ALLERGENS,
-  ];
+  Widget? getSubtitle() => null;
 
   @override
-  List<Widget> getBody(
-    final BuildContext context,
-    final AppLocalizations appLocalizations,
-    final ThemeProvider themeProvider,
-    final ThemeData themeData,
-  ) {
-    final List<AttributeGroup> groups =
-        _reorderGroups(productPreferences.attributeGroups!);
+  List<Widget> getBody() {
     final List<String> orderedImportantAttributeIds =
         productPreferences.getOrderedImportantAttributeIds();
     final List<Widget> result = <Widget>[];
-    for (final AttributeGroup group in groups) {
-      final List<Attribute> orderedImportantAttributes =
-          _reorderAttributes(group, orderedImportantAttributeIds);
+    final List<Attribute> orderedImportantAttributes =
+        _reorderAttributes(group, orderedImportantAttributeIds);
+    if (group.warning != null) {
       result.add(
-        _getListItemAttribute(context, group, productPreferences,
-            orderedImportantAttributes, appLocalizations, themeData),
-      );
-    }
-    return result;
-  }
-
-  Widget _getListItemAttribute(
-    final BuildContext context,
-    final AttributeGroup group,
-    final ProductPreferences productPreferences,
-    final List<Attribute> orderedImportantAttributes,
-    final AppLocalizations appLocalizations,
-    final ThemeData themeData,
-  ) =>
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
-            child: ListTile(
-              title: Text(
-                group.name ?? appLocalizations.unknown,
-                style: themeData.textTheme.headline3,
-              ),
-            ),
+        Container(
+          color: SmoothTheme.getColor(
+            Theme.of(context).colorScheme,
+            WARNING_COLOR,
+            ColorDestination.BUTTON_BACKGROUND,
           ),
-          if (group.warning != null)
-            Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
+          margin: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
+          child: Text(
+            group.warning ?? appLocalizations.unknown,
+            style: TextStyle(
               color: SmoothTheme.getColor(
                 Theme.of(context).colorScheme,
                 WARNING_COLOR,
-                ColorDestination.BUTTON_BACKGROUND,
-              ),
-              width: double.infinity,
-              padding: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
-              margin: const EdgeInsets.all(_TYPICAL_PADDING_OR_MARGIN),
-              child: Text(
-                group.warning ?? appLocalizations.unknown,
-                style: TextStyle(
-                  color: SmoothTheme.getColor(
-                    Theme.of(context).colorScheme,
-                    WARNING_COLOR,
-                    ColorDestination.BUTTON_FOREGROUND,
-                  ),
-                ),
+                ColorDestination.BUTTON_FOREGROUND,
               ),
             ),
-          Wrap(
-            children: List<Widget>.generate(
-              orderedImportantAttributes.length,
-              (int index) => Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: AttributeButton(
-                  orderedImportantAttributes[index],
-                  productPreferences,
-                ),
-              ),
-            ),
-          )
-        ],
+          ),
+        ),
       );
+    }
+    result.addAll(
+      List<Widget>.generate(
+        orderedImportantAttributes.length,
+        (int index) => Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: AttributeButton(
+            orderedImportantAttributes[index],
+            productPreferences,
+          ),
+        ),
+      ),
+    );
+    return result;
+  }
 
   /// Returns a list of the attributes in the preferences order.
   ///
@@ -154,15 +124,5 @@ class UserPreferencesAttributeGroup extends AbstractUserPreferences {
     );
     importantAttributes.addAll(otherAttributes);
     return importantAttributes;
-  }
-
-  List<AttributeGroup> _reorderGroups(List<AttributeGroup> groups) {
-    final List<AttributeGroup> result = <AttributeGroup>[];
-    for (final String id in _ORDERED_ATTRIBUTE_GROUP_IDS) {
-      result.addAll(groups.where((AttributeGroup g) => g.id == id));
-    }
-    result.addAll(groups.where(
-        (AttributeGroup g) => _ORDERED_ATTRIBUTE_GROUP_IDS.contains(g.id)));
-    return result;
   }
 }
--- packages/smooth_app/lib/pages/user_preferences_profile.dart
+++ packages/smooth_app/lib/pages/user_preferences_profile.dart
@@ -1,16 +1,27 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
+import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/helpers/user_management_helper.dart';
 import 'package:smooth_app/pages/abstract_user_preferences.dart';
 import 'package:smooth_app/pages/user_management/login_page.dart';
-import 'package:smooth_app/themes/theme_provider.dart';
 
 /// Collapsed/expanded display of profile for the preferences page.
 class UserPreferencesProfile extends AbstractUserPreferences {
-  UserPreferencesProfile(final Function(Function()) setState) : super(setState);
+  UserPreferencesProfile({
+    required final Function(Function()) setState,
+    required final BuildContext context,
+    required final UserPreferences userPreferences,
+    required final AppLocalizations appLocalizations,
+    required final ThemeData themeData,
+  }) : super(
+          setState: setState,
+          context: context,
+          userPreferences: userPreferences,
+          appLocalizations: appLocalizations,
+          themeData: themeData,
+        );
 
   @override
   bool isCollapsedByDefault() => true;
@@ -19,19 +30,17 @@ class UserPreferencesProfile extends AbstractUserPreferences {
   String getPreferenceFlagKey() => 'profile';
 
   @override
-  String getTitle() => 'Your Profile';
+  Widget getTitle() => Text(
+        appLocalizations.myPreferences_profile_title,
+        style: themeData.textTheme.headline2,
+      );
 
   @override
-  String getSubtitle() => 'Set app settings and find out advices and blah blah';
+  Widget? getSubtitle() =>
+      Text(appLocalizations.myPreferences_profile_subtitle);
 
   @override
-  List<Widget> getBody(
-    final BuildContext context,
-    final AppLocalizations appLocalizations,
-    final ThemeProvider themeProvider,
-    final ThemeData themeData,
-  ) =>
-      <Widget>[
+  List<Widget> getBody() => <Widget>[
         ListTile(
           leading: const Icon(Icons.threesixty_outlined),
           title: const Text('Check credentials'),
