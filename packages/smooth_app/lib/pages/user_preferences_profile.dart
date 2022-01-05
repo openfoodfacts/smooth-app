b90bbaf414dd72827a67e048a4c100b9fe254865
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-01-05 18:56:28 +0100

    feat: #849 - added country selector to preferences page (#861)
    Impacted files:
    * `country_selector.dart`: fixed an async bug; added facultative default country, decoration and padding
    * `user_preferences_profile.dart`: added a "select country" item
    * `welcome_page.dart`: added decoration and padding as parameters to `CountrySelector`

--- packages/smooth_app/lib/pages/user_preferences_profile.dart
+++ packages/smooth_app/lib/pages/user_preferences_profile.dart
@@ -5,6 +5,7 @@ import 'package:smooth_app/data_models/product_preferences.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/helpers/user_management_helper.dart';
 import 'package:smooth_app/pages/abstract_user_preferences.dart';
+import 'package:smooth_app/pages/onboarding/country_selector.dart';
 import 'package:smooth_app/pages/user_management/login_page.dart';
 
 /// Collapsed/expanded display of profile for the preferences page.
@@ -68,6 +69,12 @@ class UserPreferencesProfile extends AbstractUserPreferences {
             MaterialPageRoute<Widget>(
               builder: (BuildContext context) => const LoginPage(),
             ),
+          ),
+        ),
+        ListTile(
+          leading: const Icon(Icons.public),
+          title: CountrySelector(
+            initialCountryCode: userPreferences.userCountryCode,
           ),
         ),
         ListTile(
