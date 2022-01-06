b4352d7a53dafc093ede7951b017cb1255c27f55
M123-dev <marvinmoel05@gmail.com>
2022-01-06 20:46:38 +0100

    feat: Better user management buttons in preferences

--- packages/smooth_app/lib/pages/user_preferences_profile.dart
+++ packages/smooth_app/lib/pages/user_preferences_profile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:openfoodfacts/utils/OpenFoodAPIConfiguration.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
+import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/helpers/user_management_helper.dart';
 import 'package:smooth_app/pages/abstract_user_preferences.dart';
 import 'package:smooth_app/pages/onboarding/country_selector.dart';
@@ -41,36 +43,111 @@ class UserPreferencesProfile extends AbstractUserPreferences {
       Text(appLocalizations.myPreferences_profile_subtitle);
 
   @override
-  List<Widget> getBody() => <Widget>[
-        ListTile(
-          leading: const Icon(Icons.threesixty_outlined),
-          title: const Text('Check credentials'),
-          onTap: () async {
-            final bool correct =
-                await UserManagementHelper.checkAndReMountCredentials();
+  List<Widget> getBody() {
+    final ThemeData theme = Theme.of(context);
+    final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
+    final Size size = MediaQuery.of(context).size;
 
-            final SnackBar snackBar = SnackBar(
-              content: Text('It is $correct'),
-              action: SnackBarAction(
-                label: 'Logout',
-                onPressed: () async {
-                  UserManagementHelper.logout();
-                },
+    final List<Widget> result = <Widget>[];
+
+    //Credentials exist
+    if (OpenFoodAPIConfiguration.globalUser != null) {
+      result.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => LaunchUrlHelper.launchURL(
+                'https://openfoodfacts.org/editor/${OpenFoodAPIConfiguration.globalUser!.userId}',
+                true,
               ),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    appLocalizations.view_profile,
+                    style: theme.textTheme.bodyText2?.copyWith(
+                      fontSize: 18.0,
+                      color: theme.colorScheme.surface,
+                    ),
+                  ),
+                  const Icon(Icons.open_in_new),
+                ],
+              ),
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(
+                  Size(size.width * 0.33, theme.buttonTheme.height + 10),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(300.0),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                UserManagementHelper.logout();
+                setState(() {});
+              },
+              child: Text(
+                appLocalizations.sign_out,
+                style: theme.textTheme.bodyText2?.copyWith(
+                  fontSize: 18.0,
+                  color: theme.colorScheme.surface,
+                ),
+              ),
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(
+                  Size(size.width * 0.33, theme.buttonTheme.height + 10),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(300.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        ListTile(
-          leading: const Icon(Icons.supervised_user_circle),
-          title: const Text('User management'),
-          onTap: () => Navigator.push<Widget>(
-            context,
-            MaterialPageRoute<Widget>(
-              builder: (BuildContext context) => const LoginPage(),
+      );
+    } else {
+      // No credentials
+      result.add(
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push<dynamic>(
+                context,
+                MaterialPageRoute<dynamic>(
+                  builder: (BuildContext context) => const LoginPage(),
+                ),
+              );
+              setState(() {});
+            },
+            child: Text(
+              appLocalizations.sign_in,
+              style: theme.textTheme.bodyText2?.copyWith(
+                fontSize: 18.0,
+                color: theme.colorScheme.surface,
+              ),
+            ),
+            style: ButtonStyle(
+              minimumSize: MaterialStateProperty.all<Size>(
+                Size(size.width * 0.5, theme.buttonTheme.height + 10),
+              ),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(300.0),
+                ),
+              ),
             ),
           ),
         ),
+      );
+    }
+
+    result.addAll(
+      <Widget>[
         ListTile(
           leading: const Icon(Icons.public),
           title: CountrySelector(
@@ -82,7 +159,11 @@ class UserPreferencesProfile extends AbstractUserPreferences {
           title: Text(appLocalizations.reset),
           onTap: () => _confirmReset(context),
         ),
-      ];
+      ],
+    );
+
+    return result;
+  }
 
   void _confirmReset(BuildContext context) {
     final AppLocalizations localizations = AppLocalizations.of(context)!;
