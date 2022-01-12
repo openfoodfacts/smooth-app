dc66feca0274a331ee397b2ee97756825a0d9346
jasmeet0817 <jasmeet0817@gmail.com>
2022-01-12 23:00:56 +0530

    Show unanswered questions to the users on product page.  (#922)

---
+++ packages/smooth_app/lib/database/robotoff_questions_query.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:openfoodfacts/utils/QueryType.dart';
+import 'package:smooth_app/database/product_query.dart';
+
+class RobotoffQuestionsQuery {
+  RobotoffQuestionsQuery(this.barcode);
+  final String barcode;
+
+  Future<List<RobotoffQuestion>> getRobotoffQuestionsForProduct() async {
+    final RobotoffQuestionResult result =
+        await OpenFoodAPIClient.getRobotoffQuestionsForProduct(
+      barcode,
+      ProductQuery.getLanguage().code,
+      count: 3,
+      queryType: QueryType.PROD,
+    );
+    return result.questions ?? <RobotoffQuestion>[];
+  }
+}
