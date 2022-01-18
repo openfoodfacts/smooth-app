2c9a863c0de872ebd9be3599acab5bc466416787
Moises Alonso <moises.alonso@codeminer42.com>
2022-01-18 15:11:52 -0300

    refactor: 💡 Change the alert dialogue to smooth alert dialog

--- packages/smooth_app/lib/pages/user_preferences_profile.dart
+++ packages/smooth_app/lib/pages/user_preferences_profile.dart
@@ -7,6 +7,8 @@ import 'package:smooth_app/helpers/user_management_helper.dart';
 import 'package:smooth_app/pages/abstract_user_preferences.dart';
 import 'package:smooth_app/pages/onboarding/country_selector.dart';
 import 'package:smooth_app/pages/user_management/login_page.dart';
+import 'package:smooth_ui_library/buttons/smooth_simple_button.dart';
+import 'package:smooth_ui_library/dialogs/smooth_alert_dialog.dart';
 
 /// Collapsed/expanded display of profile for the preferences page.
 class UserPreferencesProfile extends AbstractUserPreferences {
@@ -153,26 +155,31 @@ class UserPreferencesProfile extends AbstractUserPreferences {
 
   void _confirmLogout(BuildContext context) {
     final AppLocalizations localizations = AppLocalizations.of(context)!;
+    final ThemeData theme = Theme.of(context);
     showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(localizations.sign_out_confirmation),
-          actions: <Widget>[
-            TextButton(
-              child: Text(localizations.yes),
+        return SmoothAlertDialog(
+          close: false,
+          title: localizations.sign_out,
+          body: Text(
+            localizations.sign_out_confirmation,
+          ),
+          actions: [
+            SmoothSimpleButton(
+              text: localizations.yes,
               onPressed: () async {
                 UserManagementHelper.logout();
                 Navigator.pop(context);
                 setState(() {});
               },
             ),
-            TextButton(
-              child: Text(localizations.no),
+            SmoothSimpleButton(
+              text: localizations.no,
               onPressed: () {
                 Navigator.pop(context);
               },
-            )
+            ),
           ],
         );
       },
