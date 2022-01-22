10897e93e1e85e7ad4a7061c14aa9f72e79b95dd
Pierre Slamich <pierre.slamich@gmail.com>
2022-01-22 13:47:41 +0100

    Update packages/smooth_app/lib/pages/user_preferences_settings.dart

--- packages/smooth_app/lib/pages/user_preferences_settings.dart
+++ packages/smooth_app/lib/pages/user_preferences_settings.dart
@@ -8,7 +8,7 @@ import 'package:smooth_app/pages/abstract_user_preferences.dart';
 import 'package:smooth_app/themes/smooth_theme.dart';
 import 'package:smooth_app/themes/theme_provider.dart';
 import 'package:smooth_app/views/bottom_sheet_views/user_contribution_view.dart';
-import 'package:smooth_ui_library/buttons/smoot_action_button.dart';
+import 'package:smooth_ui_library/buttons/smooth_action_button.dart';
 import 'package:smooth_ui_library/buttons/smooth_main_button.dart';
 import 'package:smooth_ui_library/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_ui_library/widgets/smooth_list_tile.dart';
