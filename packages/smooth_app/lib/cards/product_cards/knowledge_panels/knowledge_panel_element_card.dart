3fb2711418e98556b5f3c4ab68c5d1eef283e677
jasmeet0817 <jasmeet0817@gmail.com>
2022-01-25 21:50:05 +0100

    Launch urls from HtmlWidget (#1014)

--- packages/smooth_app/lib/cards/product_cards/knowledge_panels/knowledge_panel_element_card.dart
+++ packages/smooth_app/lib/cards/product_cards/knowledge_panels/knowledge_panel_element_card.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:openfoodfacts/model/KnowledgePanelElement.dart';
 import 'package:openfoodfacts/model/KnowledgePanels.dart';
 import 'package:smooth_app/cards/product_cards/knowledge_panels/knowledge_panel_card.dart';
 import 'package:smooth_app/cards/product_cards/knowledge_panels/knowledge_panel_group_card.dart';
+import 'package:smooth_app/widgets/smooth_html_widget.dart';
 import 'package:smooth_app/cards/product_cards/knowledge_panels/knowledge_panel_table_card.dart';
 import 'package:smooth_app/cards/product_cards/knowledge_panels/knowledge_panel_world_map_card.dart';
 import 'package:smooth_ui_library/util/ui_helpers.dart';
@@ -21,7 +21,9 @@ class KnowledgePanelElementCard extends StatelessWidget {
   Widget build(BuildContext context) {
     switch (knowledgePanelElement.elementType) {
       case KnowledgePanelElementType.TEXT:
-        return HtmlWidget(knowledgePanelElement.textElement!.html);
+        return SmoothHtmlWidget(
+          knowledgePanelElement.textElement!.html,
+        );
       case KnowledgePanelElementType.IMAGE:
         return Image.network(
           knowledgePanelElement.imageElement!.url,
