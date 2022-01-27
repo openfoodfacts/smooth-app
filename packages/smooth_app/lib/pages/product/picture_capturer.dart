7ef00b3d52a13bb41e405a5b6880e300b0380901
Jasmeet Singh <jasmeetsingh@google.com>
2022-01-27 23:05:33 +0100

    ...

--- packages/smooth_app/lib/pages/product/picture_capturer.dart
+++ packages/smooth_app/lib/pages/product/picture_capturer.dart
@@ -1,12 +1,9 @@
 import 'dart:io';
 
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:openfoodfacts/model/ProductImage.dart';
-import 'package:openfoodfacts/model/SendImage.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/database/product_query.dart';
 import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
