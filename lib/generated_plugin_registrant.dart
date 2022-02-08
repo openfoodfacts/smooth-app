6b9bda341b296ef84c583e4aec9fcf4a31022df1
M123-dev <marvinmoel05@gmail.com>
2022-02-08 19:35:13 +0100

    Moved project to root dir

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,28 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:camera_web/camera_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:package_info_plus_web/package_info_plus_web.dart';
+import 'package:sentry_flutter/sentry_flutter_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  CameraPlugin.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PackageInfoPlugin.registerWith(registrar);
+  SentryFlutterWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
