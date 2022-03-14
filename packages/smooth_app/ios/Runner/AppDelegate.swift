17e63a44c5ba550f680abb2d4009a222edf66e45
cli1005 <c.li@share-it.io>
2022-03-14 11:26:13 +0100

    fix: #1155 - MissingPluginException (iOS scanner issue)

--- packages/smooth_app/ios/Runner/AppDelegate.swift
+++ packages/smooth_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
