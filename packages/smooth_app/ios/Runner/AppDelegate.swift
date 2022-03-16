af0720264269c7ed466aae464c031181af705f1e
cli1005 <c.li@share-it.io>
2022-03-16 10:44:44 +0100

    fix: openfoodfacts#1155 - MissingPluginException (iOS scanner issue) - Use an old version of google_ml_kit to remove firebase link

--- packages/smooth_app/ios/Runner/AppDelegate.swift
+++ packages/smooth_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 import Flutter
-import Firebase
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,7 +9,6 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
