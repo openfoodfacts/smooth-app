b89204cd2686d2799bb573bfddd9c308bcf40dbb
Gayangi Seneviratne <sgayangi@gmail.com>
2022-03-19 20:21:38 +0530

    new colors added

--- packages/smooth_app/lib/widgets/attribute_helper.dart
+++ packages/smooth_app/lib/widgets/attribute_helper.dart
@@ -3,3 +3,14 @@ import 'package:flutter/material.dart';
 // Common values and methods for attribute display
 
 const MaterialColor WARNING_COLOR = Colors.deepOrange;
+const Color PRIMARY_BLUE_COLOR = Color(0xFF2D9CDB);
+const Color PRIMARY_GREY_COLOR = Color(0xFF4F4F4F);
+const Color GREY_COLOR = Color(0xFFF2F2F2);
+const Color WHITE_COLOR = Color(0xFFFFFFFF);
+const Color DARK_GREEN_COLOR = Color(0xFF219653);
+const Color LIGHT_GREEN_COLOR = Color(0xFF60AC0E);
+const Color LIGHT_YELLOW_COLOR = Color(0xFFF2C94C);
+const Color DARK_YELLOW_COLOR = Color(0xFFC88F01);
+const Color LIGHT_ORANGE_COLOR = Color(0xFFF2994A);
+const Color DARK_ORANGE_COLOR = Color(0xFFE07312);
+const Color RED_COLOR = Color(0xFFEB5757);
