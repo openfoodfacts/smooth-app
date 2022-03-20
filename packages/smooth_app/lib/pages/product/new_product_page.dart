1859c5a8f32b7ebb2750717a2bf0266979798802
Prabesh <prabesh.bista369@gmail.com>
2022-03-20 21:59:28 +0545

    Added PopUp action menu

--- packages/smooth_app/lib/pages/product/new_product_page.dart
+++ packages/smooth_app/lib/pages/product/new_product_page.dart
@@ -18,7 +18,6 @@ import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/helpers/analytics_helper.dart';
 import 'package:smooth_app/helpers/launch_url_helper.dart';
-import 'package:smooth_app/helpers/product_cards_helper.dart';
 import 'package:smooth_app/pages/product/category_cache.dart';
 import 'package:smooth_app/pages/product/category_picker_page.dart';
 import 'package:smooth_app/pages/product/common/product_dialog_helper.dart';
