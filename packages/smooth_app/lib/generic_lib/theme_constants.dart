f73e78f1ee8d62f0c025fb70dff16d7c2773a2ff
Aman Raj <amanpie9@gmail.com>
2022-03-20 19:36:04 +0530

    constants and methods added

---
+++ packages/smooth_app/lib/generic_lib/theme_constants.dart
@@ -0,0 +1,3 @@
+const String THEME_SYSTEM_DEFAULT = 'System Default';
+const String THEME_LIGHT = 'Light';
+const String THEME_DARK = 'Dark';
--- packages/smooth_app/lib/themes/theme_provider.dart
+++ packages/smooth_app/lib/themes/theme_provider.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
+import 'package:smooth_app/generic_lib/theme_constants.dart';
 import 'package:smooth_app/themes/smooth_theme.dart';
 
 class ThemeProvider with ChangeNotifier {
   ThemeProvider(this._userPreferences);
 
   final UserPreferences _userPreferences;
+  String get currentTheme => _userPreferences.currentTheme;
+  ThemeMode get currentThemeMode {
+    if (_userPreferences.currentTheme == THEME_SYSTEM_DEFAULT) {
+      return ThemeMode.system;
+    } else if (_userPreferences.currentTheme == THEME_LIGHT) {
+      return ThemeMode.light;
+    } else {
+      return ThemeMode.dark;
+    }
+  }
 
   bool get darkTheme => _userPreferences.isThemeDark;
 
@@ -14,6 +25,11 @@ class ThemeProvider with ChangeNotifier {
       return;
     }
     await _userPreferences.setThemeDark(value);
+    notifyListeners();
+  }
+
+  Future<void> setTheme(String value) async {
+    await _userPreferences.setTheme(value);
     notifyListeners();
   }
 
