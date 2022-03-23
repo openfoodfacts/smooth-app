f9d44d9b814363ac4682107ab09d4cecd6e5e3ce
Prabesh <prabesh.bista369@gmail.com>
2022-03-23 17:46:45 +0545

    Added Consent UI

--- packages/smooth_app/lib/pages/onboarding/consent_analytics_page.dart
+++ packages/smooth_app/lib/pages/onboarding/consent_analytics_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smooth_app/data_models/onboarding_loader.dart';
+import 'package:smooth_app/data_models/user_preferences.dart';
+import 'package:smooth_app/pages/onboarding/onboarding_flow_navigator.dart';
+
+import '../../database/local_database.dart';
 
 class ConsentAnalytics extends StatelessWidget {
   const ConsentAnalytics({Key? key}) : super(key: key);
@@ -6,21 +12,26 @@ class ConsentAnalytics extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    final LocalDatabase localDatabase = context.watch<LocalDatabase>();
+    final UserPreferences userPreferences = context.watch<UserPreferences>();
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        children: <Widget>[
           SizedBox(
-            height: size.height*0.2,
-            width: size.width*0.25,
-            child: Image.asset("assets/data.png",
-            
-            fit: BoxFit.contain,),
+            height: size.height * 0.2,
+            width: size.width * 0.28,
+            child: Image.asset(
+              'assets/onboarding/data.png',
+              fit: BoxFit.contain,
+            ),
           ),
           Align(
             alignment: Alignment.center,
             child: Text(
               'Send anonymous analytics',
+              maxLines: 1,
               style: TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
@@ -35,62 +46,100 @@ class ConsentAnalytics extends StatelessWidget {
               maxWidth: size.width * 0.8,
             ),
             child: Text(
-              "Help the Open Food Facts volunteer to improve the app.You decide if you want to send anonymous analytics.",
-              textAlign: TextAlign.center,
-              style:
-                  TextStyle(fontSize: size.height * 0.021, color: Colors.black),
-            ),
-          ),
-          SizedBox(height:size.height*0.03 ,),
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: size.width * 0.8,
-            ),
-            child: Text(
-              "If you change your mind this option can be enabled and disabled at any time from the settings.",
+              'Help the Open Food Facts volunteer to improve the app.You decide if you want to send anonymous analytics.',
               textAlign: TextAlign.center,
               style:
                   TextStyle(fontSize: size.height * 0.021, color: Colors.black),
             ),
           ),
           SizedBox(
-            height: size.height*0.03,
+            height: size.height * 0.03,
           ),
-          Ink(
-            height: size.height*0.06,
-            width: size.width*0.5,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 66, 59, 59),
-              borderRadius: BorderRadius.circular(25.0)
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: size.width * 0.8,
             ),
-            child: Center(
-              child:  Text("Authorize",
-              style: TextStyle(
-                color:Colors.white,
-                fontSize: size.height*0.02
-              ),),
+            child: Text(
+              'If you change your mind this option can be enabled and disabled at any time from the settings.',
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(fontSize: size.height * 0.021, color: Colors.black),
             ),
           ),
-           SizedBox(
-            height: size.height*0.03,
+          SizedBox(
+            height: size.height * 0.03,
           ),
-          Container(
-            height: size.height*0.06,
-            width: size.width*0.5,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 66, 59, 59),
-              borderRadius: BorderRadius.circular(25.0)
-            ),
-            child: Center(
-              child:  Text("Refuse",
-              style: TextStyle(
-                color:Colors.white,
-                fontSize: size.height*0.02
-              ),),
+          //Authorize Button
+          InkWell(
+            borderRadius: BorderRadius.circular(25.0),
+            onTap: () async {
+              await userPreferences.setCrashReports(true);
+              await userPreferences.setAnalyticsReports(true);
+              await OnboardingLoader(localDatabase)
+                  .runAtNextTime(OnboardingPage.CONSENT_PAGE, context);
+              OnboardingFlowNavigator(userPreferences).navigateToPage(
+                  context,
+                  OnboardingFlowNavigator.getNextPage(
+                      OnboardingPage.CONSENT_PAGE));
+            },
+            child: Ink(
+              height: size.height * 0.06,
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: const Color.fromARGB(144, 0, 0, 0),
+                        offset: Offset(size.width * 0.004, size.height * 0.004))
+                  ]),
+              child: Center(
+                child: Text(
+                  'Authorize',
+                  style: TextStyle(
+                      color: Colors.white, fontSize: size.height * 0.02),
+                ),
+              ),
             ),
           ),
 
+          SizedBox(
+            height: size.height * 0.03,
+          ),
 
+          //Refuse Button
+          InkWell(
+            borderRadius: BorderRadius.circular(25.0),
+            onTap: () async {
+              await userPreferences.setCrashReports(true);
+              await userPreferences.setAnalyticsReports(true);
+              await OnboardingLoader(localDatabase)
+                  .runAtNextTime(OnboardingPage.CONSENT_PAGE, context);
+              OnboardingFlowNavigator(userPreferences).navigateToPage(
+                  context,
+                  OnboardingFlowNavigator.getNextPage(
+                      OnboardingPage.CONSENT_PAGE));
+            },
+            child: Ink(
+              height: size.height * 0.06,
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: const Color.fromARGB(144, 0, 0, 0),
+                        offset: Offset(size.width * 0.004, size.height * 0.004))
+                  ]),
+              child: Center(
+                child: Text(
+                  'Refuse',
+                  style: TextStyle(
+                      color: Colors.white, fontSize: size.height * 0.02),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
