8232aadbcafce94a6d1dcb480e845479b54d376c
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2022-03-29 22:34:19 +0930

    feat: Show an analytics consent UI  during onboarding (#1310)
    * chore(develop): release 0.2.0
    
    * added dark theme on personalized page
    
    * Certain Changes
    
    * Reverted changelog changes
    
    * wrapped the circular progress indicator with center widget
    
    * wrapped CircularProgressIndicator with Center Widget
    
    * Added Floating ActionButton
    
    * Added PopUp Menu Action
    
    * Added Pop UP Action Button
    
    * Added Pop-Up Action Button
    
    * Added PopUp action menu
    
    * Added PopUp action Menu
    
    * Added Types
    
    * Certain Changes
    
    * Certain Changes
    
    * No need of Positioned Widget
    
    * Added Consent UI
    
    * Added Consent UI
    
    * certain Changes
    
    * certain changes
    
    * certain changes
    
    * certain Chnages
    
    * certain Chnages
    
    * removed hardCoded boolean value
    
    * changes made
    
    * Added new Strings
    
    * png file removed
    
    * changed color
    
    * changes made
    
    * comma Added
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Marvin M <39344769+M123-dev@users.noreply.github.com>

---
+++ packages/smooth_app/lib/pages/onboarding/consent_analytics_page.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+
+import 'package:smooth_app/data_models/onboarding_loader.dart';
+import 'package:smooth_app/data_models/user_preferences.dart';
+import 'package:smooth_app/database/local_database.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
+import 'package:smooth_app/pages/onboarding/onboarding_flow_navigator.dart';
+
+class ConsentAnalytics extends StatelessWidget {
+  const ConsentAnalytics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final LocalDatabase localDatabase = context.watch<LocalDatabase>();
+    final UserPreferences userPreferences = context.watch<UserPreferences>();
+    final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
+    const Color shadowColor = Color.fromARGB(144, 0, 0, 0);
+    const Color bodyColor = Color.fromARGB(174, 19, 18, 18);
+    const String assetName = 'assets/onboarding/analytics.svg';
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: size.height * 0.2,
+            width: size.width * 0.45,
+            child: SvgPicture.asset(
+              assetName,
+              semanticsLabel: 'Analytics Icons',
+              fit: BoxFit.contain,
+            ),
+          ),
+
+          SizedBox(height: size.height * 0.01),
+
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              appLocalizations.consent_analytics_title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline2!
+                  .apply(color: Colors.black),
+            ),
+          ),
+
+          SizedBox(height: size.height * 0.034),
+
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: size.width * 0.8,
+            ),
+            child: Text(
+              appLocalizations.consent_analytics_body1,
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .apply(color: bodyColor),
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: size.width * 0.8,
+            ),
+            child: Text(
+              appLocalizations.consent_analytics_body2,
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .apply(color: bodyColor),
+            ),
+          ),
+
+          SizedBox(height: size.height * 0.02),
+
+          //Authorize Button
+          InkWell(
+            borderRadius: CIRCULAR_BORDER_RADIUS,
+            onTap: () {
+              _analyticsLogic(true, userPreferences, localDatabase, context);
+            },
+            child: Ink(
+              height: size.height * 0.06,
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                color: LIGHT_GREEN_COLOR,
+                borderRadius: CIRCULAR_BORDER_RADIUS,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    blurRadius: 3.0,
+                    color: shadowColor,
+                    offset: Offset(size.width * 0.004, size.height * 0.004),
+                  )
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    appLocalizations.authorize_button_label,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: WHITE_COLOR,
+                        fontSize: size.height * 0.025),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: size.width * 0.02),
+                    child: Icon(
+                      Icons.check,
+                      color: WHITE_COLOR,
+                      size: size.height * 0.04,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          SizedBox(height: size.height * 0.02),
+
+          //Refuse Button
+          InkWell(
+            borderRadius: CIRCULAR_BORDER_RADIUS,
+            onTap: () {
+              _analyticsLogic(false, userPreferences, localDatabase, context);
+            },
+            child: Ink(
+              height: size.height * 0.06,
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                color: RED_COLOR,
+                borderRadius: CIRCULAR_BORDER_RADIUS,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    blurRadius: 3.0,
+                    color: shadowColor,
+                    offset: Offset(size.width * 0.004, size.height * 0.004),
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    appLocalizations.refuse_button_label,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: WHITE_COLOR,
+                        fontSize: size.height * 0.025),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: size.width * 0.02),
+                    child: Icon(
+                      Icons.close,
+                      color: WHITE_COLOR,
+                      size: size.height * 0.04,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _analyticsLogic(
+    bool accept,
+    UserPreferences userPreferences,
+    LocalDatabase localDatabase,
+    BuildContext context,
+  ) async {
+    await userPreferences.setCrashReports(accept);
+    await userPreferences.setAnalyticsReports(accept);
+    await OnboardingLoader(localDatabase).runAtNextTime(
+      OnboardingPage.CONSENT_PAGE,
+      context,
+    );
+    OnboardingFlowNavigator(userPreferences).navigateToPage(
+      context,
+      OnboardingFlowNavigator.getNextPage(OnboardingPage.CONSENT_PAGE),
+    );
+  }
+}
