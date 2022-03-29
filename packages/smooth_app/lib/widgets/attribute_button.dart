06e281922ce14ebfbb6472d5d1301f1a9f20453f
cli1005 <c.li@share-it.io>
2022-03-29 13:55:14 +0200

    fix: #890 - use setting name as attribute

--- packages/smooth_app/lib/widgets/attribute_button.dart
+++ packages/smooth_app/lib/widgets/attribute_button.dart
@@ -38,7 +38,8 @@ class AttributeButton extends StatelessWidget {
     const double horizontalPadding = LARGE_SPACE;
     final double screenWidth =
         MediaQuery.of(context).size.width - 2 * horizontalPadding;
-    final TextStyle style = themeData.textTheme.headline3!;
+    final TextStyle styleLabel = themeData.textTheme.bodyMedium!;
+    final TextStyle styleButton = themeData.textTheme.headline4!;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
       child: Row(
@@ -47,7 +48,7 @@ class AttributeButton extends StatelessWidget {
         children: <Widget>[
           SizedBox(
             width: screenWidth * .45,
-            child: Text(attribute.name!, style: style),
+            child: Text(attribute.settingName!, style: styleLabel),
           ),
           SizedBox(
             width: screenWidth * .45,
@@ -56,7 +57,7 @@ class AttributeButton extends StatelessWidget {
                 productPreferences
                     .getPreferenceImportanceFromImportanceId(importanceId)!
                     .name!,
-                style: style.copyWith(color: Colors.white),
+                style: styleButton.copyWith(color: Colors.white),
               ),
               style: ElevatedButton.styleFrom(
                 primary: _colors[importanceId],
