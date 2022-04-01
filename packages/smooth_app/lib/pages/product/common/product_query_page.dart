fcd47297fcc072ffa548a465ef91140d0788ac52
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-04-01 13:01:28 +0200

    feat: #1333 - merge conflicts fixes.
    Impacted file:
    * `product_query_page.dart`

--- packages/smooth_app/lib/pages/product/common/product_query_page.dart
+++ packages/smooth_app/lib/pages/product/common/product_query_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
@@ -8,8 +9,8 @@ import 'package:provider/provider.dart';
 import 'package:smooth_app/cards/product_cards/smooth_product_card_found.dart';
 import 'package:smooth_app/data_models/product_list_supplier.dart';
 import 'package:smooth_app/data_models/product_query_model.dart';
-import 'package:smooth_app/generic_lib/loading_dialog.dart';
 import 'package:smooth_app/generic_lib/animations/smooth_reveal_animation.dart';
+import 'package:smooth_app/generic_lib/loading_dialog.dart';
 import 'package:smooth_app/helpers/analytics_helper.dart';
 import 'package:smooth_app/pages/personalized_ranking_page.dart';
 import 'package:smooth_app/pages/product/common/product_query_page_helper.dart';
@@ -193,7 +194,7 @@ class _ProductQueryPageState extends State<ProductQueryPage> {
                   context,
                   MaterialPageRoute<Widget>(
                     builder: (BuildContext context) => PersonalizedRankingPage(
-                      productList: _model.displayProducts!,
+                      products: _model.displayProducts!,
                       title: widget.name,
                     ),
                   ),
@@ -241,126 +242,126 @@ class _ProductQueryPageState extends State<ProductQueryPage> {
                       elevation: 0,
                       automaticallyImplyLeading: false,
                       title: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            getBackArrow(context, widget.mainColor),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24.0),
-                              child: TextButton.icon(
-                                icon: Icon(
-                                  Icons.filter_list,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          _getBackArrow(context, widget.mainColor),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 24.0),
+                            child: TextButton.icon(
+                              icon: Icon(
+                                Icons.filter_list,
+                                color: widget.mainColor,
+                              ),
+                              label: Text(AppLocalizations.of(context)!.filter,
+                                  style: themeData.textTheme.subtitle1!
+                                      .copyWith(color: widget.mainColor)),
+                              style: TextButton.styleFrom(
+                                textStyle: TextStyle(
                                   color: widget.mainColor,
                                 ),
-                                label: Text(
-                                    AppLocalizations.of(context)!.filter,
-                                    style: themeData.textTheme.subtitle1!
-                                        .copyWith(color: widget.mainColor)),
-                                style: TextButton.styleFrom(
-                                  textStyle: TextStyle(
-                                    color: widget.mainColor,
-                                  ),
-                                ),
-                                onPressed: () {
-                                  showCupertinoModalBottomSheet<Widget>(
-                                    expand: false,
-                                    context: context,
-                                    backgroundColor: Colors.transparent,
-                                    bounce: true,
-                                    builder: (BuildContext context) =>
-                                        GroupQueryFilterView(
-                                      categories: _model.categories,
-                                      categoriesList: _model.sortedCategories,
-                                      callback: (String category) {
-                                        _model.selectCategory(category);
-                                        setState(() {});
-                                      },
-                                    ),
-                                  );
-                                },
                               ),
+                              onPressed: () {
+                                showCupertinoModalBottomSheet<Widget>(
+                                  expand: false,
+                                  context: context,
+                                  backgroundColor: Colors.transparent,
+                                  bounce: true,
+                                  builder: (BuildContext context) =>
+                                      GroupQueryFilterView(
+                                    categories: _model.categories,
+                                    categoriesList: _model.sortedCategories,
+                                    callback: (String category) {
+                                      _model.selectCategory(category);
+                                      setState(() {});
+                                    },
+                                  ),
+                                );
+                              },
                             ),
                           ),
-                        )
-                      ],
-                    ),
-                    flexibleSpace: LayoutBuilder(
-                      builder: (
-                        BuildContext context,
-                        BoxConstraints constraints,
-                      ) =>
-                          FlexibleSpaceBar(
-                        centerTitle: true,
-                        title: Text(
-                          widget.name,
-                          textAlign: TextAlign.center,
-                          style: themeData.textTheme.headline1!
-                              .copyWith(color: widget.mainColor),
+                        ],
+                      ),
+                      flexibleSpace: LayoutBuilder(
+                        builder: (
+                          BuildContext context,
+                          BoxConstraints constraints,
+                        ) =>
+                            FlexibleSpaceBar(
+                          centerTitle: true,
+                          title: Text(
+                            widget.name,
+                            textAlign: TextAlign.center,
+                            style: themeData.textTheme.headline1!
+                                .copyWith(color: widget.mainColor),
+                          ),
+                          background: _getHero(screenSize, themeData),
                         ),
-                        background: _getHero(screenSize, themeData),
                       ),
                     ),
-                  ),
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (BuildContext _context, int index) {
-                        if (index >= _model.displayProducts!.length) {
-                          // final button
-                          final int already = _model.displayProducts!.length;
-                          final int totalSize =
-                              _model.supplier.partialProductList.totalSize;
-                          final int next = max(
-                            0,
-                            min(
-                              _model.supplier.productQuery.pageSize,
-                              totalSize - already,
-                            ),
-                          );
-                          final Widget child;
-                          if (next == 0) {
-                            child = Text(// TODO(monsieurtanuki): localize
-                                "You've downloaded all the $totalSize products.");
-                          } else {
-                            child = ElevatedButton.icon(
-                              icon: const Icon(Icons.download_rounded),
-                              label: Text(
-                                'Download $next more products'
-                                '\n'
-                                'Already downloaded $already out of $totalSize.',
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext _context, int index) {
+                          if (index >= _model.displayProducts!.length) {
+                            // final button
+                            final int already = _model.displayProducts!.length;
+                            final int totalSize =
+                                _model.supplier.partialProductList.totalSize;
+                            final int next = max(
+                              0,
+                              min(
+                                _model.supplier.productQuery.pageSize,
+                                totalSize - already,
                               ),
-                              onPressed: () async {
-                                final bool? error =
-                                    await LoadingDialog.run<bool>(
-                                  context: context,
-                                  future: _model.loadNextPage(),
-                                );
-                                if (error != true) {
-                                  await LoadingDialog.error(context: context);
-                                }
-                              },
+                            );
+                            final Widget child;
+                            if (next == 0) {
+                              child = Text(// TODO(monsieurtanuki): localize
+                                  "You've downloaded all the $totalSize products.");
+                            } else {
+                              child = ElevatedButton.icon(
+                                icon: const Icon(Icons.download_rounded),
+                                label: Text(
+                                  'Download $next more products'
+                                  '\n'
+                                  'Already downloaded $already out of $totalSize.',
+                                ),
+                                onPressed: () async {
+                                  final bool? error =
+                                      await LoadingDialog.run<bool>(
+                                    context: context,
+                                    future: _model.loadNextPage(),
+                                  );
+                                  if (error != true) {
+                                    await LoadingDialog.error(context: context);
+                                  }
+                                },
+                              );
+                            }
+                            return Padding(
+                              padding: const EdgeInsets.only(
+                                  bottom: 90.0, left: 20, right: 20),
+                              child: child,
                             );
                           }
+                          final Product product =
+                              _model.displayProducts![index];
                           return Padding(
-                            padding: const EdgeInsets.only(
-                                bottom: 90.0, left: 20, right: 20),
-                            child: child,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 12.0,
+                              vertical: 8.0,
+                            ),
+                            child: SmoothProductCardFound(
+                              heroTag: product.barcode!,
+                              product: product,
+                              elevation:
+                                  themeData.brightness == Brightness.light
+                                      ? 0.0
+                                      : 4.0,
+                            ).build(_context),
                           );
-                        }
-                        final Product product = _model.displayProducts![index];
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12.0,
-                            vertical: 8.0,
-                          ),
-                          child: SmoothProductCardFound(
-                            heroTag: product.barcode!,
-                            product: product,
-                            elevation: themeData.brightness == Brightness.light
-                                ? 0.0
-                                : 4.0,
-                          ).build(_context),
-                        );
-                      },
-                      childCount: _model.displayProducts!.length + 1,
+                        },
+                        childCount: _model.displayProducts!.length + 1,
+                      ),
                     ),
                   ],
                 ),
