c02a37c06f56bedfc20d7cfe0068c525db5b73c3
Abhishek Bhatt <47862474+bhattabhi013@users.noreply.github.com>
2022-04-06 00:48:31 +0530

    fix: different background color on sliverAppBar (#1463)

--- packages/smooth_app/lib/pages/product/common/product_query_page.dart
+++ packages/smooth_app/lib/pages/product/common/product_query_page.dart
@@ -254,6 +254,7 @@ class _ProductQueryPageState extends State<ProductQueryPage> {
                                     style: themeData.textTheme.subtitle1!
                                         .copyWith(color: widget.mainColor)),
                                 style: TextButton.styleFrom(
+                                  primary: widget.mainColor,
                                   textStyle: TextStyle(
                                     color: widget.mainColor,
                                   ),
@@ -392,6 +393,7 @@ class _ProductQueryPageState extends State<ProductQueryPage> {
       Padding(
         padding: const EdgeInsets.only(top: 28.0),
         child: IconButton(
+          splashColor: color,
           icon: Icon(
             ConstantIcons.instance.getBackIcon(),
             color: color,
