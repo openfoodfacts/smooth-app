19782ab53d0b26e34e5f8c11c020afc4199aea46
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-04-08 09:43:18 +0200

    fix: #1518 - normal size for most alert dialogs
    Deleted file:
    * `product_list_dialog_helper.dart`
    
    Impacted files:
    * `basic_test.dart`: refactored
    * `loading_dialog.dart`: refactored
    * `product_dialog_helper.dart`: refactored
    * `smooth_alert_dialog.dart`: now constructors for simple and advanced effects
    * `user_contribution_view.dart`: now explicitly using the "advanced" dialog
    * `user_preferences_profile.dart`: refactored
    * `user_preferences_settings.dart`: now explicitly using the "advanced" dialog

--- packages/smooth_app/lib/generic_lib/dialogs/smooth_alert_dialog.dart
+++ packages/smooth_app/lib/generic_lib/dialogs/smooth_alert_dialog.dart
@@ -14,22 +14,36 @@ import 'package:smooth_app/generic_lib/design_constants.dart';
 ///
 
 class SmoothAlertDialog extends StatelessWidget {
+  /// The most simple alert dialog: no fancy effects.
   const SmoothAlertDialog({
+    this.title,
+    required this.body,
+    required this.actions,
+  })  : close = false,
+        maxHeight = null,
+        _simpleMode = true;
+
+  /// Advanced alert dialog with fancy effects.
+  const SmoothAlertDialog.advanced({
     this.title,
     this.close = true,
     this.maxHeight,
     required this.body,
     this.actions,
-  });
+  }) : _simpleMode = false;
 
   final String? title;
   final bool close;
   final double? maxHeight;
   final Widget body;
   final List<SmoothActionButton>? actions;
+  final bool _simpleMode;
 
   @override
   Widget build(BuildContext context) {
+    if (_simpleMode) {
+      return _buildSimpleDialog(context);
+    }
     return AlertDialog(
       elevation: 4,
       shape: const RoundedRectangleBorder(
@@ -38,44 +52,7 @@ class SmoothAlertDialog extends StatelessWidget {
       content: ConstrainedBox(
         constraints:
             BoxConstraints(maxHeight: maxHeight ?? double.infinity * 0.5),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            if (title != null) ...<Widget>[
-              SizedBox(
-                height: 32,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    _buildCross(true, context),
-                    if (title != null)
-                      Expanded(
-                        child: FittedBox(
-                          child: Text(
-                            title!,
-                            style: Theme.of(context).textTheme.headline2,
-                          ),
-                        ),
-                      ),
-                    _buildCross(false, context),
-                  ],
-                ),
-              ),
-              Divider(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-            ],
-            Expanded(
-              child: SingleChildScrollView(
-                child: body,
-              ),
-            ),
-          ],
-        ),
+        child: _buildContent(context),
       ),
       actions: actions == null
           ? null
@@ -112,12 +89,59 @@ class SmoothAlertDialog extends StatelessWidget {
       return Container();
     }
   }
-}
 
-///
-///   final String title;
-///   final bool close;
-///   final Widget body;
-///   final List<SmoothActionButton> actions;
-///   final double height;
-///
+  Widget _buildSimpleDialog(final BuildContext context) => AlertDialog(
+        elevation: 4,
+        shape: const RoundedRectangleBorder(
+          borderRadius: ROUNDED_BORDER_RADIUS,
+        ),
+        content: _buildContent(context),
+        actions: actions == null
+            ? null
+            : <Widget>[
+                SizedBox(
+                  height: 58,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: actions!,
+                  ),
+                ),
+              ],
+      );
+
+  Widget _buildContent(final BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          if (title != null) ...<Widget>[
+            SizedBox(
+              height: 32,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  _buildCross(true, context),
+                  if (title != null)
+                    Expanded(
+                      child: FittedBox(
+                        child: Text(
+                          title!,
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ),
+                    ),
+                  _buildCross(false, context),
+                ],
+              ),
+            ),
+            Divider(color: Theme.of(context).colorScheme.onBackground),
+            const SizedBox(height: 12),
+          ],
+          if (_simpleMode)
+            body
+          else
+            Expanded(child: SingleChildScrollView(child: body)),
+        ],
+      );
+}
