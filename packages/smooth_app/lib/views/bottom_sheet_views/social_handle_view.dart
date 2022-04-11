47c53f77159495d2e200b46deebb5ea99709eb68
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-04-11 15:15:37 +0200

    fix: #1537 - moved the "slack" and "email" contact options (#1546)
    Impacted files:
    * `smooth_main_button.dart`: added a `TODO` about probable deleting of this class.
    * `social_handle_view.dart`: moved there "slack" and "email" contact options; refactored
    * `user_preferences_settings.dart`: removed the "support" (slack or email) button

--- packages/smooth_app/lib/views/bottom_sheet_views/social_handle_view.dart
+++ packages/smooth_app/lib/views/bottom_sheet_views/social_handle_view.dart
@@ -1,12 +1,20 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mailto/mailto.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_list_tile.dart';
 import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/widgets/modal_bottomsheet_header.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SocialHandleView extends StatelessWidget {
+  static const Icon _icon = Icon(Icons.open_in_new);
+
   @override
   Widget build(BuildContext context) {
+    final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
     return Material(
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.8,
@@ -14,29 +22,45 @@ class SocialHandleView extends StatelessWidget {
           shrinkWrap: true,
           scrollDirection: Axis.vertical,
           children: <Widget>[
-            ModalBottomSheetHeader(
-                title: AppLocalizations.of(context)!.connect_with_us),
-            // Instagram
+            ModalBottomSheetHeader(title: appLocalizations.connect_with_us),
             SmoothListTile(
-                text: AppLocalizations.of(context)!.instagram,
-                leadingWidget: const Icon(Icons.open_in_new),
+                text: appLocalizations.instagram,
+                leadingWidget: _icon,
                 onPressed: () => _launchUrl(
                     context, 'https://instagram.com/open.food.facts')),
-
-            //Twitter
             SmoothListTile(
-              text: AppLocalizations.of(context)!.twitter,
-              leadingWidget: const Icon(Icons.open_in_new),
+              text: appLocalizations.twitter,
+              leadingWidget: _icon,
               onPressed: () =>
                   _launchUrl(context, 'https://www.twitter.com/openfoodfacts'),
             ),
-
-            //Blog
             SmoothListTile(
-              text: AppLocalizations.of(context)!.blog,
-              leadingWidget: const Icon(Icons.open_in_new),
+              text: appLocalizations.blog,
+              leadingWidget: _icon,
               onPressed: () =>
                   _launchUrl(context, 'https://en.blog.openfoodfacts.org'),
+            ),
+            SmoothListTile(
+              text: appLocalizations.support_join_slack,
+              leadingWidget: _icon,
+              onPressed: () =>
+                  _launchUrl(context, 'https://slack.openfoodfacts.org/'),
+            ),
+            SmoothListTile(
+              text: appLocalizations.support_via_email,
+              leadingWidget: _icon,
+              onPressed: () async {
+                final PackageInfo packageInfo =
+                    await PackageInfo.fromPlatform();
+                // TODO(M123): Change subject name when we have a different app name
+                final Mailto mailtoLink = Mailto(
+                  to: <String>['contact@openfoodfacts.org'],
+                  subject: 'Smoothie help', // TODO(monsieurtanuki): localize
+                  body:
+                      'Version:${packageInfo.version}+${packageInfo.buildNumber} running on ${Platform.operatingSystem}(${Platform.operatingSystemVersion})',
+                );
+                await launch('$mailtoLink');
+              },
             ),
           ],
         ),
