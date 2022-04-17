c179973857466253e66efee6b66818bf08b18b3a
Edouard Marquez <g123k@users.noreply.github.com>
2022-04-17 13:28:17 +0200

    Uploading an image won't show a generic message, but a different one based on the type

--- packages/smooth_app/lib/helpers/picture_capture_helper.dart
+++ packages/smooth_app/lib/helpers/picture_capture_helper.dart
@@ -26,7 +26,12 @@ Future<bool> uploadCapturedPicture(
       ProductQuery.getUser(),
       image,
     ),
-    title: appLocalizations.uploading_image,
+    title: appLocalizations.uploading_image(
+      _imageFieldLabel(
+        appLocalizations,
+        imageField,
+      ),
+    ),
   );
   if (result == null || result.error != null || result.status != 'status ok') {
     await LoadingDialog.error(
@@ -37,6 +42,24 @@ Future<bool> uploadCapturedPicture(
   }
   await _updateContinuousScanModel(context, barcode);
   return true;
+}
+
+String _imageFieldLabel(
+  AppLocalizations appLocalizations,
+  ImageField field,
+) {
+  switch (field) {
+    case ImageField.FRONT:
+      return appLocalizations.uploading_image_type_front;
+    case ImageField.INGREDIENTS:
+      return appLocalizations.uploading_image_type_ingredients;
+    case ImageField.NUTRITION:
+      return appLocalizations.uploading_image_type_nutrition;
+    case ImageField.PACKAGING:
+      return appLocalizations.uploading_image_type_packaging;
+    case ImageField.OTHER:
+      return appLocalizations.uploading_image_type_other;
+  }
 }
 
 Future<void> _updateContinuousScanModel(
