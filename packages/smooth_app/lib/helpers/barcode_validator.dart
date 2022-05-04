3221c57ce6b3287ffed8500a6fdcb8ffd3105f03
Moises <moisesalonso01@gmail.com>
2022-05-04 14:55:05 -0300

    fix: add localizated terms to the snackbar

--- packages/smooth_app/lib/helpers/barcode_validator.dart
+++ packages/smooth_app/lib/helpers/barcode_validator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_ml_barcode_scanner/google_ml_barcode_scanner.dart'
     as MlKit;
 import 'package:qr_code_scanner/qr_code_scanner.dart' as QrCodeScanner;
@@ -39,10 +40,12 @@ void showInvalidBarcodeSnackbar(BuildContext context) {
 
   disallowShowInvalidBarcodeSnackbar();
 
+  final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
+
   ScaffoldMessenger.of(context)
       .showSnackBar(
-        const SnackBar(
-          content: Text('Yay! A SnackBar!'),
+        SnackBar(
+          content: Text(appLocalizations.not_valid_barcode),
         ),
       )
       .closed
