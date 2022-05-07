2004d88a7405d930763325f40f3c9c303f3bacf6
Pierre Slamich <pierre@openfoodfacts.org>
2022-05-07 20:15:32 +0200

    fix

--- packages/smooth_app/android/fastlane/envfiles/decrypt_secrets.sh
+++ packages/smooth_app/android/fastlane/envfiles/decrypt_secrets.sh
@@ -7,3 +7,7 @@ gpg -help
 gpg --quiet --batch --yes --decrypt --passphrase="$API_JSON_FILE_DECRYPTKEY" \
 --output ./api-4712693179220384697-162836-33ea08672303.json api-4712693179220384697-162836-33ea08672303.json.gpg
 ls
+echo "keystore decypher"
+
+gpg --quiet --batch --yes --decrypt --passphrase=\Q$STORE_JKS_DECRYPTKEY\E \
+--output ./keystore.jks keystore.jks.gpg
