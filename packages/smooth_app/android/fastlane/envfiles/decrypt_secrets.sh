3831dd0dd7ba6f591fffa21d25497d2bb4a984a8
Pierre Slamich <pierre@openfoodfacts.org>
2022-05-07 19:11:30 +0200

    Update decrypt_secrets.sh

--- packages/smooth_app/android/fastlane/envfiles/decrypt_secrets.sh
+++ packages/smooth_app/android/fastlane/envfiles/decrypt_secrets.sh
@@ -3,6 +3,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 echo "api decypher"
+gpg -help
 gpg --quiet --batch --yes --decrypt --passphrase="$API_JSON_FILE_DECRYPTKEY" \
 --output ./api-4712693179220384697-162836-33ea08672303.json api-4712693179220384697-162836-33ea08672303.json.gpg
 ls
