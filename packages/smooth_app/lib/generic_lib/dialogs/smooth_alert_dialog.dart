openfoodfacts/smooth-app

commit 818cd139796e364e69a9b464f8c4b5d89b0efb0e

feat: Harmonize app dialogs (#1978)
* Harmonize app dialogs

* Flat/text button for negative & neutral states
+ fix a missing image

* Remove unused SmoothActionButton implementation

* Fix build issues

packages/smooth_app/lib/generic_lib/dialogs/smooth_alert_dialog.dart
@@ -1,24 +1,29 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
+import 'package:smooth_app/generic_lib/buttons/smooth_simple_button.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 
+/// Custom Dialog to use in the app
 ///
-///	Open by calling
-///
-///showDialog<void>(
+/// ```dart
+/// showDialog<void>(
 ///        context: context,
 ///        builder: (BuildContext context) {
 ///          return SmoothAlertDialog(...)
-///	}
-///)
+///	       }
+/// )
+/// ```
 ///
+/// If only one action button is provided, simply pass a [positiveAction]
 
 class SmoothAlertDialog extends StatelessWidget {
   /// The most simple alert dialog: no fancy effects.
   const SmoothAlertDialog({
     this.title,
     required this.body,
-    required this.actions,
+    this.positiveAction,
+    this.neutralAction,
+    this.negativeAction,
   })  : close = false,
         maxHeight = null,
         _simpleMode = true;
@@ -29,21 +34,25 @@ class SmoothAlertDialog extends StatelessWidget {
     this.close = true,
     this.maxHeight,
     required this.body,
-    this.actions,
+    this.positiveAction,
+    this.neutralAction,
+    this.negativeAction,
   }) : _simpleMode = false;
 
   final String? title;
   final bool close;
   final double? maxHeight;
   final Widget body;
-  final List<SmoothActionButton>? actions;
+  final SmoothActionButton? positiveAction;
+  final SmoothActionButton? neutralAction;
+  final SmoothActionButton? negativeAction;
   final bool _simpleMode;
 
   @override
   Widget build(BuildContext context) {
     final Widget content = _buildContent(context);
     return AlertDialog(
-      elevation: 4,
+      elevation: 4.0,
       shape: const RoundedRectangleBorder(borderRadius: ROUNDED_BORDER_RADIUS),
       content: _simpleMode
           ? content
@@ -52,40 +61,13 @@ class SmoothAlertDialog extends StatelessWidget {
                   BoxConstraints(maxHeight: maxHeight ?? double.infinity * 0.5),
               child: content,
             ),
-      actions: actions == null
-          ? null
-          : <Widget>[
-              SizedBox(
-                height: 58,
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: actions!,
-                ),
-              ),
-            ],
+      actions: _buildActions(
+        context,
+        positiveAction: positiveAction,
+        neutralAction: neutralAction,
+        negativeAction: negativeAction,
+      ),
     );
-  }
-
-  Widget _buildCross(final bool isPlaceHolder, final BuildContext context) {
-    if (close) {
-      return Visibility(
-        maintainSize: true,
-        maintainAnimation: true,
-        maintainState: true,
-        visible: !isPlaceHolder,
-        child: InkWell(
-          child: const Icon(
-            Icons.close,
-            size: 29,
-          ),
-          onTap: () => Navigator.of(context, rootNavigator: true).pop('dialog'),
-        ),
-      );
-    } else {
-      return Container();
-    }
   }
 
   Widget _buildContent(final BuildContext context) => Column(
@@ -121,4 +103,190 @@ class SmoothAlertDialog extends StatelessWidget {
             Expanded(child: SingleChildScrollView(child: body)),
         ],
       );
+
+  Widget _buildCross(final bool isPlaceHolder, final BuildContext context) {
+    if (close) {
+      return Visibility(
+        maintainSize: true,
+        maintainAnimation: true,
+        maintainState: true,
+        visible: !isPlaceHolder,
+        child: InkWell(
+          child: const Icon(
+            Icons.close,
+            size: 29.0,
+          ),
+          onTap: () => Navigator.of(context, rootNavigator: true).pop('dialog'),
+        ),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+}
+
+class SmoothActionButtonsBar extends StatelessWidget {
+  const SmoothActionButtonsBar({
+    this.positiveAction,
+    this.neutralAction,
+    this.negativeAction,
+    Key? key,
+  })  : assert(
+            positiveAction != null ||
+                neutralAction != null ||
+                negativeAction != null,
+            'At least one action must be passed!'),
+        super(key: key);
+
+  const SmoothActionButtonsBar.single({
+    required SmoothActionButton action,
+    Key? key,
+  }) : this(
+          positiveAction: action,
+          key: key,
+        );
+
+  final SmoothActionButton? positiveAction;
+  final SmoothActionButton? neutralAction;
+  final SmoothActionButton? negativeAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: _buildActions(
+        context,
+        positiveAction: positiveAction,
+        neutralAction: neutralAction,
+        negativeAction: negativeAction,
+      )!,
+    );
+  }
+}
+
+/// Generates Actions buttons with:
+/// In LTR mode: Negative - Neutral - Positive
+/// In RTL mode: Positive - Neutral - Negative
+List<Widget>? _buildActions(
+  BuildContext context, {
+  SmoothActionButton? positiveAction,
+  SmoothActionButton? neutralAction,
+  SmoothActionButton? negativeAction,
+}) {
+  if (positiveAction == null &&
+      neutralAction == null &&
+      negativeAction == null) {
+    return null;
+  }
+
+  final Size size = Size(
+    MediaQuery.of(context).size.width / 3,
+    36.0,
+  );
+
+  final List<Widget> actions = <Widget>[
+    if (negativeAction != null)
+      SizedBox.fromSize(
+        size: size,
+        child: _SmoothActionFlatButton(
+          buttonData: negativeAction,
+        ),
+      ),
+    if (neutralAction != null)
+      SizedBox.fromSize(
+        size: size,
+        child: _SmoothActionFlatButton(
+          buttonData: neutralAction,
+        ),
+      ),
+    if (positiveAction != null)
+      SizedBox.fromSize(
+        size: size,
+        child: _SmoothActionElevatedButton(
+          buttonData: positiveAction,
+        ),
+      ),
+  ];
+
+  if (Directionality.of(context) == TextDirection.rtl) {
+    return actions.reversed.toList(growable: false);
+  } else {
+    return actions;
+  }
+}
+
+class SmoothActionButton {
+  SmoothActionButton({
+    required this.text,
+    required this.onPressed,
+    this.minWidth = 15,
+    this.height = 20,
+  }) : assert(text.isNotEmpty);
+
+  final String text;
+  final VoidCallback? onPressed;
+  final double minWidth;
+  final double height;
+}
+
+class _SmoothActionElevatedButton extends StatelessWidget {
+  const _SmoothActionElevatedButton({
+    required this.buttonData,
+  });
+
+  final SmoothActionButton buttonData;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return SmoothSimpleButton(
+      onPressed: buttonData.onPressed,
+      height: buttonData.height,
+      minWidth: buttonData.minWidth,
+      child: AutoSizeText(
+        buttonData.text.toUpperCase(),
+        style: themeData.textTheme.bodyText2!
+            .copyWith(color: themeData.colorScheme.onPrimary),
+        maxLines: 1,
+      ),
+    );
+  }
+}
+
+class _SmoothActionFlatButton extends StatelessWidget {
+  const _SmoothActionFlatButton({
+    required this.buttonData,
+  });
+
+  final SmoothActionButton buttonData;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+
+    return Theme(
+      data: themeData.copyWith(
+        buttonTheme: const ButtonThemeData(
+          shape: RoundedRectangleBorder(
+            borderRadius: ROUNDED_BORDER_RADIUS,
+          ),
+        ),
+      ),
+      child: TextButton(
+        onPressed: buttonData.onPressed,
+        style: TextButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: ROUNDED_BORDER_RADIUS,
+          ),
+          textStyle: themeData.textTheme.bodyText2!
+              .copyWith(color: themeData.colorScheme.onPrimary),
+        ),
+        child: AutoSizeText(
+          buttonData.text.toUpperCase(),
+          maxLines: 1,
+        ),
+      ),
+    );
+  }
 }

packages/smooth_app/lib/pages/preferences/user_preferences_contribute.dart
@@ -5,7 +5,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:http/http.dart' as http;
 import 'package:smooth_app/data_models/github_contributors_model.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/pages/preferences/abstract_user_preferences.dart';
@@ -89,15 +88,13 @@ class UserPreferencesContribute extends AbstractUserPreferences {
                 ),
               ],
             ),
-            actions: <SmoothActionButton>[
-              SmoothActionButton(
-                onPressed: () {
-                  Navigator.of(context, rootNavigator: true).pop('dialog');
-                },
-                text: appLocalizations.okay,
-                minWidth: 100,
-              ),
-            ],
+            positiveAction: SmoothActionButton(
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop('dialog');
+              },
+              text: appLocalizations.okay,
+              minWidth: 100,
+            ),
           );
         },
       );
@@ -140,13 +137,11 @@ class UserPreferencesContribute extends AbstractUserPreferences {
                 )
               ],
             ),
-            actions: <SmoothActionButton>[
-              SmoothActionButton(
-                onPressed: () => Navigator.pop(context),
-                text: appLocalizations.okay,
-                minWidth: 100,
-              ),
-            ],
+            positiveAction: SmoothActionButton(
+              onPressed: () => Navigator.pop(context),
+              text: appLocalizations.okay,
+              minWidth: 100,
+            ),
           );
         },
       );
@@ -169,14 +164,12 @@ class UserPreferencesContribute extends AbstractUserPreferences {
                 ),
               ],
             ),
-            actions: <SmoothActionButton>[
-              SmoothActionButton(
-                onPressed: () => LaunchUrlHelper.launchURL(
-                    'https://translate.openfoodfacts.org/', false),
-                text: appLocalizations.contribute_translate_link_text,
-                minWidth: 200,
-              ),
-            ],
+            positiveAction: SmoothActionButton(
+              onPressed: () => LaunchUrlHelper.launchURL(
+                  'https://translate.openfoodfacts.org/', false),
+              text: appLocalizations.contribute_translate_link_text,
+              minWidth: 200,
+            ),
           );
         },
       );
@@ -232,14 +225,12 @@ class UserPreferencesContribute extends AbstractUserPreferences {
                 return const CircularProgressIndicator();
               },
             ),
-            actions: <SmoothActionButton>[
-              SmoothActionButton(
-                onPressed: () => LaunchUrlHelper.launchURL(
-                    'https://github.com/openfoodfacts/smooth-app', false),
-                text: AppLocalizations.of(context).contribute,
-                minWidth: 200,
-              ),
-            ],
+            positiveAction: SmoothActionButton(
+              onPressed: () => LaunchUrlHelper.launchURL(
+                  'https://github.com/openfoodfacts/smooth-app', false),
+              text: AppLocalizations.of(context).contribute,
+              minWidth: 200,
+            ),
           );
         },
       );

packages/smooth_app/lib/generic_lib/buttons/smooth_action_button.dart
@@ -1,33 +1,0 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/material.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_simple_button.dart';
-
-class SmoothActionButton extends StatelessWidget {
-  const SmoothActionButton({
-    required this.text,
-    required this.onPressed,
-    this.minWidth = 15,
-    this.height = 20,
-  });
-
-  final String text;
-  final VoidCallback? onPressed;
-  final double minWidth;
-  final double height;
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    return SmoothSimpleButton(
-      onPressed: onPressed,
-      height: height,
-      minWidth: minWidth,
-      child: AutoSizeText(
-        text,
-        style: themeData.textTheme.bodyText2!
-            .copyWith(color: themeData.colorScheme.onPrimary),
-        maxLines: 1,
-      ),
-    );
-  }
-}

packages/smooth_app/lib/generic_lib/loading_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 
 /// Dialog with a stop button, while a future is running.
@@ -47,12 +46,10 @@ class LoadingDialog<T> {
                 title ?? appLocalizations.loading_dialog_default_error_message,
               ),
             ),
-            actions: <SmoothActionButton>[
-              SmoothActionButton(
-                text: appLocalizations.close,
-                onPressed: () => Navigator.maybePop(context),
-              ),
-            ],
+            positiveAction: SmoothActionButton(
+              text: appLocalizations.close,
+              onPressed: () => Navigator.maybePop(context),
+            ),
           );
         },
       );
@@ -111,12 +108,10 @@ class LoadingDialog<T> {
           );
         },
       ),
-      actions: <SmoothActionButton>[
-        SmoothActionButton(
-          text: appLocalizations.stop,
-          onPressed: () => _popDialog(context, null),
-        ),
-      ],
+      positiveAction: SmoothActionButton(
+        text: appLocalizations.stop,
+        onPressed: () => _popDialog(context, null),
+      ),
     );
   }
 }

packages/smooth_app/lib/pages/onboarding/country_selector.dart
@@ -4,7 +4,6 @@ import 'package:iso_countries/iso_countries.dart';
 import 'package:openfoodfacts/utils/CountryHelper.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/database/product_query.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_text_form_field.dart';
 
@@ -132,12 +131,10 @@ class _CountrySelectorState extends State<CountrySelector> {
                           ],
                         ),
                       ),
-                      actions: <SmoothActionButton>[
-                        SmoothActionButton(
-                          onPressed: () => Navigator.pop(context),
-                          text: appLocalizations.cancel,
-                        ),
-                      ],
+                      positiveAction: SmoothActionButton(
+                        onPressed: () => Navigator.pop(context),
+                        text: appLocalizations.cancel,
+                      ),
                     );
                   },
                 );

packages/smooth_app/lib/pages/preferences/user_preferences_dev_mode.dart
@@ -10,7 +10,6 @@ import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/database/dao_product_list.dart';
 import 'package:smooth_app/database/local_database.dart';
 import 'package:smooth_app/database/product_query.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_app/helpers/data_importer/product_list_import_export.dart';
 import 'package:smooth_app/helpers/data_importer/smooth_app_data_importer.dart';
@@ -410,14 +409,12 @@ class UserPreferencesDevMode extends AbstractUserPreferences {
               onColorChanged: changeColor,
             ),
           ),
-          actions: <SmoothActionButton>[
-            SmoothActionButton(
-              text: 'Got it',
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-            ),
-          ],
+          positiveAction: SmoothActionButton(
+            text: 'Got it',
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
         );
       },
     );

packages/smooth_app/lib/pages/preferences/user_preferences_dialog_editor.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 
 /// Generic value editor
@@ -80,17 +79,15 @@ class _UserPreferencesEditValueDialogState<T>
         textAlign: widget.textAlignment ?? TextAlign.start,
         keyboardType: widget.keyboardType ?? TextInputType.text,
       ),
-      actions: <SmoothActionButton>[
-        SmoothActionButton(
-          text: appLocalizations.cancel,
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        SmoothActionButton(
-          text: appLocalizations.okay,
-          onPressed:
-              _isValid ? () => Navigator.of(context).pop(_currentValue) : null,
-        )
-      ],
+      negativeAction: SmoothActionButton(
+        text: appLocalizations.cancel,
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      positiveAction: SmoothActionButton(
+        text: appLocalizations.okay,
+        onPressed:
+            _isValid ? () => Navigator.of(context).pop(_currentValue) : null,
+      ),
     );
   }
 }

packages/smooth_app/lib/pages/preferences/user_preferences_faq.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
-import 'package:smooth_app/generic_lib/buttons/smooth_action_button.dart';
 import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/pages/preferences/abstract_user_preferences.dart';
@@ -67,79 +67,81 @@ class UserPreferencesFaq extends AbstractUserPreferences {
         icon: icon ?? const Icon(Icons.open_in_new),
       );
 
+  static const String _iconAssetPath = 'assets/app/release_icon.svg';
+
   Future<void> _about() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     showDialog<void>(
       context: context,
-      builder: (BuildContext context) => SmoothAlertDialog(
-        body: Column(
-          children: <Widget>[
-            ListTile(
-              leading: Image.asset('assets/app/smoothie-icon.1200x1200.png'),
-              title: FittedBox(
-                child: Text(
-                  packageInfo.appName,
-                  style: themeData.textTheme.headline1,
+      builder: (BuildContext context) {
+        return SmoothAlertDialog(
+          body: Column(
+            children: <Widget>[
+              ListTile(
+                leading: SvgPicture.asset(
+                  _iconAssetPath,
                 ),
-              ),
-              subtitle: Text(
-                '${packageInfo.version}+${packageInfo.buildNumber}',
-                style: themeData.textTheme.subtitle2,
-              ),
-            ),
-            Divider(color: themeData.colorScheme.onSurface),
-            const SizedBox(height: 20),
-            Text(appLocalizations.whatIsOff),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextButton(
-                  onPressed: () => LaunchUrlHelper.launchURL(
-                      'https://openfoodfacts.org/who-we-are', true),
+                title: FittedBox(
                   child: Text(
-                    appLocalizations.learnMore,
-                    style: const TextStyle(
-                      color: Colors.blue,
-                    ),
+                    packageInfo.appName,
+                    style: themeData.textTheme.headline1,
                   ),
                 ),
-                TextButton(
-                  onPressed: () => LaunchUrlHelper.launchURL(
-                      'https://openfoodfacts.org/terms-of-use', true),
-                  child: Text(
-                    appLocalizations.termsOfUse,
-                    style: const TextStyle(
-                      color: Colors.blue,
+                subtitle: Text(
+                  '${packageInfo.version}+${packageInfo.buildNumber}',
+                  style: themeData.textTheme.subtitle2,
+                ),
+              ),
+              Divider(color: themeData.colorScheme.onSurface),
+              const SizedBox(height: 20),
+              Text(appLocalizations.whatIsOff),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () => LaunchUrlHelper.launchURL(
+                        'https://openfoodfacts.org/who-we-are', true),
+                    child: Text(
+                      appLocalizations.learnMore,
+                      style: const TextStyle(
+                        color: Colors.blue,
+                      ),
                     ),
                   ),
-                )
-              ],
-            )
-          ],
-        ),
-        actions: <SmoothActionButton>[
-          SmoothActionButton(
+                  TextButton(
+                    onPressed: () => LaunchUrlHelper.launchURL(
+                        'https://openfoodfacts.org/terms-of-use', true),
+                    child: Text(
+                      appLocalizations.termsOfUse,
+                      style: const TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          positiveAction: SmoothActionButton(
             onPressed: () async {
               showLicensePage(
                 context: context,
                 applicationName: packageInfo.appName,
                 applicationVersion: packageInfo.version,
-                applicationIcon: Image.asset(
-                  'assets/app/smoothie-icon.1200x1200.png',
+                applicationIcon: SvgPicture.asset(
+                  _iconAssetPath,
                   height: MediaQuery.of(context).size.height * 0.1,
                 ),
               );
             },
             text: appLocalizations.licenses,
-            minWidth: 100,
           ),
-          SmoothActionButton(
+          negativeAction: SmoothActionButton(
             onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
             text: appLocalizations.okay,
-            minWidth: 100,
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }

commit 7bf5decfc52bbfcc70f9b34da04099cd00ddc49d

Fix a visual issue introduced in #1978

packages/smooth_app/lib/generic_lib/dialogs/smooth_alert_dialog.dart
@@ -174,14 +174,16 @@ List<Widget>? _buildActions(
   SmoothActionButton? neutralAction,
   SmoothActionButton? negativeAction,
 }) {
-  if (positiveAction == null &&
-      neutralAction == null &&
-      negativeAction == null) {
+  final int count = (positiveAction != null ? 1 : 0) +
+      (neutralAction != null ? 1 : 0) +
+      (negativeAction != null ? 1 : 0);
+
+  if (count == 0) {
     return null;
   }
 
   final Size size = Size(
-    MediaQuery.of(context).size.width / 3,
+    MediaQuery.of(context).size.width / (count == 1 ? 1.5 : count),
     36.0,
   );
 

packages/smooth_app/lib/pages/preferences/user_preferences_contribute.dart
@@ -151,9 +151,8 @@ class UserPreferencesContribute extends AbstractUserPreferences {
         builder: (BuildContext context) {
           final AppLocalizations appLocalizations =
               AppLocalizations.of(context);
-          return SmoothAlertDialog.advanced(
+          return SmoothAlertDialog(
             title: appLocalizations.contribute_translate_header,
-            maxHeight: MediaQuery.of(context).size.height * 0.25,
             body: Column(
               children: <Widget>[
                 Text(
@@ -168,7 +167,6 @@ class UserPreferencesContribute extends AbstractUserPreferences {
               onPressed: () => LaunchUrlHelper.launchURL(
                   'https://translate.openfoodfacts.org/', false),
               text: appLocalizations.contribute_translate_link_text,
-              minWidth: 200,
             ),
           );
         },