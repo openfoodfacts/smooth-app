ffa9d1889342a3746ce6563c2460dbfa52940d49
Marvin M <39344769+M123-dev@users.noreply.github.com>
2022-05-25 15:13:47 +0200

    Update packages/smooth_app/lib/pages/product/common/product_list_page.dart
    Co-authored-by: monsieurtanuki <fabrice_fontaine@hotmail.com>

--- packages/smooth_app/lib/pages/product/common/product_list_page.dart
+++ packages/smooth_app/lib/pages/product/common/product_list_page.dart
@@ -39,7 +39,7 @@ class _ProductListPageState extends State<ProductListPage>
   static const String _popupActionRename = 'rename';
 
   @override
-  String get traceName => 'Opened list page ${widget.productList.listType}';
+  String get traceName => 'Opened list page ${widget.productList.listType} / ${widget.productList.getParametersKey()}';
 
   @override
   String get traceTitle => 'list_page';
