6d3db5e8151a941be33bd71564f74128bead5133
Marvin M <39344769+M123-dev@users.noreply.github.com>
2022-05-26 17:59:11 +0200

    Update score_card.dart

--- packages/smooth_app/lib/cards/data_cards/score_card.dart
+++ packages/smooth_app/lib/cards/data_cards/score_card.dart
@@ -3,9 +3,8 @@ import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/generic_lib/svg_icon_chip.dart';
 import 'package:smooth_app/helpers/score_card_helper.dart';
 import 'package:smooth_app/helpers/ui_helpers.dart';
+import 'package:smooth_app/themes/constant_icons.dart';
 import 'package:smooth_app/themes/smooth_theme.dart';
-
-import '../../themes/constant_icons.dart';
 
 enum CardEvaluation {
   UNKNOWN,
