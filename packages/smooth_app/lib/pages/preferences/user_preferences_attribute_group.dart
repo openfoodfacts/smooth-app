e55c5ee912cbc0debc9e66713f15d80135c16cfc
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-05-27 11:08:42 +0200

    feat: #1918 #1934 - simplified user preferences food (#1950)
    * feat: #1918 #1934 - simplified user preferences food
    
    Deleted file:
    * `abstract_collapsible_user_preferences.dart`: not really relevant anymore and source of confusion - the code was refactored to its only child, `UserPreferencesAttributeGroup`
    
    Impacted files:
    * `abstract_user_preferences.dart`: refactored without the "collapsed" option
    * `Podfile.lock`: wtf
    * `preferences_page.dart`: simplified the display (that was Scaffold^2)
    * `project.pbxproj`: wtf
    * `pubspec.lock`: wtf
    * `user_preferences.dart`: added a setter/getter about the unique active attribute group
    * `user_preferences_attribute_group.dart`: refactored around the unique active attribute group - no action and no icon for the only expanded group
    * `user_preferences_food.dart`: removed doubled description and refresh button from onboarding
    * `user_preferences_profile.dart`: unrelated minor refactoring
    
    * feat/#1918 - changed collapsed/expanded arrows for attribute groups
    
    Deleted file:
    * `smooth_animated_collapse_arrow.dart`
    
    Impacted file:
    * `user_preferences_attribute_group.dart`: changed collapsed/expanded arrows
    
    * Update packages/smooth_app/ios/Podfile.lock
    
    Co-authored-by: Pierre Slamich <pierre.slamich@gmail.com>
    
    * Whatever works for that bloody github...
    
    * Whatever works for that bloody github...
    
    Co-authored-by: Pierre Slamich <pierre.slamich@gmail.com>

--- packages/smooth_app/lib/pages/preferences/user_preferences_attribute_group.dart
+++ packages/smooth_app/lib/pages/preferences/user_preferences_attribute_group.dart
@@ -5,13 +5,14 @@ import 'package:openfoodfacts/model/AttributeGroup.dart';
 import 'package:smooth_app/data_models/product_preferences.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
-import 'package:smooth_app/pages/preferences/abstract_collapsible_user_preferences.dart';
+import 'package:smooth_app/pages/preferences/abstract_user_preferences.dart';
+import 'package:smooth_app/pages/preferences/user_preferences_list_tile.dart';
 import 'package:smooth_app/pages/preferences/user_preferences_page.dart';
 import 'package:smooth_app/themes/smooth_theme.dart';
 import 'package:smooth_app/widgets/attribute_button.dart';
 
 /// Collapsed/expanded display of an attribute group for the preferences page.
-class UserPreferencesAttributeGroup extends AbstractCollapsibleUserPreferences {
+class UserPreferencesAttributeGroup extends AbstractUserPreferences {
   UserPreferencesAttributeGroup({
     required this.productPreferences,
     required this.group,
@@ -33,9 +34,6 @@ class UserPreferencesAttributeGroup extends AbstractCollapsibleUserPreferences {
 
   @override
   PreferencePageType? getPreferencePageType() => null;
-
-  @override
-  String getPreferenceFlagKey() => 'attribute:${group.id}';
 
   @override
   String getTitleString() => group.name ?? appLocalizations.unknown;
@@ -85,5 +83,37 @@ class UserPreferencesAttributeGroup extends AbstractCollapsibleUserPreferences {
       result.add(AttributeButton(attribute, productPreferences));
     }
     return result;
+  }
+
+  @override
+  Widget getHeader() =>
+      _isCollapsed() ? super.getHeader() : getHeaderHelper(false);
+
+  @override
+  Widget getHeaderHelper(final bool? collapsed) => UserPreferencesListTile(
+        title: getTitle(),
+        subtitle: getSubtitle(),
+        isCompactTitle: true,
+        icon: collapsed!
+            ? const Icon(Icons.keyboard_arrow_right)
+            : const Icon(Icons.keyboard_arrow_down),
+      );
+
+  bool _isCollapsed() => userPreferences.activeAttributeGroup != group.id;
+
+  @override
+  List<Widget> getContent({
+    final bool withHeader = true,
+    final bool withBody = true,
+  }) =>
+      super.getContent(
+        withHeader: withHeader,
+        withBody: !_isCollapsed(),
+      );
+
+  @override
+  Future<void> runHeaderAction() async {
+    await userPreferences.setActiveAttributeGroup(group.id!);
+    setState(() {});
   }
 }
