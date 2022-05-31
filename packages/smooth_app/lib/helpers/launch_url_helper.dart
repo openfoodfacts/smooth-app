d847638cc51a180915d88c40efdaf2ff24c64f68
Edouard Marquez <g123k@users.noreply.github.com>
2022-06-01 00:36:07 +0200

    On Android, force external links to be opened in a browser

--- packages/smooth_app/lib/helpers/launch_url_helper.dart
+++ packages/smooth_app/lib/helpers/launch_url_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:smooth_app/helpers/analytics_helper.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -18,7 +20,12 @@ class LaunchUrlHelper {
     AnalyticsHelper.trackOpenLink(url: url);
 
     if (await canLaunchUrl(Uri.parse(url))) {
-      await launchUrl(Uri.parse(url));
+      await launchUrl(
+        Uri.parse(url),
+        mode: Platform.isAndroid
+            ? LaunchMode.externalApplication
+            : LaunchMode.platformDefault,
+      );
     } else {
       throw 'Could not launch $url';
     }
