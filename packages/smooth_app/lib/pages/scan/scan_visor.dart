98c6fa1fe79c3ec19466bd75d8e8f25e33dac7e4
Edouard Marquez <g123k@users.noreply.github.com>
2022-06-02 08:11:27 +0200

    feat: New visor UI (#2122)
    * New visor UI
    
    * Remove an unused variable

--- packages/smooth_app/lib/pages/scan/scan_visor.dart
+++ packages/smooth_app/lib/pages/scan/scan_visor.dart
@@ -1,26 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_app/generic_lib/widgets/smooth_view_finder.dart';
-import 'package:smooth_app/pages/scan/scanner_overlay.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ScannerVisorWidget extends StatelessWidget {
   const ScannerVisorWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-
-    final Size scannerSize = Size(
-      screenSize.width * ScannerOverlay.scannerWidthPct,
-      getVisorHeight(screenSize),
-    );
-
-    return SmoothViewFinder(
-      boxSize: scannerSize,
-      lineLength: screenSize.width * 0.8,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: 150.0,
+      child: CustomPaint(
+        painter: _Painter(),
+        child: Center(
+          child: SvgPicture.asset(
+            'assets/icons/visor_icon.svg',
+            width: 35.0,
+            height: 32.0,
+          ),
+        ),
+      ),
     );
   }
+}
 
-  static double getVisorHeight(Size size) {
-    return size.width * ScannerOverlay.scannerHeightPct;
+class _Painter extends CustomPainter {
+  _Painter();
+
+  static const double _fullCornerSize = 31.0;
+  static const double _halfCornerSize = _fullCornerSize / 2;
+  static const Radius _borderRadius = Radius.circular(_halfCornerSize);
+
+  final Paint _paint = Paint()
+    ..strokeWidth = 3.0
+    ..color = Colors.white
+    ..style = PaintingStyle.stroke;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = Rect.fromLTRB(0.0, 0.0, size.width, size.height);
+
+    // Draw corners
+    canvas.drawPath(
+        Path()
+          // Top left
+          ..moveTo(rect.left, rect.top + _fullCornerSize)
+          ..lineTo(rect.left, rect.top + _halfCornerSize)
+          ..arcToPoint(
+            Offset(rect.left + _halfCornerSize, rect.top),
+            radius: _borderRadius,
+          )
+          ..lineTo(rect.left + _fullCornerSize, rect.top)
+
+          // Top right
+          ..moveTo(rect.right - _fullCornerSize, rect.top)
+          ..lineTo(rect.right - _halfCornerSize, rect.top)
+          ..arcToPoint(
+            Offset(rect.right, _halfCornerSize),
+            radius: _borderRadius,
+          )
+          ..lineTo(rect.right, rect.top + _fullCornerSize)
+
+          // Bottom right
+          ..moveTo(rect.right, rect.bottom - _fullCornerSize)
+          ..lineTo(rect.right, rect.bottom - _halfCornerSize)
+          ..arcToPoint(
+            Offset(rect.right - _halfCornerSize, rect.bottom),
+            radius: _borderRadius,
+          )
+          ..lineTo(rect.right - _fullCornerSize, rect.bottom)
+
+          // Bottom left
+          ..moveTo(rect.left + _fullCornerSize, rect.bottom)
+          ..lineTo(rect.left + _halfCornerSize, rect.bottom)
+          ..arcToPoint(
+            Offset(rect.left, rect.bottom - _halfCornerSize),
+            radius: _borderRadius,
+          )
+          ..lineTo(rect.left, rect.bottom - _fullCornerSize),
+        _paint);
   }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
