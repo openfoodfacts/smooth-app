37a8bac35ef3f6c63a7f66725355843b0dfe052e
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-06-09 20:10:13 +0200

    feat: #2001 - consistency for left/right button positions during onboarding
    New file:
    * `onboarding_bottom_bar.dart`: `Widget`s for onboarding bottom bar (bar, button, icon)
    
    Impacted files:
    * `consent_analytics_page.dart`: refactored around `onboarding_bottom_bar.dart`
    * `next_button.dart`: refactored around `onboarding_bottom_bar.dart`

--- packages/smooth_app/lib/pages/onboarding/next_button.dart
+++ packages/smooth_app/lib/pages/onboarding/next_button.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:smooth_app/data_models/onboarding_loader.dart';
 import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/database/local_database.dart';
-import 'package:smooth_app/generic_lib/design_constants.dart';
+import 'package:smooth_app/pages/onboarding/onboarding_bottom_bar.dart';
 import 'package:smooth_app/pages/onboarding/onboarding_flow_navigator.dart';
 import 'package:smooth_app/themes/constant_icons.dart';
 
@@ -25,88 +25,40 @@ class NextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
     final UserPreferences userPreferences = context.watch<UserPreferences>();
     final LocalDatabase localDatabase = context.watch<LocalDatabase>();
-    // Side padding is 8% of total width.
-    final double sidePadding = screenSize.width * .08;
     final OnboardingFlowNavigator navigator =
         OnboardingFlowNavigator(userPreferences);
     final OnboardingPage previousPage =
         OnboardingFlowNavigator.getPrevPage(currentPage);
-    final bool hasPrevious = previousPage != OnboardingPage.NOT_STARTED;
-    return Column(
-      children: <Widget>[
-        Container(
-          height: SMALL_SPACE,
-          width: screenSize.width,
-          color: backgroundColor == null ? null : LIGHT_GREY_COLOR,
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(
-            vertical: VERY_LARGE_SPACE,
-            horizontal: sidePadding,
-          ),
-          width: screenSize.width,
-          color: backgroundColor,
-          child: Row(
-            mainAxisAlignment: hasPrevious
-                ? MainAxisAlignment.spaceBetween
-                : MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              if (hasPrevious)
-                Padding(
-                  padding: const EdgeInsets.only(right: LARGE_SPACE),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(12),
-                      primary: Colors.white,
-                      onPrimary: Colors.black,
-                    ),
-                    onPressed: () => navigator.navigateToPage(
-                      context,
-                      previousPage,
-                    ),
-                    child: Icon(ConstantIcons.instance.getBackIcon()),
-                  ),
-                ),
-              ConstrainedBox(
-                constraints:
-                    const BoxConstraints.tightFor(height: MINIMUM_TARGET_SIZE),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    await OnboardingLoader(localDatabase)
-                        .runAtNextTime(currentPage, context);
-                    //ignore: use_build_context_synchronously
-                    navigator.navigateToPage(
-                      context,
-                      OnboardingFlowNavigator.getNextPage(currentPage),
-                    );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                    ),
-                  ),
-                  child: Text(
-                    appLocalizations.next_label,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline3
-                        ?.copyWith(color: Colors.white),
-                  ),
-                ),
+    return OnboardingBottomBar(
+      leftButton: previousPage == OnboardingPage.NOT_STARTED
+          ? null
+          : OnboardingBottomIcon(
+              onPressed: () => navigator.navigateToPage(
+                context,
+                previousPage,
               ),
-            ],
-          ),
-        ),
-      ],
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              icon: ConstantIcons.instance.getBackIcon(),
+            ),
+      rightButton: OnboardingBottomButton(
+        onPressed: () async {
+          await OnboardingLoader(localDatabase)
+              .runAtNextTime(currentPage, context);
+          //ignore: use_build_context_synchronously
+          navigator.navigateToPage(
+            context,
+            OnboardingFlowNavigator.getNextPage(currentPage),
+          );
+        },
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        label: appLocalizations.next_label,
+      ),
+      backgroundColor: backgroundColor,
     );
   }
 }
