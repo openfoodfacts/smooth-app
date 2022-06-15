5187f8856c89024feca8b04171547542bcc9cc15
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-06-15 18:40:27 +0200

    feat: #2280 - DaoProduct's just migrated to sqflite
    New files:
    * `abstract_sql_dao.dart`: DAO abstraction for SQL.
    * `bulk_deletable.dart`: Interface for bulk database deletes.
    * `bulk_insertable.dart`: Interface for bulk database inserts.
    * `bulk_manager.dart`: Manager for bulk database inserts and deletes.
    * `dao_hive_product.dart`: moved code from the previous `hive` version of `DaoProduct`, renamed here `DaoHiveProduct`
    
    Impacted files:
    * `dao_product.dart`: `sqflite` version of `DaoProduct`, that is from now on the only one to be used
    * `local_database.dart`: added `sqflite` and its one table so far, also named `DaoProduct` in order to minimize the changes everywhere else (the old `DaoProduct` being renamed `DaoHiveProduct`); migrated the product data from `hive` to `sqflite`
    * `Podfile.lock`: wtf
    * `pubspec.lock`: wtf
    * `data_importer/pubspec.yaml`: updated the `sqflite` dependency to the same as in `smooth_app`
    * `smooth_app/pubspec.yaml`: added a dependency to `sqflite`

---
+++ packages/smooth_app/lib/database/dao_hive_product.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:hive/hive.dart';
+import 'package:openfoodfacts/model/Product.dart';
+import 'package:smooth_app/database/abstract_dao.dart';
+import 'package:smooth_app/database/local_database.dart';
+
+/// Hive type adapter for [Product]
+class _ProductAdapter extends TypeAdapter<Product> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Product read(BinaryReader reader) =>
+      Product.fromJson(jsonDecode(reader.readString()) as Map<String, dynamic>);
+
+  @override
+  void write(BinaryWriter writer, Product obj) =>
+      writer.writeString(jsonEncode(obj.toJson()));
+}
+
+/// Where we store the products as "barcode => product".
+@Deprecated('use [DaoProduct] instead')
+class DaoHiveProduct extends AbstractDao {
+  @Deprecated('use [DaoProduct] instead')
+  DaoHiveProduct(final LocalDatabase localDatabase) : super(localDatabase);
+
+  static const String _hiveBoxName = 'products';
+
+  @override
+  Future<void> init() async => Hive.openLazyBox<Product>(_hiveBoxName);
+
+  @override
+  void registerAdapter() => Hive.registerAdapter(_ProductAdapter());
+
+  LazyBox<Product> _getBox() => Hive.lazyBox<Product>(_hiveBoxName);
+
+  Future<Product?> get(final String barcode) async => _getBox().get(barcode);
+
+  Future<Map<String, Product>> getAll(final List<String> barcodes) async {
+    final LazyBox<Product> box = _getBox();
+    final Map<String, Product> result = <String, Product>{};
+    for (final String barcode in barcodes) {
+      final Product? product = await box.get(barcode);
+      if (product != null) {
+        result[barcode] = product;
+      }
+    }
+    return result;
+  }
+
+  Future<void> put(final Product product) async => putAll(<Product>[product]);
+
+  Future<void> putAll(final Iterable<Product> products) async {
+    final Map<String, Product> upserts = <String, Product>{};
+    for (final Product product in products) {
+      upserts[product.barcode!] = product;
+    }
+    await _getBox().putAll(upserts);
+  }
+
+  Future<List<String>> getAllKeys() async {
+    final LazyBox<Product> box = _getBox();
+    final List<String> result = <String>[];
+    for (final dynamic key in box.keys) {
+      result.add(key.toString());
+    }
+    return result;
+  }
+
+  // Just for the migration
+  Future<void> deleteAll(final List<String> barcodes) async {
+    final LazyBox<Product> box = _getBox();
+    await box.deleteAll(barcodes);
+  }
+}
