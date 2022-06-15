41ad027f9b219f1359782d33710ce77537e3ff87
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-06-15 07:21:04 +0200

    feat: #2001 - consistency for left/right button positions during onboarding (#2235)
    New file:
    * `onboarding_bottom_bar.dart`: `Widget`s for onboarding bottom bar (bar, button, icon)
    
    Impacted file:
    * `consent_analytics_page.dart`: refactored around `onboarding_bottom_bar.dart`
    * `next_button.dart`: changed the left/right button positions; refactored around `onboarding_bottom_bar.dart`

---
+++ packages/smooth_app/lib/pages/onboarding/onboarding_bottom_bar.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
+
+/// Bottom Bar during onboarding. Typical use case: previous/next buttons.
+class OnboardingBottomBar extends StatelessWidget {
+  const OnboardingBottomBar({
+    required this.rightButton,
+    required this.backgroundColor,
+    this.leftButton,
+  }) : super(key: const Key('next'));
+
+  final Widget rightButton;
+  final Widget? leftButton;
+
+  /// Color of the background where we put the buttons.
+  ///
+  /// If null, transparent background and no visible divider.
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    // Side padding is 8% of total width.
+    final double sidePadding = screenSize.width * .08;
+    final bool hasPrevious = leftButton != null;
+    return Column(
+      children: <Widget>[
+        Container(
+          height: SMALL_SPACE,
+          width: screenSize.width,
+          color: backgroundColor == null ? null : LIGHT_GREY_COLOR,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+            vertical: VERY_LARGE_SPACE,
+            horizontal: sidePadding,
+          ),
+          width: screenSize.width,
+          color: backgroundColor,
+          child: Row(
+            mainAxisAlignment: hasPrevious
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              if (leftButton != null) leftButton!,
+              rightButton,
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+/// Onboarding Bottom Button, e.g. "next" or "previous".
+class OnboardingBottomButton extends StatelessWidget {
+  const OnboardingBottomButton({
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.foregroundColor,
+    required this.label,
+  });
+
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color foregroundColor;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) => ConstrainedBox(
+        constraints: const BoxConstraints.tightFor(height: MINIMUM_TARGET_SIZE),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(backgroundColor),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.headline3?.copyWith(
+                  color: foregroundColor,
+                ),
+          ),
+        ),
+      );
+}
+
+/// Onboarding Bottom Icon, e.g. arrow for "next" or "previous".
+class OnboardingBottomIcon extends StatelessWidget {
+  const OnboardingBottomIcon({
+    required this.onPressed,
+    required this.backgroundColor,
+    required this.foregroundColor,
+    required this.icon,
+  });
+
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color foregroundColor;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(12),
+          primary: backgroundColor,
+          onPrimary: foregroundColor,
+        ),
+        onPressed: onPressed,
+        child: Icon(icon),
+      );
+}
