3cf13f41485ef453ff91127eabd197e15574bb2c
Edouard Marquez <g123k@users.noreply.github.com>
2022-06-19 18:34:14 +0200

    Configurable device preview

--- packages/smooth_app_dev_mode/lib/main.dart
+++ packages/smooth_app_dev_mode/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_app/main.dart' as smooth;
 
+/// Launch app with
+/// flutter run -t packages/smooth_app_dev_mode/lib/main.dart
+///
+/// If you want to disable Device Preview, just add:
+/// flutter run -t packages/smooth_app_dev_mode/lib/main.dart --dart-define="DEVICE_PREVIEW=false"
 void main() {
-  if (kReleaseMode) {
-    smooth.main();
-  } else {
-    smooth.main(appBuilder: (BuildContext context) {
-      return DevicePreview(
-        builder: (_) => const smooth.SmoothApp(),
-      );
-    });
-  }
+  smooth.main(appBuilder: (BuildContext context) {
+    return DevicePreview(
+      enabled: const bool.fromEnvironment('DEVICE_PREVIEW', defaultValue: true),
+      builder: (_) => const smooth.SmoothApp(),
+    );
+  });
 }
