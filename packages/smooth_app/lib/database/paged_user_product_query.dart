f9d4dc81b9da2fd680970c379a44def21b2388b1
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-06-19 20:35:12 +0200

    feat: #2315 - added 4 user-related searches in the profile screen (#2321)
    New files:
    * `contributor_product_query.dart`: User Product Search, in "contributor" mode.
    * `informer_product_query.dart`: User Product Search, in "informer" mode.
    * `paged_search_product_query.dart`: Back-end paged queries around search.
    * `paged_user_product_query.dart`: Back-end paged queries around User.
    * `photographer_product_query.dart`: User Product Search, in "photographer" mode.
    * `to_be_completed_product_query.dart`: User Product Search, in "to be completed" mode.
    
    Impacted files:
    * `category_product_query.dart`: refactored with new class `PagedSearchProductQuery`
    * `keywords_product_query.dart`: refactored with new class `PagedSearchProductQuery`
    * `paged_product_query.dart`: now limited to page management
    * `product_list.dart`: added `enum`s and constructors around user queries
    * `product_list_page.dart`: added cases for the new 4 product list types
    * `product_query_page_helper.dart`: added cases for the new 4 product list types
    * `user_preferences_account.dart`

---
+++ packages/smooth_app/lib/database/paged_user_product_query.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:openfoodfacts/utils/OpenFoodAPIConfiguration.dart';
+import 'package:openfoodfacts/utils/QueryType.dart';
+import 'package:openfoodfacts/utils/UriHelper.dart';
+import 'package:smooth_app/database/paged_product_query.dart';
+import 'package:smooth_app/database/product_query.dart';
+
+/// Back-end paged queries around User.
+abstract class PagedUserProductQuery extends PagedProductQuery {
+  PagedUserProductQuery(this.userId);
+
+  final String userId;
+
+  @override
+  Future<SearchResult> getSearchResult() async => _searchProducts(
+        getPath(),
+        ProductQuery.getUser(),
+        pageSize,
+        pageNumber,
+        OpenFoodAPIConfiguration.globalQueryType,
+      );
+
+  String getPath();
+
+  static Future<SearchResult> _searchProducts(
+    // TODO(monsieurtanuki): move to off-dart, but probably not as is
+    final String path,
+    final User user,
+    final int pageSize,
+    final int pageNumber,
+    final QueryType? queryType,
+  ) async {
+    final Uri uri = UriHelper.getUri(
+      path: path,
+      queryType: queryType,
+      queryParameters: <String, String>{
+        'page_size': '$pageSize',
+        'page': '$pageNumber',
+      },
+    );
+    final Response response = await HttpHelper().doGetRequest(
+      uri,
+      queryType: queryType,
+      user: OpenFoodAPIConfiguration.globalUser,
+    );
+    final String jsonStr = response
+        .body; // TODO(monsieurtanuki): what about _replaceQuotes(response.body);
+    final SearchResult result = SearchResult.fromJson(
+      json.decode(jsonStr) as Map<String, dynamic>,
+    );
+
+    // TODO(monsieurtanuki): what about _removeImages(result, configuration);
+
+    return result;
+  }
+}
