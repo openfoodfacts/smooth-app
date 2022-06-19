16838e2cd01cd309747e3d464f224d4e8c6c244e
Aman Raj <amanpie9@gmail.com>
2022-06-19 20:37:21 +0530

    refactor and todo's added

--- packages/smooth_app/lib/pages/offline_data.dart
+++ packages/smooth_app/lib/pages/offline_data.dart
@@ -5,6 +5,9 @@ import 'package:provider/provider.dart';
 import 'package:smooth_app/database/dao_product.dart';
 import 'package:smooth_app/database/dao_product_list.dart';
 import 'package:smooth_app/database/local_database.dart';
+import 'package:smooth_app/generic_lib/loading_dialog.dart';
+import 'package:smooth_app/pages/preferences/user_preferences_list_tile.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OfflineDataScreen extends StatefulWidget {
   const OfflineDataScreen();
@@ -15,8 +18,9 @@ class OfflineDataScreen extends StatefulWidget {
 int length = 0;
 double size = 0;
 
+// TODO(ashaman999): update all the applocalizations
 class _OfflineDataScreenState extends State<OfflineDataScreen> {
-  Future<void> getDetails(DaoProduct daoProduct) async {
+  Future<void> _getDetails(DaoProduct daoProduct) async {
     length = await daoProduct.getLength() ?? 0;
     size = await daoProduct.getSize();
     setState(() {});
@@ -25,16 +29,90 @@ class _OfflineDataScreenState extends State<OfflineDataScreen> {
   @override
   Widget build(BuildContext context) {
     final DaoProduct daoProduct = DaoProduct(context.watch<LocalDatabase>());
-    getDetails(daoProduct);
+    _getDetails(daoProduct);
     final DaoProductList daoProductList =
         DaoProductList(context.watch<LocalDatabase>());
     final MediaQueryData mediaQueryData = MediaQuery.of(context);
     final bool dark = Theme.of(context).brightness == Brightness.dark;
-    // TODO(monsieurtanuki): experimental - find a better value
     const double titleHeightInExpandedMode = 50;
     final double backgroundHeight = mediaQueryData.size.height * .20;
-    // TODO(monsieurtanuki): get rid of explicit foregroundColor when appbartheme colors are correct
     final Color? foregroundColor = dark ? null : Colors.black;
+    final List<Widget> children = <Widget>[
+      UserPreferencesListTile(
+        title: const Text(
+          'Offline Product Data',
+        ),
+        subtitle: Text(
+          '$length products available for immediate scanning',
+        ),
+        trailing: Text(
+          '$size Mb',
+        ),
+      ),
+      UserPreferencesListTile(
+        trailing: const Icon(Icons.refresh),
+        onTap: () async {
+          String? status = await LoadingDialog.run<String>(
+            context: context,
+            future: daoProduct.getFreshLocalDataBase(),
+            title: 'Refreshing \n This may take a while',
+            dismissible: true,
+          );
+          if (status == 'OK') {
+            status = '$length items refreshed';
+          }
+          // TODO(ashaman999): dapproductlist.updateTimestamp();
+          // ignore: use_build_context_synchronously
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(status!),
+            ),
+          );
+        },
+        title: const Text(
+          'Update Offline Data',
+        ),
+        subtitle: const Text(
+          'Update the databse with the latest data from the server',
+        ),
+      ),
+      UserPreferencesListTile(
+        trailing: const Icon(Icons.delete),
+        onTap: () async {
+          final int noOdDeletedProducts = await daoProduct.clearAll();
+          await daoProductList.clearAll();
+          // ignore: use_build_context_synchronously
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content:
+                  Text('$noOdDeletedProducts products deleted, freed $size Mb'),
+            ),
+          );
+        },
+        title: const Text(
+          'Clear Offline Data',
+        ),
+        subtitle: const Text(
+          'Clear All Offline Data and Free up space',
+        ),
+      ),
+      UserPreferencesListTile(
+        trailing: const Icon(Icons.info),
+        title: const Text(
+          'Know More ',
+        ),
+        subtitle: const Text(
+          'Click to know more about the offline mode',
+        ),
+        onTap: () {
+          // TODO(ashaman999): refrence the acutal link
+          // Or maybe show something as an alert dialog
+          launchUrl(
+            Uri.parse('https://openfoodfacts.org/'),
+          );
+        },
+      ),
+    ];
     return Scaffold(
       body: SafeArea(
         child: CustomScrollView(
@@ -44,11 +122,9 @@ class _OfflineDataScreenState extends State<OfflineDataScreen> {
               snap: false,
               floating: false,
               stretch: true,
-              centerTitle: true,
               backgroundColor: dark ? null : Colors.white,
               expandedHeight: backgroundHeight + titleHeightInExpandedMode,
               foregroundColor: foregroundColor,
-              // Force a light status bar
               systemOverlayStyle: const SystemUiOverlayStyle(
                 statusBarIconBrightness: Brightness.light,
                 statusBarBrightness: Brightness.dark,
@@ -62,6 +138,7 @@ class _OfflineDataScreenState extends State<OfflineDataScreen> {
                   padding:
                       const EdgeInsets.only(bottom: titleHeightInExpandedMode),
                   child: SvgPicture.asset(
+                    // TODO(ashaman999): add a proper header image replacing this
                     'assets/preferences/main.svg',
                     height: backgroundHeight,
                   ),
@@ -69,96 +146,13 @@ class _OfflineDataScreenState extends State<OfflineDataScreen> {
               ),
             ),
             SliverList(
-                delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    ListTile(
-                      dense: true,
-                      title: Text(
-                        'Offline Product Data',
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      subtitle: Text(
-                        '$length products available for immediate scanning',
-                        style: const TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text(
-                        '$size Mb',
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                    ),
-                    ListTile(
-                      trailing: const Icon(Icons.refresh),
-                      onTap: () async {
-                        await daoProduct.getFreshLocalDataBase();
-                        // ignore: use_build_context_synchronously
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('$length products refreshed'),
-                          ),
-                        );
-                      },
-                      title: Text(
-                        'Refresh Offline Data',
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      subtitle: const Text(
-                        'Refresh the offline data base with the latest data from the server',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    ListTile(
-                      trailing: const Icon(Icons.delete),
-                      onTap: () async {
-                        final int noOdDeletedProducts =
-                            await daoProduct.clearAll();
-                        await daoProductList.clearAll();
-                        // ignore: use_build_context_synchronously
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                                '$noOdDeletedProducts products deleted, freed $size Mb'),
-                          ),
-                        );
-                      },
-                      title: Text(
-                        'Clear Offline Data',
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      subtitle: const Text(
-                        'Clear All Offline Data and Free up space',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    ListTile(
-                        trailing: const Icon(Icons.info),
-                        title: Text(
-                          'Know More ',
-                          style: Theme.of(context).textTheme.headline3,
-                        ),
-                        subtitle: const Text(
-                          'Click to know more about the offline mode',
-                          style: TextStyle(
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )),
-                  ],
-                ),
-              );
-            }, childCount: 1)),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return children[index];
+                },
+                childCount: children.length,
+              ),
+            ),
           ],
         ),
       ),
