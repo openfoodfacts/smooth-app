f671b72c1e9087170a7442bd76f0d834cf0a176e
Edouard Marquez <g123k@users.noreply.github.com>
2022-07-04 20:58:39 +0200

    Ensure ripples are drawn above cards

--- packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_card.dart
+++ packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_card.dart
@@ -3,6 +3,7 @@ import 'package:openfoodfacts/model/KnowledgePanel.dart';
 import 'package:openfoodfacts/model/KnowledgePanelElement.dart';
 import 'package:openfoodfacts/model/KnowledgePanels.dart';
 import 'package:openfoodfacts/model/Product.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/knowledge_panel/knowledge_panels/knowledge_panel_expanded_card.dart';
 import 'package:smooth_app/knowledge_panel/knowledge_panels/knowledge_panel_group_card.dart';
 import 'package:smooth_app/knowledge_panel/knowledge_panels/knowledge_panel_page.dart';
@@ -33,24 +34,29 @@ class KnowledgePanelCard extends StatelessWidget {
     final KnowledgePanelPanelGroupElement? group =
         KnowledgePanelGroupCard.groupElementOf(context);
 
-    return InkWell(
-      child: KnowledgePanelSummaryCard(
-        panel,
-        isClickable: true,
-      ),
-      onTap: () {
-        Navigator.push<Widget>(
-          context,
-          MaterialPageRoute<Widget>(
-            builder: (BuildContext context) => KnowledgePanelPage(
-              groupElement: group,
-              panel: panel,
-              allPanels: allPanels,
-              product: product,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: SMALL_SPACE),
+      child: InkWell(
+        borderRadius: ANGULAR_BORDER_RADIUS,
+        child: KnowledgePanelSummaryCard(
+          panel,
+          isClickable: true,
+          margin: EdgeInsets.zero,
+        ),
+        onTap: () {
+          Navigator.push<Widget>(
+            context,
+            MaterialPageRoute<Widget>(
+              builder: (BuildContext context) => KnowledgePanelPage(
+                groupElement: group,
+                panel: panel,
+                allPanels: allPanels,
+                product: product,
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
