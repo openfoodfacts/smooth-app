996cc17f02af70bcfea69f354228f36936e80df8
Aman Raj <amanpie9@gmail.com>
2022-07-11 11:02:40 +0530

    import app localizations

--- packages/smooth_app/lib/cards/data_cards/image_upload_card.dart
+++ packages/smooth_app/lib/cards/data_cards/image_upload_card.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/data_models/product_image_data.dart';
 import 'package:smooth_app/helpers/picture_capture_helper.dart';
