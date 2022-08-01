070e47ce03bf9710530eb1cd6e6439e338e76909
Edouard Marquez <g123k@users.noreply.github.com>
2022-08-01 12:24:33 +0200

    AutocompleteWidget: Scrollbar + dividers + correct width

--- packages/smooth_app/lib/pages/product/autocomplete.dart
+++ packages/smooth_app/lib/pages/product/autocomplete.dart
@@ -14,13 +14,17 @@ class AutocompleteOptions<T extends Object> extends StatelessWidget {
     required this.onSelected,
     required this.options,
     required this.maxOptionsHeight,
-  }) : super(key: key);
+    required this.maxOptionsWidth,
+  })  : assert(maxOptionsHeight >= 0),
+        assert(maxOptionsWidth >= 0),
+        super(key: key);
 
   final AutocompleteOptionToString<T> displayStringForOption;
 
   final AutocompleteOnSelected<T> onSelected;
 
   final Iterable<T> options;
+  final double maxOptionsWidth;
   final double maxOptionsHeight;
 
   @override
@@ -30,34 +34,43 @@ class AutocompleteOptions<T extends Object> extends StatelessWidget {
       child: Material(
         elevation: 4.0,
         child: ConstrainedBox(
-          constraints: BoxConstraints(maxHeight: maxOptionsHeight),
-          child: ListView.builder(
-            padding: EdgeInsets.zero,
-            shrinkWrap: true,
-            itemCount: options.length,
-            itemBuilder: (BuildContext context, int index) {
-              final T option = options.elementAt(index);
-              return InkWell(
-                onTap: () {
-                  onSelected(option);
-                },
-                child: Builder(builder: (BuildContext context) {
-                  final bool highlight =
-                      AutocompleteHighlightedOption.of(context) == index;
-                  if (highlight) {
-                    SchedulerBinding.instance
-                        .addPostFrameCallback((Duration timeStamp) {
-                      Scrollable.ensureVisible(context, alignment: 0.5);
-                    });
-                  }
-                  return Container(
-                    color: highlight ? Theme.of(context).focusColor : null,
-                    padding: const EdgeInsets.all(LARGE_SPACE),
-                    child: Text(displayStringForOption(option)),
-                  );
-                }),
-              );
-            },
+          constraints: BoxConstraints(
+            maxHeight: maxOptionsHeight,
+            maxWidth: maxOptionsWidth,
+            minWidth: 100.0,
+          ),
+          child: Scrollbar(
+            child: ListView.separated(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              itemCount: options.length,
+              itemBuilder: (BuildContext context, int index) {
+                final T option = options.elementAt(index);
+                return InkWell(
+                  onTap: () {
+                    onSelected(option);
+                  },
+                  child: Builder(builder: (BuildContext context) {
+                    final bool highlight =
+                        AutocompleteHighlightedOption.of(context) == index;
+                    if (highlight) {
+                      SchedulerBinding.instance
+                          .addPostFrameCallback((Duration timeStamp) {
+                        Scrollable.ensureVisible(context, alignment: 0.5);
+                      });
+                    }
+                    return Container(
+                      color: highlight ? Theme.of(context).focusColor : null,
+                      padding: const EdgeInsets.all(LARGE_SPACE),
+                      child: Text(displayStringForOption(option)),
+                    );
+                  }),
+                );
+              },
+              separatorBuilder: (_, __) => const Divider(
+                height: 1.0,
+              ),
+            ),
           ),
         ),
       ),
