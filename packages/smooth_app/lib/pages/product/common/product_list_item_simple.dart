b08dc608fe9aea6f53b2d6258644e2cec26c4f72
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-08-01 08:44:05 +0200

    feat: #2671 - product lists - downloads products when not in local database (#2673)
    * product lists - downloads products when not in local database
    * added "refresh product" button on "error card"
    
    Impacted files:
    * `attribute_button.dart`: unrelated minor refactoring
    * `design_constants.dart`: unrelated minor refactoring
    * `onboarding_bottom_bar.dart`: unrelated minor refactoring
    * `product_list_item_simple.dart`: added a "refresh product" button on "error card"; different card for "loading" and "downloading"; refactored
    * `product_model.dart`: now we try to download the product if for some unexpected reason it's not in the local database; added a callable `download` method
    * `smooth_product_card_template.dart`: added parameters `barcode` and `error` for more flexibility; added optional "action button"; refactored

--- packages/smooth_app/lib/pages/product/common/product_list_item_simple.dart
+++ packages/smooth_app/lib/pages/product/common/product_list_item_simple.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_app/cards/product_cards/smooth_product_card_found.dart';
 import 'package:smooth_app/cards/product_cards/smooth_product_card_template.dart';
+import 'package:smooth_app/data_models/fetched_product.dart';
 import 'package:smooth_app/data_models/product_list.dart';
 import 'package:smooth_app/data_models/up_to_date_product_provider.dart';
 import 'package:smooth_app/database/local_database.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/pages/product/common/product_model.dart';
 import 'package:smooth_app/services/smooth_services.dart';
 
@@ -48,35 +51,60 @@ class _ProductListItemSimpleState extends State<ProductListItemSimple> {
             ChangeNotifierProvider<ProductModel>(
           create: (final BuildContext context) => _model,
           builder: (final BuildContext context, final Widget? wtf) {
+            final AppLocalizations appLocalizations =
+                AppLocalizations.of(context);
             context.watch<ProductModel>();
             _model.setRefreshedProduct(provider.getFromBarcode(widget.barcode));
             switch (_model.loadingStatus) {
               case LoadingStatus.LOADING:
-                return const SmoothProductCardTemplate();
-              case LoadingStatus.ERROR:
-                Logs.e(
-                  'product list item simple fatal error'
-                  ' on ${widget.barcode} (${_model.loadingError})',
+                return SmoothProductCardTemplate(
+                  barcode: widget.barcode,
                 );
-                return Text('Fatal Error: ${_model.loadingError}');
+              case LoadingStatus.DOWNLOADING:
+                return SmoothProductCardTemplate(
+                  barcode: widget.barcode,
+                  message: appLocalizations.loading_dialog_default_title,
+                );
               case LoadingStatus.LOADED:
+                if (_model.product != null) {
+                  return SmoothProductCardFound(
+                    heroTag: _model.product!.barcode!,
+                    product: _model.product!,
+                    onTap: widget.onTap,
+                    onLongPress: widget.onLongPress,
+                    backgroundColor: widget.backgroundColor,
+                  );
+                }
+                break;
+              case LoadingStatus.ERROR:
             }
-            if (_model.product == null) {
-              // TODO(monsieurtanuki): button "Something strange happened: would you like to download that product again?"
-              Logs.w(
-                'product list item simple / could not load ${widget.barcode}',
-              );
-              return Text(
-                  'Oops: could not load this product (${widget.barcode})');
-            }
-            return SmoothProductCardFound(
-              heroTag: _model.product!.barcode!,
-              product: _model.product!,
-              onTap: widget.onTap,
-              onLongPress: widget.onLongPress,
-              backgroundColor: widget.backgroundColor,
+            Logs.w(
+              'product list item simple / could not load ${widget.barcode}',
+            );
+            return SmoothProductCardTemplate(
+              message: _getErrorMessage(appLocalizations),
+              barcode: widget.barcode,
+              actionButton: IconButton(
+                iconSize: MINIMUM_TOUCH_SIZE,
+                icon: const Icon(Icons.refresh),
+                onPressed: () async => _model.download(),
+              ),
             );
           },
         ),
       );
+
+  String _getErrorMessage(AppLocalizations appLocalizations) {
+    switch (_model.downloadingStatus) {
+      case null:
+        break;
+      case FetchedProductStatus.codeInvalid:
+        return appLocalizations.barcode_invalid_error;
+      case FetchedProductStatus.internetNotFound:
+        return appLocalizations.product_internet_error;
+      default:
+        return appLocalizations.error_occurred;
+    }
+    return _model.loadingError ?? 'Error';
+  }
 }
