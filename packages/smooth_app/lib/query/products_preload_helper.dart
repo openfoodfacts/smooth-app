79241d0eea431f19cf89c34028bd1d1bd408386c
Aman Raj <amanpie9@gmail.com>
2022-08-01 12:41:28 +0530

    added exception handling

--- packages/smooth_app/lib/query/products_preload_helper.dart
+++ packages/smooth_app/lib/query/products_preload_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/database/dao_product.dart';
 import 'package:smooth_app/query/product_query.dart';
@@ -5,7 +7,6 @@ import 'package:smooth_app/query/product_query.dart';
 class PreloadDataHelper {
   PreloadDataHelper(this.daoProduct);
   DaoProduct daoProduct;
-
   Future<String> getTopProducts() async {
     List<String> allProductCodes = await daoProduct.getAllKeys();
     final Map<String, Product> allProducts =
@@ -15,29 +16,37 @@ class PreloadDataHelper {
     allProductCodes = allProducts.keys.toList();
     final List<ProductField> fields = ProductQuery.fields;
     fields.remove(ProductField.KNOWLEDGE_PANELS);
-    final ProductSearchQueryConfiguration queryConfig =
-        ProductSearchQueryConfiguration(
-      fields: fields,
-      parametersList: <Parameter>[
-        const PageSize(size: 1000),
-        const PageNumber(page: 1),
-        const SortBy(option: SortOption.POPULARITY),
-      ],
-      language: ProductQuery.getLanguage(),
-      country: ProductQuery.getCountry(),
-    );
-    final SearchResult searchResult = await OpenFoodAPIClient.searchProducts(
-      ProductQuery.getUser(),
-      queryConfig,
-    );
-    if (searchResult.products!.isEmpty) {
-      return 'No products found for your country and language';
-    } else {
-      searchResult.products!.removeWhere((Product searchProduct) {
-        return allProductCodes.contains(searchProduct.barcode);
-      });
-      await daoProduct.putAll(searchResult.products!);
-      return '${searchResult.products!.length} unique products added to the database for instant scan';
+    try {
+      final ProductSearchQueryConfiguration queryConfig =
+          ProductSearchQueryConfiguration(
+        fields: fields,
+        parametersList: <Parameter>[
+          const PageSize(size: 1000),
+          const PageNumber(page: 1),
+          const SortBy(option: SortOption.POPULARITY),
+        ],
+        language: ProductQuery.getLanguage(),
+        country: ProductQuery.getCountry(),
+      );
+      final SearchResult searchResult = await OpenFoodAPIClient.searchProducts(
+        ProductQuery.getUser(),
+        queryConfig,
+      );
+      if (searchResult.products!.isEmpty) {
+        return 'No products found for your country and language';
+      } else {
+        searchResult.products!.removeWhere(
+          (Product searchProduct) {
+            return allProductCodes.contains(searchProduct.barcode);
+          },
+        );
+        await daoProduct.putAll(searchResult.products!);
+        return '${searchResult.products!.length} unique products added to the database for instant scan';
+      }
+    } on SocketException {
+      return 'No internet connection';
+    } catch (e) {
+      return 'Error: $e';
     }
   }
 }
