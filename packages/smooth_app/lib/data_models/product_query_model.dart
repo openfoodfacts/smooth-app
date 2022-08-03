8599d10f94779957ae372dd8d086864155f015fb
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-08-03 18:38:08 +0200

    feat: #1343 - "world" queries and faster cached result display (#2718)
    Deleted file:
    * `group_query_filter_view.dart`: old code never used and never changed
    
    Impacted files:
    * `app_en.arb`: added 2 labels for "world results"
    * `app_fr.arb`: added 2 labels for "world results"
    * `category_product_query.dart`: implemented world query
    * `keywords_product_query.dart`: implemented world query
    * `paged_product_query.dart`: introduced word query parameter and methods
    * `paged_search_product_query.dart`: implemented world query
    * `paged_to_be_completed_product_query.dart`: implemented world query
    * `product_list_supplier.dart`: refactored
    * `product_model.dart`: loosely related safer "notifyListeners" method
    * `product_query.dart`: now storing here the country, which makes "world" queries possible
    * `product_query_model.dart`: removed all the "category filter" code; better management of the "what if the refresh download fails?" case
    * `product_query_page.dart`: added "world" queries; removed the "category filter" code; now displays a `MaterialBanner` (more flexibility than a `SnackBar`)
    * `smooth_theme.dart`: added a banner theme for `MaterialBanner`

--- packages/smooth_app/lib/data_models/product_query_model.dart
+++ packages/smooth_app/lib/data_models/product_query_model.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/data_models/product_list_supplier.dart';
-import 'package:smooth_app/database/dao_product.dart';
-import 'package:smooth_app/query/product_query.dart';
 
 enum LoadingStatus {
   LOADING,
@@ -11,60 +8,32 @@ enum LoadingStatus {
 }
 
 class ProductQueryModel with ChangeNotifier {
-  ProductQueryModel(this._supplier, this._daoProduct) {
-    _clear();
+  ProductQueryModel(this._supplier) {
     _asyncLoad(notify: true);
   }
 
   ProductListSupplier _supplier;
-  final DaoProduct _daoProduct;
 
   ProductListSupplier get supplier => _supplier;
 
-  static const String _CATEGORY_ALL = 'all';
-  late String currentCategory;
-
   late LoadingStatus _loadingStatus;
   String? _loadingError;
-  final List<String> _barcodes = <String>[];
-  List<String>? displayBarcodes;
-  bool isNotEmpty() => _barcodes.isNotEmpty;
-  final Map<String, List<String>> _productCategories = <String, List<String>>{};
-
-  /// <Label, Label (count)> [Map]
-  final Map<String, String> categories = <String, String>{};
-
-  /// <Label, count> [Map]
-  final Map<String, int> _categoriesCounter = <String, int>{};
-
-  /// Sorted labels
-  final List<String> sortedCategories = <String>[];
+  List<String> displayBarcodes = <String>[];
+  bool isNotEmpty() => displayBarcodes.isNotEmpty;
 
   String? get loadingError => _loadingError;
   LoadingStatus get loadingStatus => _loadingStatus;
 
-  /// Sets the translation for meta category "All".
-  void setTranslationForAll(final String translationForAll) =>
-      categories[_CATEGORY_ALL] = translationForAll;
-
-  void _clear() {
-    currentCategory = _CATEGORY_ALL;
+  Future<bool> _asyncLoad({
+    final bool notify = false,
+    final bool fromScratch = false,
+  }) async {
     _loadingStatus = LoadingStatus.LOADING;
-    _loadingError = null;
-    _barcodes.clear();
-    displayBarcodes = null;
-    _productCategories.clear();
-    categories.clear();
-    _categoriesCounter.clear();
-    sortedCategories.clear();
-  }
-
-  Future<bool> _asyncLoad({final bool notify = false}) async {
     _loadingError = await supplier.asyncLoad();
     if (_loadingError != null) {
       _loadingStatus = LoadingStatus.ERROR;
     } else {
-      await _process(supplier.partialProductList.getBarcodes());
+      await _process(supplier.partialProductList.getBarcodes(), fromScratch);
       _loadingStatus = LoadingStatus.LOADED;
     }
     if (notify) {
@@ -85,10 +54,7 @@ class ProductQueryModel with ChangeNotifier {
     return _asyncLoad();
   }
 
-  // TODO(monsieurtanuki): don't clear everything if it fails?
   Future<bool> loadFromTop() async {
-    _clear();
-
     final ProductListSupplier? refreshSupplier = supplier.getRefreshSupplier();
     if (refreshSupplier != null) {
       // in that case, we were on a database supplier
@@ -96,83 +62,19 @@ class ProductQueryModel with ChangeNotifier {
     } else {
       // in that case, we were already on a back-end supplier
     }
-    await supplier.clear();
     supplier.productQuery.toTopPage();
-    return _asyncLoad();
+    return _asyncLoad(fromScratch: true);
   }
 
-  /// Sorts the products by category.
-  ///
-  /// Is a bit long because we need to get each product from the database,
-  /// in order to know before display all the available categories.
-  /// Optim 1: dismiss this filter altogether.
-  /// Optim 2: get several products at a time - how faster would that be?
-  /// Optim 3: compute the filter async.
-  Future<void> _process(final List<String> barcodes) async {
-    _barcodes.addAll(barcodes);
-    displayBarcodes = _barcodes;
-
-    categories[_CATEGORY_ALL] = ''; // to be overridden later
-    for (final String barcode in barcodes) {
-      final Product? product = await _daoProduct.get(barcode);
-      if (product == null) {
-        // unexpected
-        continue;
-      }
-      if (product.categoriesTagsInLanguages != null) {
-        final List<String>? translatedCategories =
-            product.categoriesTagsInLanguages![ProductQuery.getLanguage()];
-        if (translatedCategories != null) {
-          _productCategories[product.barcode!] = translatedCategories;
-          for (final String category in translatedCategories) {
-            categories[category] = '';
-            _categoriesCounter[category] =
-                (_categoriesCounter[category] ?? 0) + 1;
-          }
-        }
-      }
+  Future<void> _process(
+    final List<String> barcodes,
+    final bool fromScratch,
+  ) async {
+    if (fromScratch) {
+      await supplier.clearBeyondTopPage();
+      displayBarcodes.clear();
     }
-
-    final List<String> tempCategories = categories.keys.toList();
-
-    for (final String category in tempCategories) {
-      if (category != _CATEGORY_ALL) {
-        if (_categoriesCounter[category]! <= 1) {
-          categories.remove(category);
-        } else {
-          categories[category] = '$category (${_categoriesCounter[category]})';
-        }
-      }
-    }
-
-    sortedCategories.clear();
-    sortedCategories.addAll(categories.keys);
-    sortedCategories.sort((String a, String b) {
-      if (a == _CATEGORY_ALL) {
-        return -1;
-      } else if (b == _CATEGORY_ALL) {
-        return 1;
-      }
-      return _categoriesCounter[b]!.compareTo(_categoriesCounter[a]!);
-    });
-
+    displayBarcodes.addAll(barcodes);
     _loadingStatus = LoadingStatus.LOADED;
-  }
-
-  void selectCategory(String category) {
-    currentCategory = category;
-    if (category == _CATEGORY_ALL) {
-      displayBarcodes = _barcodes;
-    } else {
-      final List<String> result = <String>[];
-      _productCategories.forEach(
-        (final String barcode, final List<String> categories) {
-          if (categories.contains(category)) {
-            result.add(barcode);
-          }
-        },
-      );
-      displayBarcodes = result;
-    }
   }
 }
--- packages/smooth_app/lib/query/paged_product_query.dart
+++ packages/smooth_app/lib/query/paged_product_query.dart
@@ -7,6 +7,8 @@ import 'package:smooth_app/query/product_query.dart';
 
 /// Paged product query (with [pageSize] and [pageNumber]).
 abstract class PagedProductQuery {
+  PagedProductQuery({this.world = false});
+
   final int pageSize = _typicalPageSize;
 
   /// Likely to change: to next page, and back to top.
@@ -15,7 +17,18 @@ abstract class PagedProductQuery {
   int get pageNumber => _pageNumber;
 
   final OpenFoodFactsLanguage? language = ProductQuery.getLanguage();
-  final OpenFoodFactsCountry? country = ProductQuery.getCountry();
+  OpenFoodFactsCountry? get country => world ? null : ProductQuery.getCountry();
+
+  /// Is that the world mode (true), or the standard country mode (false).
+  final bool world;
+
+  /// Returns the same query but without country: "for the whole world".
+  ///
+  /// Null if not relevant (already in the "world" version, or no country).
+  PagedProductQuery? getWorldQuery() => null;
+
+  /// Is that a query that has potentially different data for country and world?
+  bool hasDifferentCountryWorldData() => false;
 
   static const int _typicalPageSize = 25;
   static const int _startPageNumber = 1;
