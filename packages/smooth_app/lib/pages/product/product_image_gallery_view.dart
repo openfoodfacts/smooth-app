7b70d61ec4f3dfec77d1db74283ac470edd299a1
VaiTon <eyadlorenzo@gmail.com>
2022-08-05 14:04:58 +0200

    docs: add documentation to `product_image_gallery_view.dart`

--- packages/smooth_app/lib/pages/product/product_image_gallery_view.dart
+++ packages/smooth_app/lib/pages/product/product_image_gallery_view.dart
@@ -11,6 +11,11 @@ import 'package:smooth_app/pages/product/product_image_viewer.dart';
 import 'package:smooth_app/themes/constant_icons.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 
+/// ProductImageGalleryView is a page that displays a list of product images.
+///
+/// It allows the user to add a new image and edit an existing image
+/// by clicking on it.
+///
 class ProductImageGalleryView extends StatefulWidget {
   const ProductImageGalleryView({
     this.barcode,
