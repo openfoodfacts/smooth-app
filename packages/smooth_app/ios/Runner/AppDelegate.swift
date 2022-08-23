e44fc4426f2b29408e5054efb5a40e138c80ba3d
Edouard Marquez <g123k@users.noreply.github.com>
2022-08-23 08:48:15 +0200

    Remove WorkManager iOS code

--- packages/smooth_app/ios/Runner/AppDelegate.swift
+++ packages/smooth_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import workmanager
 
 
 @UIApplicationMain
@@ -10,18 +9,6 @@ import workmanager
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-      
-      WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-                  // Registry in this case is the FlutterEngine that is created in Workmanager's
-                  // performFetchWithCompletionHandler or BGAppRefreshTask.
-                  // This will make other plugins available during a background operation.
-                  GeneratedPluginRegistrant.register(with: registry)
-              }
-      
-    WorkmanagerPlugin.registerTask(withIdentifier: "BackgroundProcess")
-      
-    // Every 15 min
-    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
