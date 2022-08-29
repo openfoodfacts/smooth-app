3dca6488006f9dc043a600522ef16c2fcd5b50e6
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-08-29 15:55:58 +0200

    feat: #2852 - Matomo message when barcode is not found (#2854)
    Impacted files:
    * `analytics_helper.dart`: new method about barcode not found; fixed typos
    * `barcode_product_query.dart`: added Matomo (and not Sentry) message when barcode is not found

--- packages/smooth_app/lib/query/barcode_product_query.dart
+++ packages/smooth_app/lib/query/barcode_product_query.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:openfoodfacts/utils/CountryHelper.dart';
 import 'package:smooth_app/data_models/fetched_product.dart';
 import 'package:smooth_app/database/dao_product.dart';
+import 'package:smooth_app/helpers/analytics_helper.dart';
 import 'package:smooth_app/query/product_query.dart';
 
 class BarcodeProductQuery {
@@ -17,11 +19,13 @@ class BarcodeProductQuery {
   final bool isScanned;
 
   Future<FetchedProduct> getFetchedProduct() async {
+    final OpenFoodFactsLanguage? language = ProductQuery.getLanguage();
+    final OpenFoodFactsCountry? country = ProductQuery.getCountry();
     final ProductQueryConfiguration configuration = ProductQueryConfiguration(
       barcode,
       fields: ProductQuery.fields,
-      language: ProductQuery.getLanguage(),
-      country: ProductQuery.getCountry(),
+      language: language,
+      country: country,
     );
 
     final ProductResult result;
@@ -45,6 +49,12 @@ class BarcodeProductQuery {
         (result.barcode == null || result.barcode!.isEmpty)) {
       return FetchedProduct.error(FetchedProductStatus.codeInvalid);
     }
+    AnalyticsHelper.trackUnknownProduct(
+      barcode: barcode,
+      isScanned: isScanned,
+      language: language,
+      country: country,
+    );
     return FetchedProduct.error(FetchedProductStatus.internetNotFound);
   }
 }
