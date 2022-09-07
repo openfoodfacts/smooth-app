e57cc0f042b6eaee8641e6425520dccf7a648d1c
monsieurtanuki <fabrice_fontaine@hotmail.com>
2022-09-07 19:51:44 +0200

    fix: #2833 - KP page refreshed by product (refactoring was needed) (#2861)
    Impacted files:
    * `knowledge_panel_card.dart`: refactored the call to  `KnowledgePanelPage`
    * `knowledge_panel_element_card.dart`: refactored the calls to `KnowledgePanelCard` and `KnowledgePanelGroupCard`
    * `knowledge_panel_expanded_card.dart`: refactored the call to `KnowledgePanelElementCard`
    * `knowledge_panel_group_card.dart`: refactored the call to `KnowledgePanelCard`
    * `knowledge_panel_page.dart`: refactored the call to `KnowledgePanelExpandedCard`; we use a `Consumer` in order to refresh when the product is refreshed
    * `knowledge_panel_page_template.dart`; refactored the call to `KnowledgePanelWidget`
    * `knowledge_panels_builder.dart`: refactored the call to `KnowledgePanelElementCard`
    * `new_product_page.dart`: refactored the call to `KnowledgePanelWidget`
    * `simple_input_page.dart`: unrelated "vertical axis" for verbose buttons
    * `summary_card.dart`: refactored the call to `KnowledgePanelPage`

--- packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_expanded_card.dart
+++ packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_expanded_card.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:openfoodfacts/model/KnowledgePanel.dart';
 import 'package:openfoodfacts/model/KnowledgePanelElement.dart';
-import 'package:openfoodfacts/model/KnowledgePanels.dart';
 import 'package:openfoodfacts/model/Product.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/knowledge_panel/knowledge_panels/knowledge_panel_element_card.dart';
 import 'package:smooth_app/knowledge_panel/knowledge_panels/knowledge_panel_summary_card.dart';
+import 'package:smooth_app/knowledge_panel/knowledge_panels_builder.dart';
 
 class KnowledgePanelExpandedCard extends StatelessWidget {
   const KnowledgePanelExpandedCard({
-    required this.panel,
-    required this.allPanels,
+    required this.panelId,
     required this.product,
     required this.isInitiallyExpanded,
   });
 
-  final KnowledgePanel panel;
-  final KnowledgePanels allPanels;
   final Product product;
+  final String panelId;
   final bool isInitiallyExpanded;
 
   @override
   Widget build(BuildContext context) {
+    final KnowledgePanel panel =
+        KnowledgePanelWidget.getKnowledgePanel(product, panelId)!;
     final List<Widget> elementWidgets = <Widget>[];
     elementWidgets.add(KnowledgePanelSummaryCard(panel, isClickable: false));
     for (final KnowledgePanelElement element
@@ -31,7 +31,6 @@ class KnowledgePanelExpandedCard extends StatelessWidget {
           padding: const EdgeInsetsDirectional.only(top: VERY_SMALL_SPACE),
           child: KnowledgePanelElementCard(
             knowledgePanelElement: element,
-            allPanels: allPanels,
             product: product,
             isInitiallyExpanded: isInitiallyExpanded,
           ),
