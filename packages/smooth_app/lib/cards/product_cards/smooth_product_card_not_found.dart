aca2f1b89f7aa3c804ad6c53dfa3266fbf95f0b5
Edouard Marquez <g123k@users.noreply.github.com>
2022-09-07 08:19:01 +0200

    Common layout for welcome / product not found / error cards

--- packages/smooth_app/lib/cards/product_cards/smooth_product_card_not_found.dart
+++ packages/smooth_app/lib/cards/product_cards/smooth_product_card_not_found.dart
@@ -1,95 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:smooth_app/cards/product_cards/smooth_product_card.dart';
 import 'package:smooth_app/generic_lib/buttons/smooth_large_button_with_icon.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
-import 'package:smooth_app/generic_lib/widgets/smooth_card.dart';
 import 'package:smooth_app/pages/product/add_new_product_page.dart';
 
 class SmoothProductCardNotFound extends StatelessWidget {
-  const SmoothProductCardNotFound({
+  SmoothProductCardNotFound({
     required this.barcode,
     this.callback,
-    this.elevation = 0.0,
-  });
+  }) : assert(barcode.isNotEmpty);
 
-  final Function(String?)? callback;
-  final double elevation;
+  final Future<void> Function(String?)? callback;
   final String barcode;
 
   @override
   Widget build(BuildContext context) {
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
-    final ThemeData themeData = Theme.of(context);
+    final TextTheme textTheme = Theme.of(context).textTheme;
 
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      final EdgeInsets padding = EdgeInsets.symmetric(
-        vertical: constraints.maxHeight * 0.10,
-        horizontal: constraints.maxWidth * 0.05,
-      );
-
-      final TextTheme textTheme = Theme.of(context).textTheme;
-
-      return SmoothCard(
-        elevation: elevation,
-        color: themeData.brightness == Brightness.light
-            ? Colors.white
-            : Colors.black,
-        padding: padding,
-        child: FittedBox(
-          fit: BoxFit.scaleDown,
-          child: SizedBox(
-            width: constraints.maxWidth - padding.horizontal,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    style: themeData.textTheme.headline5,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: appLocalizations.missing_product,
-                        style: textTheme.headline2,
-                      ),
-                      TextSpan(
-                        text: '\n${appLocalizations.add_product_take_photos}\n',
-                        style: textTheme.bodyText2,
-                      ),
-                      TextSpan(
-                        text: '(${appLocalizations.barcode_barcode(barcode)})',
-                        style: textTheme.bodyText2,
-                      ),
-                    ],
+    return SmoothProductBaseCard(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              style: textTheme.headline5,
+              children: <InlineSpan>[
+                TextSpan(
+                  text: appLocalizations.missing_product,
+                  style: textTheme.headline2,
+                ),
+                const WidgetSpan(
+                  alignment: PlaceholderAlignment.belowBaseline,
+                  baseline: TextBaseline.alphabetic,
+                  child: SizedBox(
+                    height: LARGE_SPACE,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(top: LARGE_SPACE),
-                  child: SmoothLargeButtonWithIcon(
-                    text: appLocalizations.add_product_information_button_label,
-                    icon: Icons.add,
-                    padding: const EdgeInsets.symmetric(vertical: LARGE_SPACE),
-                    onPressed: () async {
-                      // TODO(monsieurtanuki): careful, waiting for pop'ed value
-                      final String? result = await Navigator.push<String>(
-                        context,
-                        MaterialPageRoute<String>(
-                          builder: (BuildContext context) =>
-                              AddNewProductPage(barcode),
-                        ),
-                      );
-                      if (callback != null) {
-                        await callback!(result);
-                      }
-                    },
-                  ),
+                TextSpan(
+                  text: '\n${appLocalizations.add_product_take_photos}\n',
+                  style: textTheme.bodyText2,
+                ),
+                TextSpan(
+                  text: '(${appLocalizations.barcode_barcode(barcode)})',
+                  style: textTheme.bodyText2,
                 ),
               ],
             ),
           ),
-        ),
-      );
-    });
+          Padding(
+            padding: const EdgeInsetsDirectional.only(top: LARGE_SPACE),
+            child: SmoothLargeButtonWithIcon(
+              text: appLocalizations.add_product_information_button_label,
+              icon: Icons.add,
+              padding: const EdgeInsets.symmetric(vertical: LARGE_SPACE),
+              onPressed: () async {
+                // TODO(monsieurtanuki): careful, waiting for pop'ed value
+                final String? result = await Navigator.push<String>(
+                  context,
+                  MaterialPageRoute<String>(
+                    builder: (BuildContext context) =>
+                        AddNewProductPage(barcode),
+                  ),
+                );
+                if (callback != null) {
+                  await callback!(result);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
