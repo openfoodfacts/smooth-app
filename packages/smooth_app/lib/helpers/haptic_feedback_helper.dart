4898c0bf1be4d82197c12e9022b52a307008ad91
Edouard Marquez <g123k@users.noreply.github.com>
2022-09-08 08:32:43 +0200

    Preference to enable/disable haptic feedback (enabled by default)

--- packages/smooth_app/lib/helpers/haptic_feedback_helper.dart
+++ packages/smooth_app/lib/helpers/haptic_feedback_helper.dart
@@ -1,27 +1,57 @@
 import 'package:flutter/services.dart';
+import 'package:smooth_app/data_models/user_preferences.dart';
 
+/// Haptic feedback/vibrations in the app
+/// Managed by a preference in the user preferences
 class SmoothHapticFeedback {
   const SmoothHapticFeedback._();
 
+  /// Will vibrate smoothly twice
   static Future<void> confirm() async {
+    if (!(await _areHapticFeedbackAvailable())) {
+      return;
+    }
+
     await HapticFeedback.lightImpact();
     return Future<void>.delayed(const Duration(milliseconds: 50), () {
       HapticFeedback.lightImpact();
     });
   }
 
-  static Future<void> click() {
+  /// Discrete vibration
+  static Future<void> click() async {
+    if (!(await _areHapticFeedbackAvailable())) {
+      return;
+    }
+
     return HapticFeedback.selectionClick();
   }
 
-  static Future<void> lightNotification() {
+  /// According to the doc: "a collision impact with a light mass"
+  static Future<void> lightNotification() async {
+    if (!(await _areHapticFeedbackAvailable())) {
+      return;
+    }
+
     return HapticFeedback.lightImpact();
   }
 
+  /// Will vibrate heavily twice
   static Future<void> error() async {
+    if (!(await _areHapticFeedbackAvailable())) {
+      return;
+    }
+
     await HapticFeedback.heavyImpact();
     return Future<void>.delayed(const Duration(milliseconds: 50), () {
       HapticFeedback.heavyImpact();
+    });
+  }
+
+  static Future<bool> _areHapticFeedbackAvailable() async {
+    return UserPreferences.getUserPreferences()
+        .then((UserPreferences userPreferences) {
+      return userPreferences.hapticFeedbackEnabled;
     });
   }
 }
