8b08a857c335e52b700f81a9a0e61cd085887191
Peter <WildOrangutan@users.noreply.github.com>
2022-10-03 18:14:54 +0200

    feat: Add checkmarks on 'add new product' screen (#3080)
    * feat: New product basic details checkmark
    
    * refactor: New product page added row
    
    * feat: New product nutrition facts checkmark
    
    * chore: Fix new product page code formatting
    
    * fix: Change icon on new product page
    
    Closes: #2974
    
    * refactor: Add new product page added row to widget

--- packages/smooth_app/lib/pages/product/add_new_product_page.dart
+++ packages/smooth_app/lib/pages/product/add_new_product_page.dart
@@ -201,34 +201,9 @@ class _AddNewProductPageState extends State<AddNewProductPage> {
 
   Widget _buildImageUploadedRow(
       BuildContext context, ImageField imageType, File image) {
-    final ThemeData themeData = Theme.of(context);
-    return Padding(
-      padding: _ROW_PADDING_TOP,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            height: 50,
-            width: 50,
-            child: ClipRRect(
-              borderRadius: ROUNDED_BORDER_RADIUS,
-              child: Image.file(image, fit: BoxFit.cover),
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text(
-                _getAddPhotoButtonText(context, imageType),
-                style: themeData.textTheme.bodyText1,
-              ),
-            ),
-          ),
-          Icon(
-            Icons.check_box,
-            color: themeData.bottomNavigationBarTheme.selectedItemColor,
-          )
-        ],
-      ),
+    return _InfoAddedRow(
+      text: _getAddPhotoButtonText(context, imageType),
+      imgStart: image,
     );
   }
 
@@ -258,31 +233,8 @@ class _AddNewProductPageState extends State<AddNewProductPage> {
       return const SizedBox();
     }
     if (_nutritionFactsAdded) {
-      return Padding(
-        padding: _ROW_PADDING_TOP,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              width: 50.0,
-              child: Icon(
-                Icons.check,
-                color: Theme.of(context)
-                    .bottomNavigationBarTheme
-                    .selectedItemColor,
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text(
-                    AppLocalizations.of(context).nutritional_facts_added,
-                    style: Theme.of(context).textTheme.bodyText1),
-              ),
-            ),
-          ],
-        ),
-      );
+      return _InfoAddedRow(
+          text: AppLocalizations.of(context).nutritional_facts_added);
     }
 
     return Padding(
@@ -333,29 +285,8 @@ class _AddNewProductPageState extends State<AddNewProductPage> {
 
   Widget _buildaddInputDetailsButton() {
     if (_basicDetailsAdded) {
-      final ThemeData themeData = Theme.of(context);
-      return Padding(
-          padding: _ROW_PADDING_TOP,
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                width: 50.0,
-                child: Icon(
-                  Icons.check,
-                  color: themeData.bottomNavigationBarTheme.selectedItemColor,
-                ),
-              ),
-              Expanded(
-                child: Center(
-                  child: Text(
-                      AppLocalizations.of(context).basic_details_add_success,
-                      style: Theme.of(context).textTheme.bodyText1),
-                ),
-              ),
-            ],
-          ));
+      return _InfoAddedRow(
+          text: AppLocalizations.of(context).basic_details_add_success);
     }
 
     return Padding(
@@ -381,5 +312,43 @@ class _AddNewProductPageState extends State<AddNewProductPage> {
         },
       ),
     );
+  }
+}
+
+class _InfoAddedRow extends StatelessWidget {
+  const _InfoAddedRow({required this.text, this.imgStart});
+
+  final String text;
+  final File? imgStart;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return Padding(
+        padding: _ROW_PADDING_TOP,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: ClipRRect(
+                borderRadius: ROUNDED_BORDER_RADIUS,
+                child: imgStart == null
+                    ? null
+                    : Image.file(imgStart!, fit: BoxFit.cover),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text(text, style: themeData.textTheme.bodyText1),
+              ),
+            ),
+            Icon(
+              Icons.check,
+              color: themeData.bottomNavigationBarTheme.selectedItemColor,
+            )
+          ],
+        ));
   }
 }
