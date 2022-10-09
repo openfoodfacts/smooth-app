7d681a86010ab598e10b0beecc556d54585197af
Edouard Marquez <g123k@users.noreply.github.com>
2022-10-09 23:48:19 +0200

    fix: Consistent paddings between all Widgets in the product page (#3099)

--- packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_product_cards.dart
+++ packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_product_cards.dart
@@ -9,21 +9,24 @@ class KnowledgePanelProductCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> widgetsWrappedInSmoothCards = <Widget>[];
-    for (final Widget widget in knowledgePanelWidgets) {
-      widgetsWrappedInSmoothCards.add(
-        Padding(
-          padding: const EdgeInsetsDirectional.only(top: VERY_LARGE_SPACE),
-          child: buildProductSmoothCard(
-            body: widget,
-            padding: SMOOTH_CARD_PADDING,
-          ),
-        ),
-      );
-    }
+    final List<Widget> widgetsWrappedInSmoothCards = knowledgePanelWidgets
+        .map((Widget widget) => Padding(
+              padding: const EdgeInsetsDirectional.only(top: VERY_LARGE_SPACE),
+              child: buildProductSmoothCard(
+                body: widget,
+                padding: SMOOTH_CARD_PADDING,
+                margin: EdgeInsets.zero,
+              ),
+            ))
+        .toList(growable: false);
+
     return Center(
       child: Padding(
-        padding: const EdgeInsetsDirectional.only(bottom: SMALL_SPACE),
+        padding: const EdgeInsetsDirectional.only(
+          bottom: SMALL_SPACE,
+          start: SMALL_SPACE,
+          end: SMALL_SPACE,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
