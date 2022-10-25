1c938a7eba08c9c45cc6b52814cc1e64d59ba679
Aman Raj <amanpie9@gmail.com>
2022-10-25 20:16:05 +0530

    removed unused imports

--- packages/smooth_app/test/dialogs/generic_lib/dialogs_test.dart
+++ packages/smooth_app/test/dialogs/generic_lib/dialogs_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:openfoodfacts/personalized_search/product_preferences_selection.dart';
