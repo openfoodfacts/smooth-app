115c79141062405d687d3bb9a4902ec86b0d390f
Akash Srivastava <akashuna3@gmail.com>
2022-11-08 06:56:47 -0800

    fix: SignUp Issue (#3288)
    * fix: SignUp Issue
    
    * added pubspec
    
    * pubspec added

--- packages/smooth_app/lib/pages/user_management/sign_up_page.dart
+++ packages/smooth_app/lib/pages/user_management/sign_up_page.dart
@@ -349,6 +349,9 @@ class _SignUpPageState extends State<SignUpPage> with TraceableClientMixin {
             .contains(SignUpStatusError.EMAIL_ALREADY_USED)) {
           _emailFocusNode.requestFocus();
         } else if (status.statusErrors!
+            .contains(SignUpStatusError.INCORRECT_EMAIL)) {
+          _emailFocusNode.requestFocus();
+        } else if (status.statusErrors!
             .contains(SignUpStatusError.INVALID_PASSWORD)) {
           _password1FocusNode.requestFocus();
         } else if (status.statusErrors!
