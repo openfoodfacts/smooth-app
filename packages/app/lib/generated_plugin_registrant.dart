a31a4ce1ef4bdd97614840bdb419ea93ee99cc0f
Pierre-Monier <pierre.monier.dev@gmail.con>
2022-11-30 16:00:03 +0100

    clean: remove useless web platform content in app and smooth-app packages

--- packages/app/lib/generated_plugin_registrant.dart
+++ packages/app/lib/generated_plugin_registrant.dart
@@ -8,7 +8,6 @@
 
 import 'package:audioplayers_web/audioplayers_web.dart';
 import 'package:camera_web/camera_web.dart';
-import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 import 'package:device_info_plus_web/device_info_plus_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
@@ -26,7 +25,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   AudioplayersPlugin.registerWith(registrar);
   CameraPlugin.registerWith(registrar);
-  ConnectivityPlusPlugin.registerWith(registrar);
   DeviceInfoPlusPlugin.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
   FlutterSecureStorageWeb.registerWith(registrar);
