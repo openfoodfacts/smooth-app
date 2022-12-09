edb655b59bcd504c1da8b8b6c5479313a2f7ef55
Pierre-Monier <pierre.monier.dev@gmail.com>
2022-12-09 16:15:24 +0100

    refactor: add deletionwebview state private and remove l10n from path segments

--- packages/smooth_app/lib/pages/preferences/account_deletion_webview.dart
+++ packages/smooth_app/lib/pages/preferences/account_deletion_webview.dart
@@ -3,18 +3,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/utils/OpenFoodAPIConfiguration.dart';
-import 'package:provider/provider.dart';
-import 'package:smooth_app/data_models/user_preferences.dart';
 import 'package:smooth_app/helpers/user_management_helper.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class AccountDeletionWebview extends StatefulWidget {
   @override
-  AccountDeletionWebviewState createState() => AccountDeletionWebviewState();
+  State<AccountDeletionWebview> createState() => _AccountDeletionWebviewState();
 }
 
-class AccountDeletionWebviewState extends State<AccountDeletionWebview> {
+class _AccountDeletionWebviewState extends State<AccountDeletionWebview> {
   @override
   void initState() {
     super.initState();
@@ -24,13 +22,9 @@ class AccountDeletionWebviewState extends State<AccountDeletionWebview> {
     }
   }
 
-  String _getUrl(UserPreferences userPreferences) {
-    final String languageCode = userPreferences.appLanguageCode ??
-        Localizations.localeOf(context).toString();
-
+  String _getUrl() {
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
     final String subject = appLocalizations.account_deletion_subject;
-    final String pathSegment = appLocalizations.account_deletion_path_segment;
 
     final String? userId = OpenFoodAPIConfiguration.globalUser?.userId;
 
@@ -38,8 +32,8 @@ class AccountDeletionWebviewState extends State<AccountDeletionWebview> {
         scheme: 'https',
         host: 'blog.openfoodfacts.org',
         pathSegments: <String>[
-          languageCode,
-          pathSegment,
+          'en',
+          'account-deletion',
         ],
         queryParameters: <String, String>{
           'your-subject': subject,
@@ -54,12 +48,10 @@ class AccountDeletionWebviewState extends State<AccountDeletionWebview> {
 
   @override
   Widget build(BuildContext context) {
-    final UserPreferences userPreferences = context.watch<UserPreferences>();
-
     return SmoothScaffold(
       appBar: AppBar(),
       body: WebView(
-        initialUrl: _getUrl(userPreferences),
+        initialUrl: _getUrl(),
       ),
     );
   }
