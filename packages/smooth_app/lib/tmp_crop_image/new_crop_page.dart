1f54c6a550e91b7518d20a300214525b1953b828
monsieurtanuki <fabrice_fontaine@hotmail.com>
2023-01-05 08:10:39 +0100

    fix: 3490 - verbose debug when cropping and saving a pic
    Impacted file:
    * `new_crop_page.dart`: verbose debug when cropping and saving a pic

--- packages/smooth_app/lib/tmp_crop_image/new_crop_page.dart
+++ packages/smooth_app/lib/tmp_crop_image/new_crop_page.dart
@@ -15,6 +15,7 @@ import 'package:smooth_app/data_models/continuous_scan_model.dart';
 import 'package:smooth_app/database/dao_int.dart';
 import 'package:smooth_app/database/local_database.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
+import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_app/helpers/database_helper.dart';
 import 'package:smooth_app/helpers/product_cards_helper.dart';
 import 'package:smooth_app/pages/image_crop_page.dart';
@@ -176,44 +177,89 @@ class _CropPageState extends State<CropPage> {
     );
   }
 
-  Future<void> _saveFileAndExit() async {
-    final LocalDatabase localDatabase = context.read<LocalDatabase>();
-    final DaoInt daoInt = DaoInt(localDatabase);
-    final image2.Image? rawImage = await _controller.croppedBitmap();
-    if (rawImage == null) {
-      return;
-    }
-    final Uint8List data = Uint8List.fromList(image2.encodeJpg(rawImage));
-    final int sequenceNumber =
-        await getNextSequenceNumber(daoInt, _CROP_PAGE_SEQUENCE_KEY);
+  Future<bool> _saveFileAndExit() async {
+    late String debug;
+    try {
+      debug = _debugStep('00');
+      final LocalDatabase localDatabase = context.read<LocalDatabase>();
+      debug = _debugStep('01');
+      final DaoInt daoInt = DaoInt(localDatabase);
+      debug = _debugStep('02');
+      final image2.Image? rawImage = await _controller.croppedBitmap();
+      debug = _debugStep('03');
+      if (rawImage == null) {
+        return true;
+      }
+      debug = _debugStep('04');
+      final Uint8List data = Uint8List.fromList(image2.encodeJpg(rawImage));
+      debug = _debugStep('05');
+      final int sequenceNumber =
+          await getNextSequenceNumber(daoInt, _CROP_PAGE_SEQUENCE_KEY);
 
-    final Directory tempDirectory = await getTemporaryDirectory();
-    final String path = '${tempDirectory.path}/crop_$sequenceNumber.jpeg';
-    final File file = File(path);
-    await file.writeAsBytes(data);
+      debug = _debugStep('06');
+      final Directory tempDirectory = await getTemporaryDirectory();
+      debug = _debugStep('07');
+      final String path = '${tempDirectory.path}/crop_$sequenceNumber.jpeg';
+      debug = _debugStep('08');
+      final File file = File(path);
+      debug = _debugStep('09');
+      await file.writeAsBytes(data);
+      debug = _debugStep('10');
 
-    if (!mounted) {
-      return;
-    }
+      if (!mounted) {
+        return true;
+      }
 
-    await BackgroundTaskImage.addTask(
-      widget.barcode,
-      imageField: widget.imageField,
-      imageFile: file,
-      widget: this,
-    );
-    localDatabase.notifyListeners();
-    if (!mounted) {
-      return;
-    }
-    final ContinuousScanModel model = context.read<ContinuousScanModel>();
-    await model
-        .onCreateProduct(widget.barcode); // TODO(monsieurtanuki): a bit fishy
+      debug = _debugStep('11');
+      await BackgroundTaskImage.addTask(
+        widget.barcode,
+        imageField: widget.imageField,
+        imageFile: file,
+        widget: this,
+      );
+      debug = _debugStep('12');
+      localDatabase.notifyListeners();
+      debug = _debugStep('13');
+      if (!mounted) {
+        return true;
+      }
+      debug = _debugStep('14');
+      final ContinuousScanModel model = context.read<ContinuousScanModel>();
+      debug = _debugStep('15');
+      await model
+          .onCreateProduct(widget.barcode); // TODO(monsieurtanuki): a bit fishy
+      debug = _debugStep('16');
 
-    if (!mounted) {
-      return;
+      if (!mounted) {
+        return true;
+      }
+      debug = _debugStep('17');
+      Navigator.of(context).pop<File>(file);
+      return true;
+    } catch (e) {
+      debugPrint('crop exception on debug step $debug: $e');
+      if (mounted) {
+        await showDialog<void>(
+          context: context,
+          builder: (BuildContext context) => SmoothAlertDialog(
+            body: Text('latest debug step $debug and exception $e'),
+            actionsAxis: Axis.vertical,
+            positiveAction: SmoothActionButton(
+              text: 'ok',
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+        );
+      }
+      return false;
     }
-    Navigator.of(context).pop<File>(file);
+  }
+
+  // TODO(monsieurtanuki): get rid of this somehow when the bug is found
+  // cf. https://github.com/openfoodfacts/smooth-app/issues/3490
+  String _debugStep(final String value) {
+    debugPrint('debug step: $value');
+    return value;
   }
 
   static const String _CROP_PAGE_SEQUENCE_KEY = 'crop_page_sequence';
@@ -245,8 +291,7 @@ class _CropPageState extends State<CropPage> {
       }
     }
 
-    await _saveFileAndExit();
-    return true;
+    return _saveFileAndExit();
   }
 
   Future<void> _capture() async {
