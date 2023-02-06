419dd00f7152a1bd8cd03f5e9dac7c529ca90045
monsieurtanuki <fabrice_fontaine@hotmail.com>
2023-02-06 13:43:02 +0100

    fix: 3663 - packaging number of units and weight as nums
    New files:
    * `simple_input_number_field.dart`: Simple input text field, for numbers.
    * `simple_input_text_field.dart`: Simple input text field, with autocompletion. Used to be in `simple_input_widget.dart`
    
    Impacted files:
    * `edit_new_packagings.dart`: minor refactoring
    * `edit_new_packagings_component.dart`: now we use a numeric keyboard for number of units and weight
    * `edit_new_packagings_helper.dart`: now we format and parse number of units as int? and weight as double?
    * `nutrition_page_loaded.dart`: minor refactoring
    * `simple_input_widget.dart`: refactored code to new file `simple_input_text_field.dart`

--- packages/smooth_app/lib/pages/product/edit_new_packagings_component.dart
+++ packages/smooth_app/lib/pages/product/edit_new_packagings_component.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/cards/category_cards/asset_cache_helper.dart';
 import 'package:smooth_app/cards/category_cards/svg_async_asset.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/pages/product/edit_new_packagings_helper.dart';
 import 'package:smooth_app/pages/product/explanation_widget.dart';
-import 'package:smooth_app/pages/product/simple_input_widget.dart';
+import 'package:smooth_app/pages/product/simple_input_number_field.dart';
+import 'package:smooth_app/pages/product/simple_input_text_field.dart';
 
 /// Edit display of a single [ProductPackaging] component.
 class EditNewPackagingsComponent extends StatefulWidget {
@@ -37,22 +39,23 @@ class _EditNewPackagingsComponentState
     final List<Widget> expandedChildren = !widget.helper.expanded
         ? <Widget>[]
         : <Widget>[
-            _EditLine(
-              // TODO(monsieurtanuki): different display for numbers
+            _EditNumberLine(
               title: appLocalizations.edit_packagings_element_field_units,
               controller: widget.helper.controllerUnits,
               // this icon has 2 colors: we need 2 distinct files
               iconName: dark ? 'counter-dark' : 'counter-light',
               iconColor: null,
+              decimal: false,
+              numberFormat: widget.helper.unitNumberFormat,
             ),
-            _EditLine(
+            _EditTextLine(
               title: appLocalizations.edit_packagings_element_field_shape,
               controller: widget.helper.controllerShape,
               tagType: TagType.PACKAGING_SHAPES,
               iconName: 'shape',
               iconColor: iconColor,
             ),
-            _EditLine(
+            _EditTextLine(
               title: appLocalizations.edit_packagings_element_field_material,
               controller: widget.helper.controllerMaterial,
               tagType: TagType.PACKAGING_MATERIALS,
@@ -60,26 +63,27 @@ class _EditNewPackagingsComponentState
               iconColor: iconColor,
               hint: appLocalizations.edit_packagings_element_hint_material,
             ),
-            _EditLine(
+            _EditTextLine(
               title: appLocalizations.edit_packagings_element_field_recycling,
               controller: widget.helper.controllerRecycling,
               tagType: TagType.PACKAGING_RECYCLING,
               iconName: 'recycling',
               iconColor: iconColor,
             ),
-            _EditLine(
+            _EditTextLine(
               title: appLocalizations.edit_packagings_element_field_quantity,
               controller: widget.helper.controllerQuantity,
               iconName: 'quantity',
               iconColor: iconColor,
             ),
-            _EditLine(
-              // TODO(monsieurtanuki): different display for numbers
+            _EditNumberLine(
               title: appLocalizations.edit_packagings_element_field_weight,
               controller: widget.helper.controllerWeight,
               iconName: 'weight',
               iconColor: iconColor,
               hint: appLocalizations.edit_packagings_element_hint_weight,
+              decimal: true,
+              numberFormat: widget.helper.decimalNumberFormat,
             ),
           ];
     return Column(
@@ -111,8 +115,8 @@ class _EditNewPackagingsComponentState
 }
 
 /// Edit display of a single line inside a [ProductPackaging], e.g. its shape.
-class _EditLine extends StatelessWidget {
-  const _EditLine({
+class _EditTextLine extends StatelessWidget {
+  const _EditTextLine({
     required this.title,
     required this.controller,
     required this.iconName,
@@ -147,7 +151,7 @@ class _EditLine extends StatelessWidget {
           LayoutBuilder(
             builder: (_, BoxConstraints constraints) => SizedBox(
               width: constraints.maxWidth,
-              child: SimpleInputWidgetField(
+              child: SimpleInputTextField(
                 focusNode: FocusNode(),
                 autocompleteKey: UniqueKey(),
                 constraints: constraints,
@@ -155,6 +159,68 @@ class _EditLine extends StatelessWidget {
                 hintText: '',
                 controller: controller,
                 withClearButton: true,
+              ),
+            ),
+          ),
+          if (hint != null)
+            Padding(
+              padding: const EdgeInsets.only(bottom: LARGE_SPACE),
+              child: ExplanationWidget(hint!),
+            ),
+        ],
+      );
+}
+
+/// Edit display of a _number_ inside a [ProductPackaging], e.g. its weight.
+class _EditNumberLine extends StatelessWidget {
+  const _EditNumberLine({
+    required this.title,
+    required this.controller,
+    required this.iconName,
+    required this.iconColor,
+    required this.decimal,
+    required this.numberFormat,
+    this.hint,
+  });
+
+  final String title;
+  final TextEditingController controller;
+  final String iconName;
+  final Color? iconColor;
+  final String? hint;
+  final bool decimal;
+  final NumberFormat numberFormat;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ListTile(
+            leading: SvgAsyncAsset(
+              AssetCacheHelper(
+                <String>['assets/packagings/$iconName.svg'],
+                'no url for packagings/$iconName',
+                color: iconColor,
+                width: MINIMUM_TOUCH_SIZE,
+              ),
+            ),
+            title: Text(title),
+          ),
+          LayoutBuilder(
+            builder: (_, BoxConstraints constraints) => SizedBox(
+              width: constraints.maxWidth,
+              child: SimpleInputNumberField(
+                focusNode: FocusNode(),
+                constraints: constraints,
+                hintText: '',
+                controller: controller,
+                decimal: decimal,
+                withClearButton: true,
+                numberFormat: numberFormat,
+                numberRegExp: SimpleInputNumberField.getNumberRegExp(
+                  decimal: decimal,
+                ),
               ),
             ),
           ),
