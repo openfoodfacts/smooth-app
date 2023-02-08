6588b0b2236c57412af90af4ce7eaee69d180d29
Marvin Möltgen <39344769+M123-dev@users.noreply.github.com>
2023-02-08 20:45:11 +0100

    fix: Webview (#3688)
    * fix: Webview
    
    * Update user_preferences_page_test.dart

--- packages/smooth_app/lib/pages/preferences/account_deletion_webview.dart
+++ packages/smooth_app/lib/pages/preferences/account_deletion_webview.dart
@@ -5,47 +5,36 @@ import 'package:smooth_app/helpers/user_management_helper.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class AccountDeletionWebview extends StatefulWidget {
+class AccountDeletionWebview extends StatelessWidget {
   @override
-  State<AccountDeletionWebview> createState() => _AccountDeletionWebviewState();
-}
-
-class _AccountDeletionWebviewState extends State<AccountDeletionWebview> {
-  final WebViewController _controller = WebViewController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.loadRequest(_getUri());
-  }
-
-  Uri _getUri() {
+  Widget build(BuildContext context) {
+    final WebViewController controller = WebViewController();
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
     final String subject = appLocalizations.account_deletion_subject;
     final String? userId = OpenFoodAPIConfiguration.globalUser?.userId;
-    return Uri(
-      scheme: 'https',
-      host: 'blog.openfoodfacts.org',
-      pathSegments: <String>[
-        'en',
-        'account-deletion',
-      ],
-      queryParameters: <String, String>{
-        'your-subject': subject,
-        if (userId != null && userId.isEmail)
-          'your-mail': userId
-        else if (userId != null)
-          'your-name': userId
-      },
-    );
-  }
 
-  @override
-  Widget build(BuildContext context) {
+    controller.loadRequest(
+      Uri(
+        scheme: 'https',
+        host: 'blog.openfoodfacts.org',
+        pathSegments: <String>[
+          'en',
+          'account-deletion',
+        ],
+        queryParameters: <String, String>{
+          'your-subject': subject,
+          if (userId != null && userId.isEmail)
+            'your-mail': userId
+          else if (userId != null)
+            'your-name': userId
+        },
+      ),
+    );
+
     return SmoothScaffold(
       appBar: AppBar(),
       body: WebViewWidget(
-        controller: WebViewController(),
+        controller: controller,
       ),
     );
   }
