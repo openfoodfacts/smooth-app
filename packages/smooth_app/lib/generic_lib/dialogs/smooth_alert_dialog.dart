9816631ba96598285d06f44e6970eda7bfd66913
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-02-21 14:15:44 +0530

    add buttonTheme.secondary color instead of focus color

--- packages/smooth_app/lib/generic_lib/dialogs/smooth_alert_dialog.dart
+++ packages/smooth_app/lib/generic_lib/dialogs/smooth_alert_dialog.dart
@@ -416,7 +416,7 @@ class _SmoothActionFlatButton extends StatelessWidget {
           textStyle: themeData.textTheme.bodyMedium!.copyWith(
             color: themeData.colorScheme.onPrimary,
           ),
-          backgroundColor: Theme.of(context).focusColor,
+          backgroundColor: themeData.buttonTheme.colorScheme?.secondary,
           padding: const EdgeInsets.symmetric(
             horizontal: SMALL_SPACE,
           ),
