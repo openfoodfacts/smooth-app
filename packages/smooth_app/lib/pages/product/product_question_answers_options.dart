a8fa7068d7712aad42111e39ce6074887dc040b3
Pratham Soni <pratham1108soni@gmail.com>
2023-03-04 12:11:41 +0530

    new widget created for product page

---
+++ packages/smooth_app/lib/pages/product/product_question_answers_options.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:openfoodfacts/model/Insight.dart';
+import 'package:openfoodfacts/model/RobotoffQuestion.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
+
+Color _yesBackground = Colors.green;
+Color _noBackground = Colors.red;
+const Color _maybeBackground = Colors.white;
+const Color _yesNoTextColor = Colors.white;
+Color _maybeTextColor = Colors.grey.shade700;
+
+/// Display of the typical Yes / No / Maybe options for Robotoff
+class ProductQuestionAnswersOptions extends StatelessWidget {
+  const ProductQuestionAnswersOptions(
+    this.question, {
+    Key? key,
+    required this.onAnswer,
+  }) : super(key: key);
+
+  final RobotoffQuestion question;
+  final Function(InsightAnnotation) onAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    final double yesNoHeight = MediaQuery.of(context).size.width / (6);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Expanded(
+          child: SizedBox(
+            height: yesNoHeight,
+            child: _buildAnswerButton(
+              context,
+              insightAnnotation: InsightAnnotation.NO,
+              backgroundColor: _noBackground,
+              contentColor: _yesNoTextColor,
+            ),
+          ),
+        ),
+        Expanded(
+          child: SizedBox(
+            height: yesNoHeight,
+            child: _buildAnswerButton(
+              context,
+              insightAnnotation: InsightAnnotation.MAYBE,
+              backgroundColor: _maybeBackground,
+              contentColor: _maybeTextColor,
+            ),
+          ),
+        ),
+        Expanded(
+          child: SizedBox(
+            height: yesNoHeight,
+            child: _buildAnswerButton(
+              context,
+              insightAnnotation: InsightAnnotation.YES,
+              backgroundColor: _yesBackground,
+              contentColor: _yesNoTextColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAnswerButton(
+    BuildContext context, {
+    required InsightAnnotation insightAnnotation,
+    required Color backgroundColor,
+    required Color contentColor,
+    EdgeInsets padding = const EdgeInsets.all(SMALL_SPACE),
+  }) {
+    final AppLocalizations appLocalizations = AppLocalizations.of(context);
+    final ThemeData theme = Theme.of(context);
+
+    String buttonText;
+    switch (insightAnnotation) {
+      case InsightAnnotation.YES:
+        buttonText = appLocalizations.yes;
+        break;
+      case InsightAnnotation.NO:
+        buttonText = appLocalizations.no;
+        break;
+      case InsightAnnotation.MAYBE:
+        buttonText = appLocalizations.skip;
+    }
+
+    return Padding(
+      padding: padding,
+      child: TextButton(
+        onPressed: () => onAnswer(insightAnnotation),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(backgroundColor),
+        ),
+        child: Text(
+          buttonText,
+          style: theme.textTheme.headline3!.apply(color: contentColor),
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/smooth_app/lib/pages/product/product_question_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
+
+/// Display of a Robotoff question text.
+class ProductQuestionCard extends StatelessWidget {
+  const ProductQuestionCard(
+    this.question, {
+    Key? key,
+  }) : super(key: key);
+
+  final RobotoffQuestion question;
+
+  static const Color robotoffBackground = Color(0xFFFFEFB7);
+
+  @override
+  Widget build(BuildContext context) {
+    final Future<Product> productFuture = OpenFoodAPIClient.getProduct(
+      ProductQueryConfiguration(question.barcode!),
+    ).then((ProductResult result) => result.product!);
+
+    return FutureBuilder<Product>(
+        future: productFuture,
+        builder: (BuildContext context, AsyncSnapshot<Product> snapshot) {
+          if (!snapshot.hasData) {
+            return _buildQuestionShimmer();
+          }
+          return _buildQuestionText(context, question);
+        });
+  }
+
+  Widget _buildQuestionText(BuildContext context, RobotoffQuestion question) {
+    final ThemeData theme = Theme.of(context);
+    final bool isDarkMode = theme.brightness == Brightness.dark;
+    return Padding(
+      padding: const EdgeInsets.only(left: SMALL_SPACE),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsetsDirectional.only(bottom: VERY_SMALL_SPACE),
+            child: Text(
+              question.question!,
+              style: theme.textTheme.headline4!.apply(
+                color: isDarkMode ? Colors.white : theme.cardTheme.color,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(bottom: VERY_SMALL_SPACE),
+            child: Text(
+              question.value!,
+              style: theme.textTheme.headline4?.apply(
+                  color: isDarkMode ? Colors.white : theme.cardTheme.color),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQuestionShimmer() => Shimmer.fromColors(
+        baseColor: robotoffBackground,
+        highlightColor: Colors.white,
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          shape: const RoundedRectangleBorder(
+            borderRadius: ROUNDED_BORDER_RADIUS,
+          ),
+          child: Container(
+            height: LARGE_SPACE * 4,
+          ),
+        ),
+      );
+}
