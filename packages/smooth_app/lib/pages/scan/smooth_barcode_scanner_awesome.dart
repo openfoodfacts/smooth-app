6991d5b0bb3c90fe1d18862e76fd914e636a995a
Marvin M <39344769+M123-dev@users.noreply.github.com>
2023-03-18 09:01:37 +0100

    First scanner impl

--- packages/smooth_app/lib/pages/scan/smooth_barcode_scanner_awesome.dart
+++ packages/smooth_app/lib/pages/scan/smooth_barcode_scanner_awesome.dart
@@ -1,16 +1,23 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:camerawesome/pigeon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 import 'package:smooth_app/pages/scan/scan_header.dart';
 import 'package:smooth_app/pages/scan/scan_visor.dart';
 import 'package:smooth_app/widgets/smooth_product_carousel.dart';
 
 class SmoothBarcodeScannerAwesome extends StatelessWidget {
-  const SmoothBarcodeScannerAwesome(this.onScan);
+  SmoothBarcodeScannerAwesome(this.onScan);
 
   final Future<bool> Function(String) onScan;
+
+  final BarcodeScanner _barcodeScanner = BarcodeScanner(
+    formats: <BarcodeFormat>[BarcodeFormat.all],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +27,12 @@ class SmoothBarcodeScannerAwesome extends StatelessWidget {
         pathBuilder: () async {
           return 'just_needed_because.jpg';
         },
+      ),
+      onImageForAnalysis: (AnalysisImage img) => _processImageBarcode(img),
+      imageAnalysisConfig: AnalysisConfig(
+        outputFormat: InputAnalysisImageFormat.nv21,
+        width: 1024,
+        maxFramesPerSecond: 5,
       ),
       builder: (
         CameraState cameraState,
@@ -52,6 +65,76 @@ class SmoothBarcodeScannerAwesome extends StatelessWidget {
         ) as Widget;
       },
     );
+  }
+
+  Future<void> _processImageBarcode(AnalysisImage img) async {
+    final Size imageSize = Size(img.width.toDouble(), img.height.toDouble());
+    final InputImageRotation imageRotation =
+        InputImageRotation.values.byName(img.rotation.name);
+
+    final List<InputImagePlaneMetadata> planeData = img.planes.map(
+      (ImagePlane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: img.height,
+          width: img.width,
+        );
+      },
+    ).toList();
+
+    final InputImage inputImage;
+
+    if (Platform.isIOS) {
+      final InputImageData inputImageData = InputImageData(
+        size: imageSize,
+        imageRotation: imageRotation,
+        inputImageFormat: _inputImageFormat(img.format),
+        planeData: planeData,
+      );
+
+      final WriteBuffer allBytes = WriteBuffer();
+      for (final ImagePlane plane in img.planes) {
+        allBytes.putUint8List(plane.bytes);
+      }
+      final Uint8List bytes = allBytes.done().buffer.asUint8List();
+
+      inputImage =
+          InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+    } else {
+      inputImage = InputImage.fromBytes(
+        bytes: img.nv21Image!,
+        inputImageData: InputImageData(
+          imageRotation: imageRotation,
+          inputImageFormat: InputImageFormat.nv21,
+          planeData: planeData,
+          size: Size(img.width.toDouble(), img.height.toDouble()),
+        ),
+      );
+    }
+
+    try {
+      final List<Barcode> recognizedBarCodes =
+          await _barcodeScanner.processImage(inputImage);
+      for (final Barcode barcode in recognizedBarCodes) {
+        debugPrint('Barcode: [${barcode.format}]: ${barcode.rawValue}');
+
+        onScan(barcode.rawValue.toString());
+      }
+    } catch (error) {
+      debugPrint('Camera awesome ml kit error $error');
+      rethrow;
+    }
+  }
+
+  InputImageFormat _inputImageFormat(InputAnalysisImageFormat format) {
+    switch (format) {
+      case InputAnalysisImageFormat.bgra8888:
+        return InputImageFormat.bgra8888;
+      case InputAnalysisImageFormat.nv21:
+        return InputImageFormat.nv21;
+      default:
+        return InputImageFormat.yuv420;
+    }
   }
 }
 
