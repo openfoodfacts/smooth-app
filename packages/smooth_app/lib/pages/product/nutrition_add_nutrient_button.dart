b93f32dc6a6ec36d0342528406ece6d24b06d250
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-03-20 23:08:01 +0530

    format

--- packages/smooth_app/lib/pages/product/nutrition_add_nutrient_button.dart
+++ packages/smooth_app/lib/pages/product/nutrition_add_nutrient_button.dart
@@ -1,3 +1,4 @@
+import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
