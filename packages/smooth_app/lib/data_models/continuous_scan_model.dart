726b5d438dac199e4423dccb375aed0beb73f62e
Edouard Marquez <g123k@users.noreply.github.com>
2023-05-27 14:23:40 +0200

    The scanner may return cropped barcodes. (#4030)
    We try to fix them with a simple algorithm for now

--- packages/smooth_app/lib/data_models/continuous_scan_model.dart
+++ packages/smooth_app/lib/data_models/continuous_scan_model.dart
@@ -105,6 +105,8 @@ class ContinuousScanModel with ChangeNotifier {
       return false;
     }
 
+    code = _fixBarcodeIfNecessary(code);
+
     if (_latestScannedBarcode == code || _barcodes.contains(code)) {
       lastConsultedBarcode = code;
       return false;
@@ -279,5 +281,15 @@ class ContinuousScanModel with ChangeNotifier {
   Future<void> refresh() async {
     await _refresh();
     notifyListeners();
+  }
+
+  /// Sometimes the scanner may fail, this is a simple fix for now
+  /// But could be improved in the future
+  String _fixBarcodeIfNecessary(String code) {
+    if (code.length == 12) {
+      return '0$code';
+    } else {
+      return code;
+    }
   }
 }
