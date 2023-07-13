644fd96766af19a3e0bd3b6c544525cc8fbfdf20
Edouard Marquez <moi@edouard-marquez.me>
2023-07-13 16:14:42 +0200

    Don't use cupertino but widgets.dart as a dependency

--- packages/smooth_app/lib/helpers/keyboard_helper.dart
+++ packages/smooth_app/lib/helpers/keyboard_helper.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 
 extension KeyboardContextExtention on BuildContext {
   bool get keyboardVisible => MediaQuery.of(this).viewInsets.bottom > 0.0;
