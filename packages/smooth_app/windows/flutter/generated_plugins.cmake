8e80d4239bc45e4f055ca6a3e272b3cadb34093d
Edouard Marquez <g123k@users.noreply.github.com>
2023-07-21 01:40:56 +0200

    feat: A nice animation for the onboarding (#4311)

--- packages/smooth_app/windows/flutter/generated_plugins.cmake
+++ packages/smooth_app/windows/flutter/generated_plugins.cmake
@@ -7,6 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_secure_storage_windows
   permission_handler_windows
+  rive_common
   sentry_flutter
   share_plus
   url_launcher_windows
