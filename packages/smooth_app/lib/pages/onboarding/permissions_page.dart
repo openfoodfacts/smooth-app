b9c6be7082cc3d93f69bcd748d1b99c10373ce41
Edouard Marquez <g123k@users.noreply.github.com>
2023-07-21 01:48:27 +0200

    fix: Permissions page: use "a well balanced" style to ensure we have bigger line height (#4341)

--- packages/smooth_app/lib/pages/onboarding/permissions_page.dart
+++ packages/smooth_app/lib/pages/onboarding/permissions_page.dart
@@ -12,6 +12,7 @@ import 'package:smooth_app/helpers/permission_helper.dart';
 import 'package:smooth_app/helpers/provider_helper.dart';
 import 'package:smooth_app/pages/onboarding/onboarding_bottom_bar.dart';
 import 'package:smooth_app/pages/onboarding/onboarding_flow_navigator.dart';
+import 'package:smooth_app/widgets/smooth_text.dart';
 
 class PermissionsPage extends StatefulWidget {
   const PermissionsPage(
@@ -80,12 +81,14 @@ class _PermissionsPageState extends State<PermissionsPage> {
                       appLocalizations.permissions_page_body1,
                       maxLines: 2,
                       textAlign: TextAlign.center,
+                      style: WellSpacedTextHelper.TEXT_STYLE_WITH_WELL_SPACED,
                     ),
                     const SizedBox(height: MEDIUM_SPACE),
                     AutoSizeText(
                       appLocalizations.permissions_page_body2,
                       maxLines: 3,
                       textAlign: TextAlign.center,
+                      style: WellSpacedTextHelper.TEXT_STYLE_WITH_WELL_SPACED,
                     ),
                   ],
                 ),
