a62267f4fe532f3ebb94c9fd8972db947c08bb49
monsieurtanuki <fabrice_fontaine@hotmail.com>
2023-08-05 18:16:33 +0200

    fix: smooth_error_card_test.dart

--- packages/smooth_app/test/pages/generic_lib/widgets/smooth_error_card_test.dart
+++ packages/smooth_app/test/pages/generic_lib/widgets/smooth_error_card_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:smooth_app/data_models/user_preferences.dart';
+import 'package:smooth_app/data_models/preferences/user_preferences.dart';
 import 'package:smooth_app/generic_lib/buttons/smooth_simple_button.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_error_card.dart';
 import 'package:smooth_app/themes/theme_provider.dart';
