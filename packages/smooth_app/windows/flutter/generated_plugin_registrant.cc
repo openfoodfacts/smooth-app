f79bae611fece28e0b2da25f6340b4063fa5fda6
monsieurtanuki <fabrice_fontaine@hotmail.com>
2023-08-16 12:01:25 +0200

    feat: 4423 - specific "Not connected to internet" displayed error (#4455)
    * feat: 4423 - specific "Not connected to internet" displayed error
    
    Impacted files:
    * `barcode_product_query.dart`: removed useless `try` as already `catch`'ed
    * `continuous_scan_model.dart`: removed the `codeInvalid` case that could never happen
    * `fetched_product.dart`: refactored with explicit constructors and additional exception and connectivity fields; removed the `codeInvalid` case that could never happen
    * `new_product_page.dart`: minor refactoring
    * `product_dialog_helper.dart`: removed the `codeInvalid` case that could never happen; minor refactoting
    * `product_list_item_simple.dart`: removed the `codeInvalid` case that could never happen
    * `product_loader_page.dart`: removed useless `try` as already `catch`'ed
    * `product_refresher.dart`: added a specific "You're not connected to the internet" error message; refactored using more `FetchedProduct`; removed useless method
    * `pubspec.lock`: wtf
    * `pubspec.yaml`: added package `connectivity_plus`
    * `question_card.dart`: refactored using `FetchedProduct`
    
    * feat: 4423 - fixed pubspec.yaml
    
    * feat: 4423 - new "server down" message after a ping attempt
    
    Impacted files:
    * `fetched_product.dart`: added field `failedPingedHost` where we store the host that we couldn't ping
    * `generated_plugin_registrant.cc`: wtf
    * `generated_plugins.cmake`: wtf
    * `GeneratedPluginRegistrant.swift`: wtf
    * `main.dart`: registered `DartPingIOS`
    * `product_refresher.dart`: now trying to ping the server if exception and connection
    * `pubspec.lock`: wtf
    * `pubspec.yaml: added packages `dart_ping` and `dart_ping_ios`
    
    * feat: 4423 - stupid lint check part 1
    
    * feat: 4423 - stupid lint check part 2
    
    * feat: 4423 - stupid lint check part 3
    
    * feat: 4423 - stupid lint check part 5
    
    * feat: 4423 - localizations
    
    Impacted files:
    * `app_en.arb`: 4 new labels when we couldn't retrieve a product (not found, no internet, server down, server error)
    * `product_refresher.dart`: used the new labels

--- packages/smooth_app/windows/flutter/generated_plugin_registrant.cc
+++ packages/smooth_app/windows/flutter/generated_plugin_registrant.cc
@@ -7,6 +7,7 @@
 #include "generated_plugin_registrant.h"
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <file_selector_windows/file_selector_windows.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
@@ -18,6 +19,8 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
