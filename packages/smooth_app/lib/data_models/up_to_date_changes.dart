openfoodfacts/smooth-app

commit 346c6e30c4931afe54eb0fcc6335fd9e270624d8

fix: 1815 - around context.mounted (#4768)
* fix: 1815 - around context.mounted

The PR is about two related topics:
* Removing all the `ignore: use_build_context_synchronously`
* Replacing all the `State<StatefulWidget> widget` by `BuildContext context`, now that it's possible in flutter

* Update packages/smooth_app/lib/pages/onboarding/permissions_page.dart

* format fix

packages/smooth_app/lib/pages/product/ordered_nutrients_cache.dart
@@ -21,15 +21,19 @@ class OrderedNutrientsCache {
     final BuildContext context,
   ) async {
     final OrderedNutrientsCache cache = OrderedNutrientsCache._();
-    cache._orderedNutrients = await cache._get() ??
-        // ignore: use_build_context_synchronously
+    cache._orderedNutrients = await cache._get();
+    if (cache._orderedNutrients == null) {
+      if (context.mounted) {
         await LoadingDialog.run<OrderedNutrients>(
           context: context,
           future: cache._download(),
         );
+      }
+    }
     if (cache._orderedNutrients == null) {
-      // ignore: use_build_context_synchronously
-      await LoadingDialog.error(context: context);
+      if (context.mounted) {
+        await LoadingDialog.error(context: context);
+      }
       return null;
     }
     return cache;

packages/smooth_app/lib/background/background_task.dart
@@ -111,31 +111,30 @@ abstract class BackgroundTask {
   @protected
   Future<void> addToManager(
     final LocalDatabase localDatabase, {
-    final State<StatefulWidget>? widget,
+    final BuildContext? context,
     final bool showSnackBar = true,
   }) async {
     await BackgroundTaskManager.getInstance(localDatabase).add(this);
-    if (widget == null || !widget.mounted) {
+    if (context == null || !context.mounted) {
       return;
     }
     if (!showSnackBar) {
       return;
     }
 
-    if (widget.context.mounted) {
-      // ignore: use_build_context_synchronously
-      if (getFloatingMessage(AppLocalizations.of(widget.context))
-          case (
-            final String message,
-            final AlignmentGeometry alignment,
-          )) {
-        // ignore: use_build_context_synchronously
-        SmoothFloatingMessage(message: message).show(
-          widget.context,
-          duration: SnackBarDuration.medium,
-          alignment: alignment,
-        );
-      }
+    if (!context.mounted) {
+      return;
+    }
+    if (getFloatingMessage(AppLocalizations.of(context))
+        case (
+          final String message,
+          final AlignmentGeometry alignment,
+        )) {
+      SmoothFloatingMessage(message: message).show(
+        context,
+        duration: SnackBarDuration.medium,
+        alignment: alignment,
+      );
     }
   }
 

packages/smooth_app/lib/background/background_task_crop.dart
@@ -61,9 +61,9 @@ class BackgroundTaskCrop extends BackgroundTaskUpload {
     required final int y1,
     required final int x2,
     required final int y2,
-    required final State<StatefulWidget> widget,
+    required final BuildContext context,
   }) async {
-    final LocalDatabase localDatabase = widget.context.read<LocalDatabase>();
+    final LocalDatabase localDatabase = context.read<LocalDatabase>();
     final String uniqueId = await _operationType.getNewKey(
       localDatabase,
       barcode: barcode,
@@ -81,7 +81,10 @@ class BackgroundTaskCrop extends BackgroundTaskUpload {
       x2,
       y2,
     );
-    await task.addToManager(localDatabase, widget: widget);
+    if (!context.mounted) {
+      return;
+    }
+    await task.addToManager(localDatabase, context: context);
   }
 
   @override

packages/smooth_app/lib/background/background_task_details.dart
@@ -69,11 +69,11 @@ class BackgroundTaskDetails extends BackgroundTaskBarcode {
   /// Adds the background task about changing a product.
   static Future<void> addTask(
     final Product minimalistProduct, {
-    required final State<StatefulWidget> widget,
+    required final BuildContext context,
     required final BackgroundTaskDetailsStamp stamp,
     final bool showSnackBar = true,
   }) async {
-    final LocalDatabase localDatabase = widget.context.read<LocalDatabase>();
+    final LocalDatabase localDatabase = context.read<LocalDatabase>();
     final String uniqueId = await _operationType.getNewKey(
       localDatabase,
       barcode: minimalistProduct.barcode,
@@ -83,9 +83,12 @@ class BackgroundTaskDetails extends BackgroundTaskBarcode {
       uniqueId,
       stamp,
     );
+    if (!context.mounted) {
+      return;
+    }
     await task.addToManager(
       localDatabase,
-      widget: widget,
+      context: context,
       showSnackBar: showSnackBar,
     );
   }

packages/smooth_app/lib/background/background_task_full_refresh.dart
@@ -32,10 +32,10 @@ class BackgroundTaskFullRefresh extends BackgroundTaskPaged {
   static const OperationType _operationType = OperationType.fullRefresh;
 
   static Future<void> addTask({
-    required final State<StatefulWidget> widget,
+    required final BuildContext context,
     required final int pageSize,
   }) async {
-    final LocalDatabase localDatabase = widget.context.read<LocalDatabase>();
+    final LocalDatabase localDatabase = context.read<LocalDatabase>();
     final String uniqueId = await _operationType.getNewKey(
       localDatabase,
     );
@@ -43,7 +43,10 @@ class BackgroundTaskFullRefresh extends BackgroundTaskPaged {
       uniqueId,
       pageSize,
     );
-    await task.addToManager(localDatabase, widget: widget);
+    if (!context.mounted) {
+      return;
+    }
+    await task.addToManager(localDatabase, context: context);
   }
 
   @override

packages/smooth_app/lib/background/background_task_hunger_games.dart
@@ -50,9 +50,9 @@ class BackgroundTaskHungerGames extends BackgroundTaskBarcode {
     required final String barcode,
     required final String insightId,
     required final InsightAnnotation insightAnnotation,
-    required final State<StatefulWidget> widget,
+    required final BuildContext context,
   }) async {
-    final LocalDatabase localDatabase = widget.context.read<LocalDatabase>();
+    final LocalDatabase localDatabase = context.read<LocalDatabase>();
     final String uniqueId = await _operationType.getNewKey(
       localDatabase,
       barcode: barcode,
@@ -63,7 +63,10 @@ class BackgroundTaskHungerGames extends BackgroundTaskBarcode {
       insightAnnotation.value,
       uniqueId,
     );
-    await task.addToManager(localDatabase, widget: widget);
+    if (!context.mounted) {
+      return;
+    }
+    await task.addToManager(localDatabase, context: context);
   }
 
   @override

packages/smooth_app/lib/background/background_task_image.dart
@@ -71,9 +71,9 @@ class BackgroundTaskImage extends BackgroundTaskUpload {
     required final int y1,
     required final int x2,
     required final int y2,
-    required final State<StatefulWidget> widget,
+    required final BuildContext context,
   }) async {
-    final LocalDatabase localDatabase = widget.context.read<LocalDatabase>();
+    final LocalDatabase localDatabase = context.read<LocalDatabase>();
     final String uniqueId = await _operationType.getNewKey(
       localDatabase,
       barcode: barcode,
@@ -91,7 +91,10 @@ class BackgroundTaskImage extends BackgroundTaskUpload {
       x2,
       y2,
     );
-    await task.addToManager(localDatabase, widget: widget);
+    if (!context.mounted) {
+      return;
+    }
+    await task.addToManager(localDatabase, context: context);
   }
 
   @override

packages/smooth_app/lib/background/background_task_offline.dart
@@ -31,11 +31,11 @@ class BackgroundTaskOffline extends BackgroundTaskProgressing {
   static const OperationType _operationType = OperationType.offline;
 
   static Future<void> addTask({
-    required final State<StatefulWidget> widget,
+    required final BuildContext context,
     required final int pageSize,
     required final int totalSize,
   }) async {
-    final LocalDatabase localDatabase = widget.context.read<LocalDatabase>();
+    final LocalDatabase localDatabase = context.read<LocalDatabase>();
     final String uniqueId = await _operationType.getNewKey(
       localDatabase,
       totalSize: totalSize,
@@ -47,7 +47,10 @@ class BackgroundTaskOffline extends BackgroundTaskProgressing {
       pageSize,
       totalSize,
     );
-    await task.addToManager(localDatabase, widget: widget);
+    if (!context.mounted) {
+      return;
+    }
+    await task.addToManager(localDatabase, context: context);
   }
 
   @override

commit a15f8712dda4265d44b7c383640610e048321075

fix: 4627 - no more nutrients wiped out
Impacted files:
* `ordered_nutrients_cache.dart`: removed a bug I introduced in #4768.
* `up_to_date_changes.dart`: now nutriments are safer when background tasks are running.

packages/smooth_app/lib/data_models/up_to_date_changes.dart
@@ -80,7 +80,10 @@ class UpToDateChanges {
     if (change.noNutritionData != null) {
       initial.noNutritionData = change.noNutritionData;
     }
-    if (change.nutriments != null) {
+    // cf. https://github.com/openfoodfacts/smooth-app/issues/4627
+    // If we don't check if nutriments is empty, most of the time we overwrite
+    // potentially existing nutriments with an empty map.
+    if (change.nutriments != null && !change.nutriments!.isEmpty()) {
       initial.nutriments = change.nutriments;
     }
     if (change.servingSize != null) {

packages/smooth_app/lib/pages/product/ordered_nutrients_cache.dart
@@ -24,7 +24,7 @@ class OrderedNutrientsCache {
     cache._orderedNutrients = await cache._get();
     if (cache._orderedNutrients == null) {
       if (context.mounted) {
-        await LoadingDialog.run<OrderedNutrients>(
+        cache._orderedNutrients = await LoadingDialog.run<OrderedNutrients>(
           context: context,
           future: cache._download(),
         );