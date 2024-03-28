d854a60ae38e49af1fa4ac6dd4d383af8725be16
Gabriel Moraes <gmmoraes@Gabriels-Mac-mini.local>
2024-03-27 21:23:42 -0300

    fix: Adding AutoSizeText to FloatingActionButton and adding it to a FittedBox

--- packages/smooth_app/lib/widgets/ranking_floating_action_button.dart
+++ packages/smooth_app/lib/widgets/ranking_floating_action_button.dart
@@ -1,11 +1,10 @@
 import 'dart:math';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:smooth_app/generic_lib/animations/smooth_reveal_animation.dart';
 
-// TODO(monsieurtanuki): we should probably remove that class to avoid confusion with the "compare" button
-/// Floating Action Button dedicated to Personal Ranking
 class RankingFloatingActionButton extends StatelessWidget {
   const RankingFloatingActionButton({
     required this.onPressed,
@@ -19,21 +18,30 @@ class RankingFloatingActionButton extends StatelessWidget {
   Widget build(BuildContext context) => SmoothRevealAnimation(
         animationCurve: Curves.easeInOutBack,
         startOffset: const Offset(0.0, 1.0),
-        child: FittedBox(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(width: MediaQuery.of(context).size.width * 0.09),
-              FloatingActionButton.extended(
-                heroTag: 'ranking_fab_${Random(100)}',
-                elevation: 12.0,
-                icon: const Icon(rankingIconData),
-                label: Text(AppLocalizations.of(context).myPersonalizedRanking),
-                onPressed: onPressed,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(width: MediaQuery.of(context).size.width * 0.09),
+            Flexible(
+              fit: FlexFit.tight,
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                alignment: Alignment.center,
+                child: FloatingActionButton.extended(
+                  heroTag: 'ranking_fab_${Random().nextInt(100)}',
+                  elevation: 12.0,
+                  icon: const Icon(rankingIconData),
+                  label: AutoSizeText(
+                    AppLocalizations.of(context).myPersonalizedRanking,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: onPressed,
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
 }
