db20b51cb6dc968a87af2d48956a53fbbdc9536e
monsieurtanuki <fabrice_fontaine@hotmail.com>
2024-05-05 12:34:40 +0200

    feat: 5191 - knowledge panel image card now clickable (#5220)
    Impacted files:
    * `knowledge_panel_image_card.dart`: now using the optional `linkUrl` field for click operation
    * `pubspec.lock`: wtf
    * `pubspec.yaml`: upgraded to off-dart 3.7.0

--- packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_image_card.dart
+++ packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_image_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:smooth_app/helpers/launch_url_helper.dart';
 
 /// Card that displays a Knowledge Panel _Image_ element.
 class KnowledgePanelImageCard extends StatelessWidget {
@@ -12,11 +13,21 @@ class KnowledgePanelImageCard extends StatelessWidget {
 
   // TODO(g123k): It would be nice to provide a Placeholder
   @override
-  Widget build(BuildContext context) => Image.network(
-        imageElement.url,
-        width: imageElement.width?.toDouble(),
-        height: imageElement.height?.toDouble(),
-      );
+  Widget build(BuildContext context) {
+    final Widget image = Image.network(
+      imageElement.url,
+      width: imageElement.width?.toDouble(),
+      height: imageElement.height?.toDouble(),
+    );
+    final String? linkUrl = imageElement.linkUrl;
+    if (linkUrl == null) {
+      return image;
+    }
+    return InkWell(
+      onTap: () => LaunchUrlHelper.launchURL(linkUrl),
+      child: image,
+    );
+  }
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
