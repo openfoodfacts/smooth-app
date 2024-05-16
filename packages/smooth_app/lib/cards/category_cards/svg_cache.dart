860523623777e09eeb3aafe7ff86836b278e655b
Edouard Marquez <g123k@users.noreply.github.com>
2024-05-16 15:53:15 +0200

    Update packages/smooth_app/lib/cards/category_cards/svg_cache.dart
    Co-authored-by: monsieurtanuki <fabrice_fontaine@hotmail.com>

--- packages/smooth_app/lib/cards/category_cards/svg_cache.dart
+++ packages/smooth_app/lib/cards/category_cards/svg_cache.dart
@@ -99,9 +99,9 @@ class SvgCache extends AbstractCache {
       'nutriscore-unknown-new-formula-en.svg' =>
         localizations.nutriscore_unknown_new_formula,
       'nutriscore-not-applicable.svg' =>
-        localizations.nutriscore_not_applicable_new_formula,
-      'nutriscore-not-applicable-new-formula-en.svg' =>
         localizations.nutriscore_not_applicable,
+      'nutriscore-not-applicable-new-formula-en.svg' =>
+        localizations.nutriscore_not_applicable_new_formula,
       _ => null,
     };
   }
