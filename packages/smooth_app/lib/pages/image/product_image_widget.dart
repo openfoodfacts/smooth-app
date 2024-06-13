a45d3988915572e1f8a56852b927a3a5a33c46cd
Edouard Marquez <moi@edouard-marquez.me>
2024-06-13 12:14:20 +0200

    Card based layout

--- packages/smooth_app/lib/pages/image/product_image_widget.dart
+++ packages/smooth_app/lib/pages/image/product_image_widget.dart
@@ -5,12 +5,13 @@ import 'package:intl/intl.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/generic_lib/widgets/images/smooth_image.dart';
+import 'package:smooth_app/generic_lib/widgets/smooth_card.dart';
 import 'package:smooth_app/query/product_query.dart';
 import 'package:smooth_app/resources/app_icons.dart';
 import 'package:smooth_app/themes/smooth_theme_colors.dart';
 
 /// Displays a product image thumbnail with the upload date on top.
-class ProductImageWidget extends StatelessWidget {
+class ProductImageWidget extends StatefulWidget {
   const ProductImageWidget({
     required this.productImage,
     required this.barcode,
@@ -22,23 +23,54 @@ class ProductImageWidget extends StatelessWidget {
   final double squareSize;
 
   @override
+  State<ProductImageWidget> createState() => _ProductImageWidgetState();
+}
+
+class _ProductImageWidgetState extends State<ProductImageWidget> {
+  @override
+  void initState() {
+    super.initState();
+    _loadImagePalette();
+  }
+
+  Future<void> _loadImagePalette() async {
+    final ColorScheme palette = await ColorScheme.fromImageProvider(
+        provider: NetworkImage(widget.productImage.getUrl(
+      widget.barcode,
+      uriHelper: ProductQuery.uriProductHelper,
+    )));
+
+    setState(() {
+      backgroundColor = palette.primaryContainer;
+      darkBackground = backgroundColor!.computeLuminance() < 0.5;
+    });
+  }
+
+  Color? backgroundColor;
+  bool? darkBackground;
+
+  @override
   Widget build(BuildContext context) {
+    final SmoothColorsThemeExtension colors =
+        Theme.of(context).extension<SmoothColorsThemeExtension>()!;
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
     final DateFormat dateFormat =
         DateFormat.yMd(ProductQuery.getLanguage().offTag);
 
+    darkBackground = darkBackground ?? true;
+
     final Widget image = SmoothImage(
-      width: squareSize,
-      height: squareSize,
+      width: widget.squareSize,
+      height: widget.squareSize,
       imageProvider: NetworkImage(
-        productImage.getUrl(
-          barcode,
+        widget.productImage.getUrl(
+          widget.barcode,
           uriHelper: ProductQuery.uriProductHelper,
         ),
       ),
       rounded: false,
     );
-    final DateTime? uploaded = productImage.uploaded;
+    final DateTime? uploaded = widget.productImage.uploaded;
     if (uploaded == null) {
       return image;
     }
@@ -51,43 +83,55 @@ class ProductImageWidget extends StatelessWidget {
           : appLocalizations.product_image_accessibility_label(date),
       excludeSemantics: true,
       button: true,
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: image,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: SMALL_SPACE,
-                vertical: VERY_SMALL_SPACE,
+      child: SmoothCard(
+        padding: EdgeInsets.zero,
+        color: backgroundColor ?? colors.primaryBlack,
+        borderRadius: ANGULAR_BORDER_RADIUS,
+        margin: EdgeInsets.zero,
+        child: ClipRRect(
+          borderRadius: ANGULAR_BORDER_RADIUS,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: image,
               ),
-              child: Stack(
-                children: <Widget>[
-                  Center(
-                    child: AutoSizeText(
-                      date,
-                      maxLines: 1,
-                    ),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: SMALL_SPACE,
+                    vertical: VERY_SMALL_SPACE,
                   ),
-                  if (expired)
-                    Positioned.directional(
-                      end: 0.0,
-                      height: 20.0,
-                      textDirection: Directionality.of(context),
-                      child: Outdated(
-                        size: 18.0,
-                        color: Theme.of(context)
-                            .extension<SmoothColorsThemeExtension>()!
-                            .red,
+                  child: Stack(
+                    children: <Widget>[
+                      Center(
+                        child: AutoSizeText(
+                          date,
+                          maxLines: 1,
+                          style: TextStyle(
+                            color: darkBackground!
+                                ? Colors.white
+                                : colors.primaryDark,
+                          ),
+                        ),
                       ),
-                    ),
-                ],
-              ),
-            ),
-          )
-        ],
+                      if (expired)
+                        Positioned.directional(
+                          end: 0.0,
+                          height: 20.0,
+                          textDirection: Directionality.of(context),
+                          child: Outdated(
+                            size: 18.0,
+                            color: colors.red,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
