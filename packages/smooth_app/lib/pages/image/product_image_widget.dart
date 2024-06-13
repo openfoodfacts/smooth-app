8979d68fc3eff1b051440b28540adc394c212201
Edouard Marquez <moi@edouard-marquez.me>
2024-06-13 10:16:24 +0200

    Photo gallery: accessibility improvements + date format translatable

--- packages/smooth_app/lib/pages/image/product_image_widget.dart
+++ packages/smooth_app/lib/pages/image/product_image_widget.dart
@@ -1,10 +1,14 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/generic_lib/widgets/images/smooth_image.dart';
 import 'package:smooth_app/query/product_query.dart';
+import 'package:smooth_app/resources/app_icons.dart';
+import 'package:smooth_app/services/smooth_services.dart';
+import 'package:smooth_app/themes/smooth_theme_colors.dart';
 
 /// Displays a product image thumbnail with the upload date on top.
 class ProductImageWidget extends StatelessWidget {
@@ -18,10 +22,19 @@ class ProductImageWidget extends StatelessWidget {
   final String barcode;
   final double squareSize;
 
-  static final DateFormat _dateFormat = DateFormat('yyyy-MM-dd');
-
   @override
   Widget build(BuildContext context) {
+    final AppLocalizations appLocalizations = AppLocalizations.of(context);
+    DateFormat dateFormat;
+
+    try {
+      dateFormat = DateFormat(appLocalizations.product_image_date_format);
+    } catch (_) {
+      dateFormat = DateFormat('yyyy-MM-dd');
+      Logs.e(
+          'Invalid date format detected: ${appLocalizations.product_image_date_format}');
+    }
+
     final Widget image = SmoothImage(
       width: squareSize,
       height: squareSize,
@@ -37,35 +50,53 @@ class ProductImageWidget extends StatelessWidget {
     if (uploaded == null) {
       return image;
     }
-    final DateTime now = DateTime.now();
-    final String date = _dateFormat.format(uploaded);
-    final bool expired = now.difference(uploaded).inDays > 365;
-    return Stack(
-      children: <Widget>[
-        image,
-        SizedBox(
-          width: squareSize,
-          height: squareSize,
-          child: Align(
-            alignment: Alignment.bottomCenter,
+    final bool expired = DateTime.now().difference(uploaded).inDays > 365;
+    final String date = dateFormat.format(uploaded);
+
+    return Semantics(
+      label: expired
+          ? appLocalizations.product_image_outdated_accessibility_label(date)
+          : appLocalizations.product_image_accessibility_label(date),
+      excludeSemantics: true,
+      button: true,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: image,
+          ),
+          SizedBox(
+            width: double.infinity,
             child: Padding(
-              padding: const EdgeInsets.all(SMALL_SPACE),
-              child: Container(
-                height: VERY_LARGE_SPACE,
-                color: expired
-                    ? Colors.red.withAlpha(128)
-                    : Colors.white.withAlpha(128),
-                child: Center(
-                  child: AutoSizeText(
-                    date,
-                    maxLines: 1,
+              padding: const EdgeInsets.symmetric(
+                horizontal: SMALL_SPACE,
+                vertical: VERY_SMALL_SPACE,
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Center(
+                    child: AutoSizeText(
+                      date,
+                      maxLines: 1,
+                    ),
                   ),
-                ),
+                  if (expired)
+                    Positioned.directional(
+                      end: 0.0,
+                      height: 20.0,
+                      textDirection: Directionality.of(context),
+                      child: Outdated(
+                        size: 18.0,
+                        color: Theme.of(context)
+                            .extension<SmoothColorsThemeExtension>()!
+                            .red,
+                      ),
+                    ),
+                ],
               ),
             ),
-          ),
-        ),
-      ],
+          )
+        ],
+      ),
     );
   }
 }
