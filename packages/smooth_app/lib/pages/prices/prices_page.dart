b110334c3d577683969270baddae6e6d9ad9046b
Edouard Marquez <g123k@users.noreply.github.com>
2024-06-25 08:22:11 +0200

    feat: A feedback form for the prices page (#5442)
    * A feedback form for the prices page
    
    * Fix

--- packages/smooth_app/lib/pages/prices/prices_page.dart
+++ packages/smooth_app/lib/pages/prices/prices_page.dart
@@ -1,9 +1,16 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:smooth_app/data_models/preferences/user_preferences.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_back_button.dart';
 import 'package:smooth_app/helpers/launch_url_helper.dart';
+import 'package:smooth_app/helpers/provider_helper.dart';
 import 'package:smooth_app/pages/prices/get_prices_model.dart';
 import 'package:smooth_app/pages/prices/product_prices_list.dart';
+import 'package:smooth_app/resources/app_icons.dart';
+import 'package:smooth_app/themes/smooth_theme_colors.dart';
 import 'package:smooth_app/widgets/smooth_app_bar.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 
@@ -48,6 +55,98 @@ class PricesPage extends StatelessWidget {
               label: Text(appLocalizations.prices_add_a_price),
               icon: const Icon(Icons.add),
             ),
+      bottomNavigationBar: ConsumerFilter<UserPreferences>(
+        buildWhen:
+            (UserPreferences? previousValue, UserPreferences currentValue) =>
+                previousValue?.shouldShowPricesFeedbackForm !=
+                currentValue.shouldShowPricesFeedbackForm,
+        builder: (
+          final BuildContext context,
+          final UserPreferences userPreferences,
+          _,
+        ) {
+          /*if (!userPreferences.shouldShowPricesFeedbackForm) {
+            return EMPTY_WIDGET;
+          }*/
+
+          return const _PricesFeedbackForm();
+        },
+      ),
+    );
+  }
+}
+
+class _PricesFeedbackForm extends StatelessWidget {
+  const _PricesFeedbackForm();
+
+  @override
+  Widget build(BuildContext context) {
+    final AppLocalizations appLocalizations = AppLocalizations.of(context);
+    final SmoothColorsThemeExtension? themeExtension =
+        Theme.of(context).extension<SmoothColorsThemeExtension>();
+
+    return Ink(
+      width: double.infinity,
+      height: kBottomNavigationBarHeight,
+      color: themeExtension!.primaryBlack,
+      child: IconTheme(
+        data: const IconThemeData(color: Colors.white),
+        child: InkWell(
+          onTap: () async {
+            LaunchUrlHelper.launchURL(
+              'https://forms.gle/Vmh9SR3HhPpjMnVF7',
+            );
+            context.read<UserPreferences>().markPricesFeedbackFormAsCompleted();
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: MEDIUM_SPACE,
+              vertical: SMALL_SPACE,
+            ),
+            child: Row(
+              children: <Widget>[
+                ExcludeSemantics(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: themeExtension.secondaryNormal,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const AspectRatio(
+                      aspectRatio: 1.0,
+                      child: Lab(
+                        color: Colors.white,
+                        size: 13.0,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: SMALL_SPACE),
+                Expanded(
+                  child: AutoSizeText(
+                    appLocalizations.prices_feedback_form,
+                    maxLines: 2,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: SMALL_SPACE),
+                InkWell(
+                  customBorder: const CircleBorder(),
+                  onTap: () => context
+                      .read<UserPreferences>()
+                      .markPricesFeedbackFormAsCompleted(),
+                  child: const AspectRatio(
+                    aspectRatio: 1.0,
+                    child: CloseButtonIcon(),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
