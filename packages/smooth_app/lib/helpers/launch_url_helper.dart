05b4e9e3cd1bffa3309d4f868736b3d932a4c7b6
Edouard Marquez <moi@edouard-marquez.me>
2024-06-28 09:36:28 +0200

    Second method to follow deep links with `LaunchUrlHelper`

--- packages/smooth_app/lib/helpers/launch_url_helper.dart
+++ packages/smooth_app/lib/helpers/launch_url_helper.dart
@@ -1,10 +1,26 @@
 import 'dart:io';
 
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import 'package:smooth_app/helpers/analytics_helper.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LaunchUrlHelper {
-  LaunchUrlHelper._();
+  const LaunchUrlHelper._();
+
+  static Future<void> launchURLAndFollowDeepLinks(
+    BuildContext context,
+    String url,
+  ) async {
+    assert(url.isNotEmpty);
+
+    if (url.startsWith(RegExp('http(s)?://[a-z]*.openfoodfacts.(net|org)'))) {
+      AnalyticsHelper.trackOutlink(url: url);
+      GoRouter.of(context).go(url);
+    } else {
+      return launchURL(url);
+    }
+  }
 
   /// Launches the url in an external browser.
   static Future<void> launchURL(String url) async {
