a4b3632067442e34d9fc48e2156125910078dca2
Edouard Marquez <g123k@users.noreply.github.com>
2024-10-21 15:09:17 +0200

    The user was stuck in the onboarding (#5723)

--- packages/smooth_app/lib/pages/navigator/app_navigator.dart
+++ packages/smooth_app/lib/pages/navigator/app_navigator.dart
@@ -72,6 +72,13 @@ class AppNavigator extends InheritedWidget {
     _router.router.pushReplacement(routeName, extra: extra);
   }
 
+  /// Remove all the screens from the stack
+  void clearStack() {
+    while (_router.router.canPop() == true) {
+      _router.router.pop();
+    }
+  }
+
   void pop([dynamic result]) {
     _router.router.pop(result);
   }
@@ -286,7 +293,7 @@ class _SmoothGoRouter {
               externalLink = true;
             }
           } else if (path == _ExternalRoutes.MOBILE_APP_DOWNLOAD) {
-            return AppRoutes.HOME;
+            return AppRoutes.HOME();
           } else if (path == _ExternalRoutes.GUIDE_NUTRISCORE_V2) {
             return AppRoutes.GUIDE_NUTRISCORE_V2;
           } else if (path == _ExternalRoutes.SIGNUP) {
@@ -421,7 +428,8 @@ class AppRoutes {
   AppRoutes._();
 
   // Home page (or walkthrough during the onboarding)
-  static String get HOME => _InternalAppRoutes.HOME_PAGE;
+  static String HOME({bool redraw = false}) =>
+      '${_InternalAppRoutes.HOME_PAGE}?redraw:$redraw';
 
   // Product details (a [Product] is mandatory in the extra)
   static String PRODUCT(
