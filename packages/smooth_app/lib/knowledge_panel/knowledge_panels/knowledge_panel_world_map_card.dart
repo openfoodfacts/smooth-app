7fa5d0b04d93f101b9dd0fa417363a1368edf52a
Edouard Marquez <moi@edouard-marquez.me>
2025-01-07 01:46:11 +0100

    Minor improvements

--- packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_world_map_card.dart
+++ packages/smooth_app/lib/knowledge_panel/knowledge_panels/knowledge_panel_world_map_card.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
-import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/pages/product/world_map_page.dart';
+import 'package:smooth_app/resources/app_icons.dart' as icons;
 
 class KnowledgePanelWorldMapCard extends StatelessWidget {
   const KnowledgePanelWorldMapCard(this.mapElement);
@@ -55,47 +56,60 @@ class KnowledgePanelWorldMapCard extends StatelessWidget {
         userAgentPackageName: 'org.openfoodfacts.app',
       ),
       MarkerLayer(markers: markers),
-      RichAttributionWidget(
-        animationConfig: const ScaleRAWA(),
-        showFlutterMapAttribution: false,
-        attributions: <SourceAttribution>[
-          TextSourceAttribution(
-            'OpenStreetMap contributors',
-            onTap: () => LaunchUrlHelper.launchURL(
-              'https://www.openstreetmap.org/copyright',
-            ),
-          ),
-        ],
-      ),
     ];
+
+    final String? kpTitle = _getTitle(context);
 
     return Padding(
       padding: const EdgeInsetsDirectional.only(bottom: MEDIUM_SPACE),
-      child: SizedBox(
-        height: 200,
-        child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute<Widget>(
-                builder: (_) => WorldMapPage(
-                  title: _getTitle(context),
-                  mapOptions: _generateMapOptions(
-                    coordinates: coordinates,
-                    markerSize: markerSize,
-                    initialZoom: 12.0,
-                    interactive: true,
+      child: Semantics(
+        label: AppLocalizations.of(context)
+            .knowledge_panel_world_map_accessibility_label(kpTitle ?? '?'),
+        image: true,
+        button: true,
+        child: SizedBox(
+          width: double.infinity,
+          height: 200.0,
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: IgnorePointer(
+                  ignoring: true,
+                  child: FlutterMap(
+                    options: mapOptions,
+                    children: <Widget>[
+                      ...children,
+                      const _ExpandMapIcon(),
+                    ],
                   ),
-                  children: children,
                 ),
               ),
-            );
-          },
-          child: IgnorePointer(
-            ignoring: true,
-            child: FlutterMap(
-              options: mapOptions,
-              children: children,
-            ),
+              Positioned.fill(
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute<Widget>(
+                          builder: (_) {
+                            return WorldMapPage(
+                              title: kpTitle,
+                              mapOptions: _generateMapOptions(
+                                coordinates: coordinates,
+                                markerSize: markerSize,
+                                initialZoom: 12.0,
+                                interactive: true,
+                              ),
+                              children: children,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -150,6 +164,35 @@ class KnowledgePanelWorldMapCard extends StatelessWidget {
         mapElement.pointers.map(
           (KnowledgePanelGeoPointer pointer) =>
               pointer.geo?.toJson().toString(),
+        ),
+      ),
+    );
+  }
+}
+
+class _ExpandMapIcon extends StatelessWidget {
+  const _ExpandMapIcon();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: AlignmentDirectional.bottomEnd,
+      child: Padding(
+        padding: EdgeInsetsDirectional.all(
+          VERY_SMALL_SPACE,
+        ),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.black38,
+            shape: BoxShape.circle,
+          ),
+          child: Padding(
+            padding: EdgeInsetsDirectional.all(SMALL_SPACE),
+            child: icons.Expand(
+              size: 15.0,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
