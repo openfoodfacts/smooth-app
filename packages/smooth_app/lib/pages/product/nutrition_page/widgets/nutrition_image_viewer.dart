d262eb235c71bbaadfbb3d716729ac023b6acf02
Edouard Marquez <moi@edouard-marquez.me>
2025-01-21 14:40:24 +0100

    Loading + error states

--- packages/smooth_app/lib/pages/product/nutrition_page/widgets/nutrition_image_viewer.dart
+++ packages/smooth_app/lib/pages/product/nutrition_page/widgets/nutrition_image_viewer.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:provider/provider.dart';
 import 'package:scanner_shared/scanner_shared.dart';
 import 'package:smooth_app/database/transient_file.dart';
+import 'package:smooth_app/helpers/ui_helpers.dart';
 import 'package:smooth_app/query/product_query.dart';
 import 'package:smooth_app/themes/theme_provider.dart';
 
-class NutritionImageViewer extends StatelessWidget {
+class NutritionImageViewer extends StatefulWidget {
   const NutritionImageViewer({
     super.key,
     required this.visible,
@@ -15,8 +17,15 @@ class NutritionImageViewer extends StatelessWidget {
   final bool visible;
 
   @override
+  State<NutritionImageViewer> createState() => _NutritionImageViewerState();
+}
+
+class _NutritionImageViewerState extends State<NutritionImageViewer> {
+  bool _isLoading = true;
+
+  @override
   Widget build(BuildContext context) {
-    if (!visible) {
+    if (!widget.visible) {
       return EMPTY_WIDGET;
     }
 
@@ -28,15 +37,74 @@ class NutritionImageViewer extends StatelessWidget {
           color: context.lightTheme() ? Colors.grey[850]! : Colors.grey[800]!,
           child: InteractiveViewer(
             child: Image(
+              fit: BoxFit.contain,
               image: TransientFile.fromProduct(
                 context.watch<Product>(),
                 ImageField.NUTRITION,
                 ProductQuery.getLanguage(),
               ).getImageProvider()!,
+              frameBuilder: _frameBuilder,
+              loadingBuilder: _loadingBuilder,
+              errorBuilder: _errorBuilder,
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _frameBuilder(
+    BuildContext context,
+    Widget child,
+    int? frame,
+    bool wasSynchronouslyLoaded,
+  ) {
+    if (frame == null) {
+      return _loadingWidget();
+    } else if (_isLoading) {
+      onNextFrame(
+        () => setState(() => _isLoading = false),
+      );
+    }
+    return child;
+  }
+
+  Widget _loadingBuilder(
+    BuildContext context,
+    Widget child,
+    ImageChunkEvent? loadingProgress,
+  ) {
+    if (loadingProgress == null) {
+      return child;
+    }
+    return _loadingWidget();
+  }
+
+  Widget _errorBuilder(
+    BuildContext context,
+    Object error,
+    StackTrace? stackTrace,
+  ) {
+    onNextFrame(
+      () => setState(() => _isLoading = false),
+    );
+
+    return FractionallySizedBox(
+      widthFactor: 0.6,
+      child: Center(
+        child: Text(
+          AppLocalizations.of(context).nutrition_page_photo_error,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loadingWidget() {
+    return const Center(child: CircularProgressIndicator());
   }
 }
