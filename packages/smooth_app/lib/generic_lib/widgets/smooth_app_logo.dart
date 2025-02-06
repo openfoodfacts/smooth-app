2ff9d1f7ef4c592911983aa8c099d54b3a17b277
Edouard Marquez <moi@edouard-marquez.me>
2025-02-06 12:00:24 +0100

    fix: Infinite redraw in the gallery

--- packages/smooth_app/lib/generic_lib/widgets/smooth_app_logo.dart
+++ packages/smooth_app/lib/generic_lib/widgets/smooth_app_logo.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:provider/provider.dart';
 import 'package:smooth_app/themes/theme_provider.dart';
 
 /// An animated logo which can depend on [SmoothSharedAnimationController]
@@ -28,11 +26,15 @@ class _SmoothAnimatedLogoState extends State<SmoothAnimatedLogo>
   void initState() {
     super.initState();
 
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      if (mounted) {
-        _attachAnimation();
-      }
-    });
+    _controller ??= AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..addListener(_onAnimationChanged);
+
+    _animation = Tween<double>(begin: widget.opacityMin, end: widget.opacityMax)
+        .animate(_controller!);
+
+    _controller!.repeat(reverse: true);
   }
 
   @override
@@ -43,36 +45,14 @@ class _SmoothAnimatedLogoState extends State<SmoothAnimatedLogo>
     );
   }
 
-  void _attachAnimation() {
-    AnimationController? controller =
-        _SmoothSharedAnimationControllerState.of(context);
-
-    if (controller == null) {
-      _controller = AnimationController(
-        duration: const Duration(seconds: 1),
-        vsync: this,
-      );
-      controller = _controller;
-    }
-
-    _animation = Tween<double>(begin: widget.opacityMin, end: widget.opacityMax)
-        .animate(controller!)
-      ..addListener(_onAnimationChanged);
-
-    if (!controller.isAnimating) {
-      controller.repeat(reverse: true);
-    }
-  }
-
   void _onAnimationChanged() {
-    if (context.mounted) {
+    if (context.mounted && _controller!.isAnimating) {
       setState(() {});
     }
   }
 
   @override
   void dispose() {
-    _animation?.removeListener(_onAnimationChanged);
     _controller?.dispose();
     super.dispose();
   }
@@ -88,59 +68,5 @@ class SmoothAppLogo extends StatelessWidget {
           ? 'assets/app/release_icon_transparent.svg'
           : 'assets/app/release_icon_dark_transparent_no_border.svg',
     );
-  }
-}
-
-/// A shared [AnimationController] that can be used by multiple
-/// [SmoothAnimatedLogo] and ensure they are all perfectly synced.
-class SmoothSharedAnimationController extends StatefulWidget {
-  const SmoothSharedAnimationController({
-    required this.child,
-  });
-
-  final Widget child;
-
-  @override
-  State<SmoothSharedAnimationController> createState() =>
-      _SmoothSharedAnimationControllerState();
-}
-
-class _SmoothSharedAnimationControllerState
-    extends State<SmoothSharedAnimationController>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Provider<_SmoothSharedAnimationControllerState>(
-      create: (_) => this,
-      child: widget.child,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  static AnimationController? of(BuildContext context) {
-    try {
-      return Provider.of<_SmoothSharedAnimationControllerState>(
-        context,
-        listen: false,
-      )._controller;
-    } catch (_) {
-      return null;
-    }
   }
 }
