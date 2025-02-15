1b15bdc7074a8fd50c162406fcc0cfde17e353cb
monsieurtanuki <fabrice_fontaine@hotmail.com>
2025-02-15 09:26:56 +0100

    fix: 6020 - disable automatic report of failed http connections

--- packages/smooth_app/lib/helpers/analytics_helper.dart
+++ packages/smooth_app/lib/helpers/analytics_helper.dart
@@ -237,6 +237,7 @@ class AnalyticsHelper {
         options
           ..tracesSampleRate = 1.0
           ..beforeSend = _beforeSend
+          ..captureFailedRequests = false
           ..environment =
               '${GlobalVars.storeLabel.name}-${GlobalVars.scannerLabel.name}';
       },
