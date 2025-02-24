ec5faa9fa1f58511660934c59cf543500cfcf90e
monsieurtanuki <fabrice_fontaine@hotmail.com>
2025-02-24 09:29:16 +0100

    After first comments
    Impacted files:
    * `price_existing_amount_card.dart`: now displays the category, if relevant
    * `price_existing_amount_field.dart`: now displays the "PricePer" component, if relevant

--- packages/smooth_app/lib/pages/prices/price_existing_amount_card.dart
+++ packages/smooth_app/lib/pages/prices/price_existing_amount_card.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
@@ -6,9 +8,10 @@ import 'package:smooth_app/generic_lib/widgets/smooth_card.dart';
 import 'package:smooth_app/pages/prices/price_existing_amount_field.dart';
 import 'package:smooth_app/pages/prices/price_meta_product.dart';
 import 'package:smooth_app/pages/prices/price_product_list_tile.dart';
+import 'package:smooth_app/query/product_query.dart';
 
 /// Card that displays an existing amount.
-class PriceExistingAmountCard extends StatelessWidget {
+class PriceExistingAmountCard extends StatefulWidget {
   const PriceExistingAmountCard(
     this.price,
   );
@@ -16,11 +19,62 @@ class PriceExistingAmountCard extends StatelessWidget {
   final Price price;
 
   @override
+  State<PriceExistingAmountCard> createState() =>
+      _PriceExistingAmountCardState();
+}
+
+class _PriceExistingAmountCardState extends State<PriceExistingAmountCard> {
+  String? _categoryTag;
+  String? _categoryName;
+
+  @override
+  void initState() {
+    super.initState();
+    _categoryTag = widget.price.categoryTag;
+    unawaited(_loadCategoryName());
+  }
+
+  Future<void> _loadCategoryName() async {
+    if (_categoryTag == null) {
+      return;
+    }
+    final OpenFoodFactsLanguage language = ProductQuery.getLanguage();
+    final Map<String, TaxonomyCategory>? map =
+        await OpenFoodAPIClient.getTaxonomyCategories(
+      TaxonomyCategoryQueryConfiguration(
+        tags: <String>[_categoryTag!],
+        country: ProductQuery.getCountry(),
+        languages: <OpenFoodFactsLanguage>[language],
+        fields: <TaxonomyCategoryField>[TaxonomyCategoryField.NAME],
+        includeChildren: false,
+      ),
+    );
+    if (map == null) {
+      return;
+    }
+    final TaxonomyCategory? category = map[_categoryTag];
+    if (category == null) {
+      return;
+    }
+    final Map<OpenFoodFactsLanguage, String>? categoryNames = category.name;
+    if (categoryNames == null || categoryNames.isEmpty) {
+      return;
+    }
+    _categoryName = categoryNames[language];
+    if (_categoryName != null) {
+      if (mounted) {
+        setState(() {});
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
-    final bool isDiscounted = price.priceIsDiscounted ?? false;
+    final bool isDiscounted = widget.price.priceIsDiscounted ?? false;
     return SmoothCardWithRoundedHeader(
-      title: appLocalizations.prices_amount_subtitle,
+      // TODO(monsieurtanuki): localize
+      title: 'Price previously added',
       leading: const Icon(Icons.history),
       contentPadding: const EdgeInsetsDirectional.symmetric(
         vertical: MEDIUM_SPACE,
@@ -28,9 +82,13 @@ class PriceExistingAmountCard extends StatelessWidget {
       ),
       child: Column(
         children: <Widget>[
-          if (price.product != null)
+          if (widget.price.product != null)
             PriceProductListTile(
-              product: PriceMetaProduct.priceProduct(price.product!),
+              product: PriceMetaProduct.priceProduct(widget.price.product!),
+            ),
+          if (_categoryName != null || _categoryTag != null)
+            ListTile(
+              title: Text((_categoryName ?? _categoryTag)!),
             ),
           SwitchListTile(
             value: isDiscounted,
@@ -43,7 +101,8 @@ class PriceExistingAmountCard extends StatelessWidget {
             children: <Widget>[
               Expanded(
                 child: PriceExistingAmountField(
-                  value: price.price,
+                  value: widget.price.price,
+                  pricePer: widget.price.pricePer,
                 ),
               ),
               const SizedBox(width: LARGE_SPACE),
@@ -51,7 +110,8 @@ class PriceExistingAmountCard extends StatelessWidget {
                 child: !isDiscounted
                     ? Container()
                     : PriceExistingAmountField(
-                        value: price.priceWithoutDiscount,
+                        value: widget.price.priceWithoutDiscount,
+                        pricePer: widget.price.pricePer,
                       ),
               ),
             ],
