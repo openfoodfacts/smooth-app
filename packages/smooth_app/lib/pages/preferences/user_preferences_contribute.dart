dbc7acd6aeb6d2a94a926039ca7f573c88b62c6a
monsieurtanuki <fabrice_fontaine@hotmail.com>
2025-02-26 18:39:08 +0100

    feat: 5455 - new "data quality" link in "contribute" page

--- packages/smooth_app/lib/pages/preferences/user_preferences_contribute.dart
+++ packages/smooth_app/lib/pages/preferences/user_preferences_contribute.dart
@@ -77,6 +77,14 @@ class UserPreferencesContribute extends AbstractUserPreferences {
         Icons.translate,
       ),
       _getListTile(
+        appLocalizations.contribute_data_quality,
+        () async => LaunchUrlHelper.launchURL(
+          'https://wiki.openfoodfacts.org/Data_quality',
+        ),
+        Icons.high_quality_outlined,
+        externalLink: true,
+      ),
+      _getListTile(
         appLocalizations.how_to_contribute,
         () async => LaunchUrlHelper.launchURL(
           ProductQuery.replaceSubdomain(
