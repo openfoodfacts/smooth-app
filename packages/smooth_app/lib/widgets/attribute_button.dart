f93c659381b0e787f7f083f492e693eec2a4e134
chetanr25 <1ds22ai010@dsce.edu.com>
2025-03-19 22:01:12 +0530

    Fix: Overflow UI, radio working normally

--- packages/smooth_app/lib/widgets/attribute_button.dart
+++ packages/smooth_app/lib/widgets/attribute_button.dart
@@ -35,46 +35,34 @@ class _AttributeButtonState extends State<AttributeButton> {
     final String currentImportanceId = widget.productPreferences
         .getImportanceIdForAttributeId(widget.attribute.id!);
     const double horizontalPadding = LARGE_SPACE;
-    final double widgetWidth =
-        MediaQuery.sizeOf(context).width - 2 * horizontalPadding;
-    final double importanceWidth = widgetWidth / 4;
+    // final double widgetWidth =
+    //     MediaQuery.sizeOf(context).width - 2 * horizontalPadding;
+    // final double importanceWidth = widgetWidth / 4;
     final TextStyle style = themeData.textTheme.headlineMedium!;
     final String? info = widget.attribute.settingNote;
     final List<Widget> children = <Widget>[];
-    if (!editMode) {
+    if (!editMode && currentImportanceId != null) {
       children.add(
-        Expanded(
-          child: Material(
-            type: MaterialType.transparency,
-            child: InkWell(
-              onTap: () async => widget.productPreferences.setImportance(
-                widget.attribute.id!,
-                currentImportanceId,
-              ),
-              child: Container(
-                width: importanceWidth,
-                margin: const EdgeInsets.symmetric(horizontal: 2.0),
-                constraints:
-                    const BoxConstraints(minHeight: MINIMUM_TOUCH_SIZE),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.radio_button_checked,
-                    color: themeData.colorScheme.primary,
-                  ),
-                  title: Text(
-                    widget.productPreferences
-                        .getPreferenceImportanceFromImportanceId(
-                            currentImportanceId)!
-                        .name!,
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () {
-                      setState(() => editMode = !editMode);
-                    },
-                  ),
-                ),
-              ),
+        InkWell(
+          onTap: () async => widget.productPreferences.setImportance(
+            widget.attribute.id!,
+            currentImportanceId,
+          ),
+          child: ListTile(
+            leading: Icon(
+              Icons.radio_button_checked,
+              color: themeData.colorScheme.primary,
+            ),
+            title: AutoSizeText(
+              widget.productPreferences
+                  .getPreferenceImportanceFromImportanceId(currentImportanceId)!
+                  .name!,
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                setState(() => editMode = !editMode);
+              },
             ),
           ),
         ),
@@ -82,39 +70,26 @@ class _AttributeButtonState extends State<AttributeButton> {
     } else {
       for (final String importanceId in AttributeButton._importanceIds) {
         children.add(
-          Expanded(
-            child: Material(
-              type: MaterialType.transparency,
-              child: InkWell(
-                onTap: () async {
-                  setState(() => editMode = !editMode);
-                  widget.productPreferences.setImportance(
-                    widget.attribute.id!,
-                    importanceId,
-                  );
-                },
-                child: Container(
-                  width: importanceWidth,
-                  margin: const EdgeInsets.symmetric(horizontal: 2.0),
-                  constraints:
-                      const BoxConstraints(minHeight: MINIMUM_TOUCH_SIZE),
-                  child: ListTile(
-                    leading: Icon(
-                      currentImportanceId == importanceId
-                          ? Icons.radio_button_checked
-                          : Icons.radio_button_off,
-                      color: themeData.colorScheme.primary,
-                    ),
-                    title: AutoSizeText(
-                      widget.productPreferences
-                          .getPreferenceImportanceFromImportanceId(
-                              importanceId)!
-                          .name!,
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
+          InkWell(
+            onTap: () async {
+              setState(() => editMode = !editMode);
+              widget.productPreferences.setImportance(
+                widget.attribute.id!,
+                importanceId,
+              );
+            },
+            child: ListTile(
+              leading: Icon(
+                currentImportanceId == importanceId
+                    ? Icons.radio_button_checked
+                    : Icons.radio_button_off,
+                color: themeData.colorScheme.primary,
+              ),
+              title: AutoSizeText(
+                widget.productPreferences
+                    .getPreferenceImportanceFromImportanceId(importanceId)!
+                    .name!,
+                maxLines: 2,
               ),
             ),
           ),
@@ -154,7 +129,7 @@ class _AttributeButtonState extends State<AttributeButton> {
                       },
                     ),
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: children,
