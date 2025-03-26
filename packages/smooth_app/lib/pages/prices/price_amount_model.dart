d85f23ff1a2934b369010756ad67ef1cf51af700
Abhishek Prattipati <sreenivas1013@gmail.com>
2025-03-26 19:59:06 +0530

    Improved-2, Fixed negative input issue

--- packages/smooth_app/lib/pages/prices/price_amount_model.dart
+++ packages/smooth_app/lib/pages/prices/price_amount_model.dart
@@ -48,11 +48,15 @@ class PriceAmountModel {
     _promo = value;
   }
 
-  static double? validateDouble(final String value) =>
-      double.tryParse(value) ??
-      double.tryParse(
-        value.replaceAll(',', '.'),
-      );
+// This will check the input and returns null if invalid, else the double value
+  static double? validateDouble(final String value) {
+    final double? res = double.tryParse(value.replaceAll(',', '.'));
+    if (res == null || res <= 0) {
+      return null;
+    } else {
+      return res;
+    }
+  }
 
   String? checkParameters(final BuildContext context) {
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
