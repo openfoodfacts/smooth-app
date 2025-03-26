530253e51b0c8c061494a65674bc49e992d06a2b
Abhishek Prattipati <sreenivas1013@gmail.com>
2025-03-26 19:39:31 +0530

    Revert "Improved, Fixed negative amount issue"
    This reverts commit bfe5fc0526cacf5c2f8250a3d35ee31904c0c8a1.
    
    Problem in macos folder

--- packages/smooth_app/lib/pages/prices/price_amount_model.dart
+++ packages/smooth_app/lib/pages/prices/price_amount_model.dart
@@ -48,14 +48,11 @@ class PriceAmountModel {
     _promo = value;
   }
 
-  static double? validateDouble(final String value) {
-    final double? res = double.tryParse(value.replaceAll(',', '.'));
-    if (res == null || res <= 0) {
-      return null;
-    } else {
-      return res;
-    }
-  }
+  static double? validateDouble(final String value) =>
+      double.tryParse(value) ??
+      double.tryParse(
+        value.replaceAll(',', '.'),
+      );
 
   String? checkParameters(final BuildContext context) {
     final AppLocalizations appLocalizations = AppLocalizations.of(context);
