c0737d6e8d4f049382fda4f6ed1b96870cd3b397
cherish2003 <saicherish90@gmail.com>
2025-03-29 03:51:56 +0530

    fixes

--- packages/smooth_app/lib/pages/product/common/product_refresher.dart
+++ packages/smooth_app/lib/pages/product/common/product_refresher.dart
@@ -2,20 +2,17 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dart_ping/dart_ping.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_app/data_models/fetched_product.dart';
 import 'package:smooth_app/database/dao_product.dart';
 import 'package:smooth_app/database/local_database.dart';
-import 'package:smooth_app/generic_lib/dialogs/smooth_alert_dialog.dart';
 import 'package:smooth_app/generic_lib/loading_dialog.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_snackbar.dart';
-import 'package:smooth_app/pages/user_management/login_page.dart';
+import 'package:smooth_app/pages/user_management/authentication_bottom_sheet.dart';
 import 'package:smooth_app/query/product_query.dart';
 import 'package:smooth_app/query/search_products_manager.dart';
 import 'package:smooth_app/services/smooth_services.dart';
-import 'package:smooth_app/pages/user_management/authentication_bottom_sheet.dart';
 
 /// Refreshes a product on the BE then on the local database.
 class ProductRefresher {
