5e12da076d84a432cbd30b7dd6c95c722054a021
Abhishek Prattipati <sreenivas1013@gmail.com>
2025-04-01 00:16:48 +0530

    code reorder

--- packages/smooth_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/smooth_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import app_settings
 import audioplayers_darwin
 import connectivity_plus
 import device_info_plus
@@ -25,7 +24,6 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
