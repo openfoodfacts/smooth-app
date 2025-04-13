623a33c47a6cb273d5d49c428dfb8bd645f5c615
Abhishek Prattipati <sreenivas1013@gmail.com>
2025-04-13 21:40:59 +0530

    fix-2:add extra condition

--- packages/smooth_app/lib/pages/prices/product_price_add_page.dart
+++ packages/smooth_app/lib/pages/prices/product_price_add_page.dart
@@ -152,7 +152,8 @@ class _ProductPriceAddPageState extends State<ProductPriceAddPage>
                         index: i,
                       ),
                     const SizedBox(height: LARGE_SPACE),
-                    if (model.proofType == ProofType.receipt)
+                    if (model.proofType == ProofType.receipt ||
+                        model.length != 1)
                       const PriceAddProductCard(),
                     // so that the last items don't get hidden by the FAB
                     const SizedBox(height: MINIMUM_TOUCH_SIZE * 2),
