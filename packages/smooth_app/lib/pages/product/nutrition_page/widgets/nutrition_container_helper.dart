abefdf6d9857ec0f1340798c4c8eac4e88b0d3a9
Primaël Quémerais <primael.quemerais@icloud.com>
2025-05-30 08:54:23 +0200

    fix

--- packages/smooth_app/lib/pages/product/nutrition_page/widgets/nutrition_container_helper.dart
+++ packages/smooth_app/lib/pages/product/nutrition_page/widgets/nutrition_container_helper.dart
@@ -104,12 +104,14 @@ class NutritionContainerHelper extends ChangeNotifier {
       // When using Robotoff extraction we enforce the perSize to 100g
       perSize = PerSize.oneHundredGrams;
 
-      final Set<String> extractedNutrients = extractionResult
-              .latestInsight?.data?.nutrients?.keys
-              .map((String key) =>
-                  key.replaceAll('_100g', '').replaceAll('_serving', ''))
-              .toSet() ??
-          <String>{};
+      Set<String> extractedNutrients =
+          extractionResult.latestInsight?.data?.nutrients?.keys.toSet() ??
+              <String>{};
+
+      extractedNutrients = extractedNutrients
+          .where((String nutrient) => !nutrient.contains('_serving'))
+          .map((String key) => key.replaceAll('_100g', ''))
+          .toSet();
 
       for (final String nutrientOffTag in extractedNutrients) {
         // If the nutrient is not in the list of nutrients, we add it
