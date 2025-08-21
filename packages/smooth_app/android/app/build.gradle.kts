42b40cf692f4ccca38bd1bf5499c6f9d77fe0862
Edouard Marquez <g123k@users.noreply.github.com>
2025-08-21 10:37:35 +0200

    Fix Android build (#6858)

--- packages/smooth_app/android/app/build.gradle.kts
+++ packages/smooth_app/android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "org.openfoodfacts.scanner"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
