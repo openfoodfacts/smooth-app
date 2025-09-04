24134b727474e9690a33b12c4b3a68ca05b37f1d
Primaël Quémerais <primael.quemerais@icloud.com>
2025-09-04 18:04:50 +0200

    Reverted unnecessary changes

--- packages/smooth_app/lib/pages/prices/price_category_widget.dart
+++ packages/smooth_app/lib/pages/prices/price_category_widget.dart
@@ -1,8 +1,7 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
-import 'package:smooth_app/generic_lib/design_constants.dart';
-import 'package:smooth_app/pages/prices/price_button.dart';
+import 'package:smooth_app/l10n/app_localizations.dart';
+import 'package:smooth_app/pages/prices/price_header_container.dart';
 import 'package:smooth_app/pages/prices/price_l10n_helper.dart';
 
 /// Price Category display (no price data here).
@@ -32,32 +31,14 @@ class _PriceCategoryWidgetState extends State<PriceCategoryWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Semantics(
-      label: _generateSemanticsLabel(),
-      container: true,
-      excludeSemantics: true,
-      child: Padding(
-        padding: const EdgeInsetsGeometry.directional(start: 12.0),
-        child: Wrap(
-          spacing: VERY_SMALL_SPACE,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          runSpacing: 0,
-          children: <Widget>[
-            if (_helper.getCategory() != null)
-              AutoSizeText(
-                _helper.getCategory()!,
-                maxLines: 2,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-            if (_helper.getOrigins() != null)
-              for (final String tag in _helper.getOrigins()!)
-                PriceButton(title: tag, onPressed: () {}),
-            if (_helper.getLabels() != null)
-              for (final String tag in _helper.getLabels()!)
-                PriceButton(title: tag, onPressed: () {}),
-          ],
-        ),
-      ),
+    final AppLocalizations appLocalizations = AppLocalizations.of(context);
+
+    return PriceHeaderContainer(
+      semanticsLabel: _generateSemanticsLabel(),
+      imageProvider: null,
+      line1: _helper.getCategory()!,
+      line2: _helper.getOrigins()?.join(',${appLocalizations.sep}'),
+      line3: _helper.getLabels()?.join(',${appLocalizations.sep}'),
     );
   }
 
