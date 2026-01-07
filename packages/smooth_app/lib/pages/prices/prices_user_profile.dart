b1240e180852a6e809b81a3511e3881c2c32b849
chetanr25 <chetan250204@gmail.com>
2026-01-07 19:25:58 +0530

    refactor: remove hardcoded width calculation in prices user profile, enhanced UI for Dashboard tile

--- packages/smooth_app/lib/pages/prices/prices_user_profile.dart
+++ packages/smooth_app/lib/pages/prices/prices_user_profile.dart
@@ -3,6 +3,8 @@ import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_card.dart';
 import 'package:smooth_app/l10n/app_localizations.dart';
+import 'package:smooth_app/themes/smooth_theme_colors.dart';
+import 'package:smooth_app/themes/theme_provider.dart';
 
 class PricesUserProfile extends StatelessWidget {
   const PricesUserProfile({super.key, required this.profile});
@@ -21,34 +23,55 @@ class PricesUserProfile extends StatelessWidget {
               style: const TextStyle(fontWeight: FontWeight.bold),
             ),
           ),
-          Wrap(
-            alignment: WrapAlignment.spaceEvenly,
-            children: <Widget>[
-              _profileStatsButton(
-                Icons.sell_outlined,
-                profile.priceCount ?? 0,
-                appLocalizations.prices_generic_title,
-                context,
-              ),
-              _profileStatsButton(
-                Icons.location_on,
-                profile.locationCount ?? 0,
-                'locations',
-                context,
-              ),
-              _profileStatsButton(
-                Icons.restaurant_menu,
-                profile.productCount ?? 0,
-                appLocalizations.settings_app_products,
-                context,
-              ),
-              _profileStatsButton(
-                Icons.image,
-                profile.proofCount ?? 0,
-                appLocalizations.prices_proof_subtitle,
-                context,
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(SMALL_SPACE),
+            child: Column(
+              spacing: SMALL_SPACE,
+              children: <Widget>[
+                Row(
+                  spacing: SMALL_SPACE,
+                  children: <Widget>[
+                    Expanded(
+                      child: _profileStatsButton(
+                        Icons.sell_outlined,
+                        profile.priceCount ?? 0,
+                        appLocalizations.prices_generic_title,
+                        context,
+                      ),
+                    ),
+                    Expanded(
+                      child: _profileStatsButton(
+                        Icons.location_on,
+                        profile.locationCount ?? 0,
+                        'locations',
+                        context,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  spacing: SMALL_SPACE,
+                  children: <Widget>[
+                    Expanded(
+                      child: _profileStatsButton(
+                        Icons.restaurant_menu,
+                        profile.productCount ?? 0,
+                        appLocalizations.settings_app_products,
+                        context,
+                      ),
+                    ),
+                    Expanded(
+                      child: _profileStatsButton(
+                        Icons.image,
+                        profile.proofCount ?? 0,
+                        appLocalizations.prices_proof_subtitle,
+                        context,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -59,31 +82,49 @@ class PricesUserProfile extends StatelessWidget {
     IconData icon,
     int count,
     String label,
-    BuildContext context, {
-    Color? color,
-  }) {
-    return SmoothCard(
-      // color: Theme.of(context).colorScheme.onSurface.withAlpha(24),
-      child: Container(
-        width: MediaQuery.sizeOf(context).width / 2 - 3 * LARGE_SPACE,
-        padding: const EdgeInsets.all(SMALL_SPACE),
+    BuildContext context,
+  ) {
+    final SmoothColorsThemeExtension themeExtension = context
+        .extension<SmoothColorsThemeExtension>();
+    final bool lightTheme = context.lightTheme();
+
+    return Material(
+      color: lightTheme
+          ? themeExtension.primaryMedium
+          : themeExtension.primaryDark,
+      borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(MEDIUM_SPACE),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(icon, color: color, size: DEFAULT_ICON_SIZE),
+                Icon(
+                  icon,
+                  size: DEFAULT_ICON_SIZE,
+                  color: lightTheme ? themeExtension.primaryDark : Colors.white,
+                ),
                 const SizedBox(width: VERY_SMALL_SPACE),
                 Text(
                   count.toString(),
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: lightTheme
+                        ? themeExtension.primaryDark
+                        : Colors.white,
+                  ),
                 ),
               ],
             ),
             Text(
               label,
-              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: lightTheme ? themeExtension.primaryDark : Colors.white,
+              ),
             ),
           ],
         ),
