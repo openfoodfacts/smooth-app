79c37ca0d37004af61b81f9fff85c758e5664c5f
monsieurtanuki <fabrice_fontaine@hotmail.com>
2026-01-12 09:57:51 +0100

    feat: 7345 - hunger games - access to best image quality
    Impacted file:
    * `question_image_full_page.dart`: added a FAB that links to a browser with the best version of the image

--- packages/smooth_app/lib/pages/hunger_games/question_image_full_page.dart
+++ packages/smooth_app/lib/pages/hunger_games/question_image_full_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:smooth_app/helpers/launch_url_helper.dart';
 import 'package:smooth_app/widgets/smooth_app_bar.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 
@@ -23,6 +24,12 @@ class QuestionImageFullPage extends StatelessWidget {
         maxLines: 2,
       ),
     ),
+    floatingActionButton: FloatingActionButton(
+      child: const Icon(Icons.zoom_in),
+      onPressed: () async => LaunchUrlHelper.launchURL(
+        _getBetterUrl(question.imageUrl!) ?? question.imageUrl!,
+      ),
+    ),
     body: ConstrainedBox(
       constraints: const BoxConstraints.expand(),
       child: InteractiveViewer(
@@ -41,4 +48,29 @@ class QuestionImageFullPage extends StatelessWidget {
       ),
     ),
   );
+
+  // cf. https://github.com/openfoodfacts/smooth-app/issues/3065
+  String? _getBetterUrl(final String url) {
+    const int ascii0 = 48;
+    const int ascii9 = 57;
+
+    if (!url.endsWith('.400.jpg')) {
+      return null;
+    }
+    final int pos = url.lastIndexOf('/');
+    if (pos == -1) {
+      return null;
+    }
+    final String newLastPart;
+    final String lastPart = url.substring(pos + 1);
+    final int firstChar = lastPart.codeUnitAt(0);
+    if (firstChar >= ascii0 && firstChar <= ascii9) {
+      // example: from 2.400.jpg to 2.jpg
+      newLastPart = lastPart.replaceAll('.400.', '.');
+    } else {
+      // example: from front_fr.4.400.jpg to front_fr.4.full.jpg
+      newLastPart = lastPart.replaceAll('.400.', '.full.');
+    }
+    return url.replaceFirst(lastPart, newLastPart);
+  }
 }
