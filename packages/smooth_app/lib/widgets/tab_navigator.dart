be1631039055507fd3acbf09c9f1b5a65dc587aa
Edouard Marquez <moi@edouard-marquez.me>
2026-01-17 19:12:07 +0100

    Fix for invisible camera feed

--- packages/smooth_app/lib/widgets/tab_navigator.dart
+++ packages/smooth_app/lib/widgets/tab_navigator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/pages/history_page.dart';
 import 'package:smooth_app/pages/homepage/homepage.dart';
 import 'package:smooth_app/pages/page_manager.dart';
@@ -6,13 +7,22 @@ import 'package:smooth_app/pages/preferences_v2/preferences_page.dart';
 import 'package:smooth_app/pages/scan/scan_page.dart';
 
 class TabNavigator extends StatelessWidget {
-  const TabNavigator({required this.navigatorKey, required this.tabItem});
+  const TabNavigator({
+    required this.navigatorKey,
+    required this.tabItem,
+    this.visible = true,
+  });
 
   final GlobalKey<NavigatorState> navigatorKey;
   final BottomNavigationTab tabItem;
+  final bool visible;
 
   @override
   Widget build(BuildContext context) {
+    if (!visible) {
+      return EMPTY_WIDGET;
+    }
+
     final Widget child = switch (tabItem) {
       BottomNavigationTab.HomePage => const HomePage(),
       BottomNavigationTab.List => const HistoryPage(),
