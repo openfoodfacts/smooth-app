a67d6c6e959399e9c458fea42100fd935af9ba4f
monsieurtanuki <fabrice_fontaine@hotmail.com>
2026-06-19 19:17:01 +0200

    fix: 7586 - NOVA 1 text for NOVA 1

--- packages/smooth_app/lib/pages/guides/guide/guide_nova.dart
+++ packages/smooth_app/lib/pages/guides/guide/guide_nova.dart
@@ -135,7 +135,8 @@ class _NOVALogos extends StatelessWidget {
             child: Row(
               children: assets
                   .map(
-                    (String path) => Expanded(
+                    (String path) =>
+                    Expanded(
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: SvgPicture(
@@ -145,7 +146,7 @@ class _NOVALogos extends StatelessWidget {
                         ),
                       ),
                     ),
-                  )
+              )
                   .toList(growable: false),
             ),
           );
@@ -168,7 +169,7 @@ class _NOVASection2 extends StatelessWidget {
         GuidesTitleWithText(
           title: appLocalizations.guide_nova_groups_arg1_title,
           icon: const icons.FoodIcons.nova1(),
-          text: appLocalizations.guide_nova_groups_arg2_text,
+          text: appLocalizations.guide_nova_groups_arg1_text,
         ),
         GuidesTitleWithText(
           title: appLocalizations.guide_nova_groups_arg2_title,
