1ec12a08b3a8f98e7ab833c04b6f88d804172439
monsieurtanuki <fabrice_fontaine@hotmail.com>
2026-07-06 19:01:12 +0200

    Apply suggestion from @monsieurtanuki

--- packages/smooth_app/lib/generic_lib/bottom_sheets/smooth_bottom_sheet.dart
+++ packages/smooth_app/lib/generic_lib/bottom_sheets/smooth_bottom_sheet.dart
@@ -10,7 +10,6 @@ import 'package:smooth_app/helpers/haptic_feedback_helper.dart';
 import 'package:smooth_app/resources/app_icons.dart' as icons;
 import 'package:smooth_app/themes/smooth_theme_colors.dart';
 import 'package:smooth_app/themes/theme_provider.dart';
-import 'package:smooth_app/widgets/smooth_view_padding.dart';
 
 Future<T?> showSmoothModalSheet<T>({
   required BuildContext context,
