3d646c1e11cd41c781981fb1dbf4aa0cd3b8ddd6
Pierre Slamich <pierre@openfoodfacts.org>
2026-09-19 21:39:01 +0200

    Update analytics_helper.dart

--- packages/smooth_app/lib/helpers/analytics_helper.dart
+++ packages/smooth_app/lib/helpers/analytics_helper.dart
@@ -255,6 +255,7 @@ class AnalyticsHelper {
             'https://22ec5d0489534b91ba455462d3736680@o241488.ingest.sentry.io/5376745'
         // To set a uniform sample rate
         ..tracesSampleRate = 1.0
+        ..enableTombstone = true
         ..beforeSend = _beforeSend
         ..captureFailedRequests = false
         ..environment =
