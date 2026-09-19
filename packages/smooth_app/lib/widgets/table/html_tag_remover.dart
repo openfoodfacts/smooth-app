1be85aa136bf75677897e640749fe2861ca03131
monsieurtanuki <fabrice_fontaine@hotmail.com>
2026-09-19 14:13:11 +0200

    fix: 7779 - KP cell tooltips without html tags (#7782)
    * fix: 7779 - KP cell tooltips without html tags
    
    * coderabbitai suggestion

---
+++ packages/smooth_app/lib/widgets/table/html_tag_remover.dart
@@ -0,0 +1,18 @@
+import 'package:html/dom.dart';
+import 'package:html/dom_parsing.dart';
+import 'package:html/parser.dart';
+
+/// Remover of HTML tags
+class HtmlTagRemover extends TreeVisitor {
+  String extract(String html) {
+    _buffer.clear();
+    final Document document = parse(html);
+    visit(document);
+    return _buffer.toString();
+  }
+
+  final StringBuffer _buffer = StringBuffer();
+
+  @override
+  void visitText(Text node) => _buffer.write(node.data);
+}
